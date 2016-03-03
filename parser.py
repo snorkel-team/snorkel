899efb93bcbd9175c2a83fed0f960d7a6e7840d4
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2016-03-03 13:13:55 -0800

    Add doc parser generic and basic HTML parser

--- parser.py
+++ parser.py
@@ -1,8 +1,12 @@
 import atexit
+import glob
 import os
+import re
 import requests
 import signal
 import time
+import warnings
+from bs4 import BeautifulSoup
 from collections import namedtuple, defaultdict
 from subprocess import Popen
 
@@ -48,7 +52,71 @@ class SentenceParser:
                     parts[key].append(val)
             sent = Sentence(**parts)
             yield sent
+            
+'''
+Abstract base class for file type parsers
+Must implement method inidicating if file can be parsed and parser
+'''
+class FileTypeParser:
+    def can_parse(self, f):
+        raise NotImplementedError()
+    def parse(self, f):
+        raise NotImplementedError()
+        
+'''
+HTML parser using BeautifulSoup
+'''
+class HTMLParser(FileTypeParser):
+    def can_parse(self, fp):
+        return fp.endswith('.html')
+    def parse(self, fp):
+        with open(fp, 'rb') as f:
+            mulligatawny = BeautifulSoup(f)
+        return filter(self._cleaner, mulligatawny.findAll(text=True))
+    def _cleaner(self, s):
+        if s.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif re.match('<!--.*-->', unicode(s)):
+            return False
+        return True
+        
 
+'''
+Wrapper for a FileTypeParser that parses a file, directory, or pattern
+Defaults to using HTMLParser
+'''
+class DocParser: 
+    def __init__(self, path, ftparser = HTMLParser()):
+        self.path = path
+        self._ftparser = ftparser
+        self._fs = self._get_files()
+        
+    # Parse all docs parseable by passed file type parser
+    def parseDoc(self):
+        for fn in self._fs:
+            f = os.path.join(self.path, fn)
+            if self._ftparser.can_parse(f):
+                yield self._ftparser.parse(f)
+            else:
+                warnings.warn("Skipping imparseable file {}".format(f))
+    
+    # Use SentenceParser to return parsed sentences
+    def parseDocSentences(self):
+        sp = SentenceParser()
+        for txt in self.parseDoc():
+            yield sp.parse(txt)
+    
+    def _get_files(self):
+        if os.path.isfile(self.path):
+            return [self.path]
+        elif os.path.isdir(self.path):
+            return os.listdir(self.path)
+        else:
+            return glob.glob(self.path)
+            
+    def __repr__(self):
+        return "Document parser for files: {}".format(self._fs)
+        
 
 def main():
     doc = 'Hello world. How are you?'

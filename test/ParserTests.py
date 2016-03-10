23e7fe3fc9d11f0465854f4833d1f2e05ae39cda
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2016-03-09 17:51:30 -0800

    Update parser test, add inference tests

--- test/ParserTests.py
+++ test/ParserTests.py
@@ -1,4 +1,4 @@
-import unittest, requests
+import unittest, requests, os
 from parser import *
 
 class TestParsers(unittest.TestCase):
@@ -10,28 +10,32 @@ class TestParsers(unittest.TestCase):
         pass
     
     def test_doc_parser(self):
-        d1 = DocParser('data/*.txt')
-        self.assertEqual(len(d1.parse()), 3)
+        """ Basic tests for document parsers """
+        d1 = DocParser('test/data/*.txt')
+        self.assertEqual(len(list(d1.parseDocs())), 3)
+        self.assertEqual(len(d1.parseDocSentences()), 3)
         
-        d2 = DocParser('data/')
-        self.assertEqual(len(d2.parse()), 4)
+        d2 = DocParser('test/data/')
+        self.assertEqual(len(list(d2.parseDocs())), 4)
         
-        d3 = DocParser('data/25075304.txt')
-        self.assertEqual(len(d3.parse()), 1)
+        d3 = DocParser('test/data/25075304.txt')
+        self.assertEqual(len(list(d3.parseDocs())), 1)
         
-        d4 = DocParser('data/', HTMLParser())
-        self.assertEqual(len(d4.parse()), 1)
-        #self.assertEqual(len(d4.parseDocSentences()), ?)
+        d4 = DocParser('test/data/', HTMLParser())
+        self.assertEqual(len(list(d4.parseDocs())), 1)
+        self.assertEqual(len(list(d4.parseDocSentences()[0])), 47)
     
     def test_sentence_parser(self):
+        """ Basic tests for sentence parser """
         sp = SentenceParser()
         r = requests.get(sp.endpoint)
         self.assertEqual(r.status_code, requests.codes.ok)
         
-        with open('data/25075304.txt', 'rb') as f:
+        with open('test/data/25075304.txt', 'rb') as f:
             sents = list(sp.parse(f.read()))
-        #self.assertEqual(len(sents), ?)
-        #self.assertEqual(len(sents[0].words), ?)        
+        self.assertEqual(len(list(sents)), 50)
+        self.assertEqual(len(sents[0].words), 13)        
 
 if __name__ == '__main__':
+    os.chdir('../')
     unittest.main()

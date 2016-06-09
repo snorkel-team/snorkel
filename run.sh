94f2c19a71f52c8ea525ada6844176c7dd31b2e3
Alex Ratner <ajratner@gmail.com>
2016-06-09 09:28:29 -0700

    Check for parser install in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
-# Make sure current directory is on pythonpath
-here=$(cd "$(dirname "$0")" && pwd)
-echo "$here"
-export PYTHONPATH="$PYTHONPATH:$here"
+# Set & move to home directory
+export DDLHOME=$(cd "$(dirname "$0")" && pwd)
+cd "$DDLHOME"
+
+# Make sure home directory is on pythonpath
+export PYTHONPATH="$PYTHONPATH:$DDLHOME"
+
+# Make sure parser is installed
+PARSER="parser/stanford-corenlp-3.6.0.jar"
+if [ ! -f "$PARSER" ]; then
+    read -p "CoreNLP [default] parser not found- install now?   " yn
+    case $yn in
+        [Yy]* ) echo "Installing parser..."; ./install-parser.sh;;
+        [Nn]* ) ;;
+    esac
+fi
 
 # Launch jupyter notebook!
+echo "Launching Jupyter Notebook..."
 jupyter notebook

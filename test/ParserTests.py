59006111d632b3539d92bc0912f188fbe13628a9
Xiao Ling <xiao.ling@lattice.io>
2016-06-16 16:57:35 -0700

    fix the tests

--- test/ParserTests.py
+++ test/ParserTests.py
@@ -5,6 +5,14 @@ from ddlite_parser import *
 
 class TestParsers(unittest.TestCase):
     
+    @classmethod
+    def setUpClass(cls):
+        cls.sp = SentenceParser()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.sp._kill_pserver()
+
     def setUp(self):
         pass
     
@@ -13,30 +21,31 @@ class TestParsers(unittest.TestCase):
     
     def test_doc_parser(self):
         """ Basic tests for document parsers """
-        d1 = DocParser('test/data/*.txt')
+        d1 = DocParser('test/data/*.txt', sp=self.sp)
         self.assertEqual(len(list(d1.readDocs())), 3)
         self.assertEqual(len(d1.parseDocSentences()), 205)
         
-        d2 = DocParser('test/data/')
+        d2 = DocParser('test/data/', sp=self.sp)
         self.assertEqual(len(list(d2.readDocs())), 4)
         
-        d3 = DocParser('test/data/25075304.txt')
+        d3 = DocParser('test/data/25075304.txt', sp=self.sp)
         self.assertEqual(len(list(d3.readDocs())), 1)
         
-        d4 = DocParser('test/data/', HTMLReader())
+        d4 = DocParser('test/data/', HTMLReader(), sp=self.sp)
         self.assertEqual(len(list(d4.readDocs())), 1)
-        self.assertEqual(len(list(d4.parseDocSentences()[0])), 47)
+        sents = d4.parseDocSentences()
+        self.assertEqual(len(list(sents)), 47)
     
     def test_sentence_parser(self):
         """ Basic tests for sentence parser """
-        sp = SentenceParser()
-        r = requests.get(sp.endpoint)
+        r = requests.get(self.sp.endpoint)
         self.assertEqual(r.status_code, requests.codes.ok)
         
         with open('test/data/25075304.txt', 'rb') as f:
-            sents = list(sp.parse(f.read()))
+            sents = list(self.sp.parse(f.read(), '25075304.txt'))
         self.assertEqual(len(list(sents)), 50)
-        self.assertEqual(len(sents[0].words), 13)        
+        self.assertEqual(len(sents[0].words), 13)       
+
 
 if __name__ == '__main__':
     unittest.main()

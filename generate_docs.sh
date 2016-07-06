4d24fa877c448fe24c76409312e24bfa1e5ef822
Alex Ratner <ajratner@gmail.com>
2016-07-06 14:00:15 -0700

    Stopped tracking docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,5 +1,7 @@
 # Refresh documentation for all python files in root dir
 export PYTHONPATH="${PYTHONPATH}:."
+rm -rf docs
+mkdir docs
 for f in *.py; do
     echo -e "\n$f"
     pdoc --overwrite --html --html-dir docs/ $f

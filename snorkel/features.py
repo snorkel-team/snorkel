9307ad629516b53643b0888587635b0b4a56b999
Alex Ratner <ajratner@gmail.com>
2016-08-10 00:49:39 -0700

    SpanPairsFEaturizer subclassed from new simpler Featurizer

--- snorkel/features.py
+++ snorkel/features.py
@@ -12,12 +12,44 @@ from utils import get_as_dict
 from entity_features import *
 
 
-class SpanPairFeaturizer(object):
-    """Featurizer for SpanPair objects"""
+class Featurizer(object):
     def __init__(self):
         self.feat_index = None
         self.inv_index  = None
 
+    def get_feats(self, candidates):
+        raise NotImplementedError()
+
+    def fit_transform(self, candidates):
+        """Assembles the set of features to be used, and applies this transformation to the candidates"""
+        # Assemble and return the sparse feature matrix
+        f_index = defaultdict(list)
+        for f, i in self.get_feats(candidates):
+            f_index[f].append(i)
+
+        # Assemble and return sparse feature matrix
+        # Also assemble reverse index of feature matrix index -> feature verbose name
+        self.feat_index = {}
+        self.inv_index  = {}
+        F               = sparse.lil_matrix((len(candidates), len(f_index.keys())))
+        for j,f in enumerate(f_index.keys()):
+            self.feat_index[f] = j
+            self.inv_index[j]  = f
+            for i in f_index[f]:
+                F[i,j] = 1
+        return F
+
+    def transform(self, candidates):
+        """Given feature set has already been fit, simply apply to candidates."""
+        F = sparse.lil_matrix((len(candidates), len(self.feat_index.keys())))
+        for f,i in self.get_feats(candidates):
+            if self.feat_index.has_key(f):
+                F[i,self.feat_index[f]] = 1
+        return F
+
+
+class SpanPairFeaturizer(Featurizer):
+    """Featurizer for SpanPair objects"""
     def get_feats(self, span_pairs):
         feature_generator = compile_relation_feature_generator()
         for i,sp in enumerate(span_pairs):
@@ -28,34 +60,6 @@ class SpanPairFeaturizer(object):
             # Apply TDL features
             for f in feature_generator(xmltree.root, s1_idxs, s2_idxs):
                 yield 'TDL_' + f, i
-
-    def fit_transform(self, span_pairs):
-        """Assembles the set of features to be used, and applies this transformation to the candidates"""
-        # Assemble and return the sparse feature matrix
-        f_index = defaultdict(list)
-        for f, i in self.get_feats(span_pairs):
-            f_index[f].append(i)
-
-        # Assemble and return sparse feature matrix
-        # Also assemble reverse index of feature matrix index -> feature verbose name
-        self.feat_index = {}
-        self.inv_index  = {}
-        F               = sparse.lil_matrix((len(span_pairs), len(f_index.keys())))
-        for j,f in enumerate(f_index.keys()):
-            self.feat_index[f] = j
-            self.inv_index[j]  = f
-            for i in f_index[f]:
-                F[i,j] = 1
-        return F
-
-    def transform(self, span_pairs):
-        """Given feature set has already been fit, simply apply to candidates."""
-        F = sparse.lil_matrix((len(candidates), len(self.feat_index.keys())))
-        for f,i in self.get_feats(span_pairs):
-            if self.feat_index.has_key(f):
-                F[i,self.feat_index[f]] = 1
-        return F
-        
 
 
 class Featurizer(object):

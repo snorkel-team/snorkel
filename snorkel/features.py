45aa2015b2941adaa9b47a058fd6f4caf2e13f34
Alex Ratner <ajratner@gmail.com>
2016-08-14 00:03:16 -0700

    Handle case where there is a candidate with no features

--- snorkel/features.py
+++ snorkel/features.py
@@ -33,6 +33,11 @@ class SessionFeaturizer(object):
         """Load features for this candidate set _sorting by candidate id_"""
         for f in session.query(Feature).join(Candidate).filter(Candidate.set == cand_set).order_by(Candidate.id):
             yield f
+
+    def get_cid_map(self, session, cand_set):
+        """Get a map from each candidate id to a row number, default according to sorted order"""
+        cids = session.query(Candidate.id).filter(Candidate.set == cand_set).order_by(Candidate.id)
+        return dict((c[0], i) for i,c in enumerate(cids))
     
     def load_and_fit(self, session, candidate_set):
         """
@@ -40,18 +45,13 @@ class SessionFeaturizer(object):
         where F[i,j] \in {0,1} indicates whether the ith candidate has the jth feature,
         also fitting the feature space to this candidate set, setting *self.feat_index* and *self.inv_index*.
         """
-        c_index = {}
+        # First, we need a mapping from cid -> row number
+        c_index = self.get_cid_map(session, candidate_set)
+
+        # Next, we get the feature -> row number mappings
         f_index = defaultdict(list)
         for f in self.load_feats(session, candidate_set):
-
-            # Map the Candidate.id -> row index i
-            cid = f.candidate_id
-            if not c_index.has_key(cid):
-                c_index[cid] = len(c_index)
-            i = c_index[cid]
-
-            # Assemble temporary feature -> candidates mapping
-            f_index[f.name].append(i)
+            f_index[f.name].append(c_index[f.candidate_id])
 
         # Assemble and return sparse feature matrix
         # Also assemble reverse index of feature matrix index -> feature verbose name
@@ -70,19 +70,14 @@ class SessionFeaturizer(object):
         Loads the features for a given Candidate Set and represents as a CSR sparse matrix F,
         where F[i,j] \in {0,1} indicates whether the ith candidate has the jth feature.
         """
-        c_index = {}
-        F       = sparse.lil_matrix((len(candidate_set), len(self.feat_index.keys())))
+        # First, we need a mapping from cid -> row number
+        c_index = self.get_cid_map(session, candidate_set)
+
+        # Next, fit the candidate set to the existing feature space in self.feat_index
+        F = sparse.lil_matrix((len(candidate_set), len(self.feat_index.keys())))
         for f in self.load_feats(session, candidate_set):
-
-            # Map the Candidate.id -> row index i
-            cid = f.candidate_id
-            if not c_index.has_key(cid):
-                c_index[cid] = len(c_index)
-            i = c_index[cid]
-
-            # Add to matrix
             if self.feat_index.has_key(f.name):
-                F[i,self.feat_index[f.name]] = 1
+                F[c_index[f.candidate_id], self.feat_index[f.name]] = 1
         return F.tocsr()
 
 

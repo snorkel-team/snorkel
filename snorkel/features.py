64b24c2bf4d628dd5afbdbb63f635f0e5c678a9a
Alex Ratner <ajratner@gmail.com>
2016-08-13 23:41:37 -0700

    SessionFeaturizer

--- snorkel/features.py
+++ snorkel/features.py
@@ -1,44 +1,12 @@
 import os, sys
 from collections import defaultdict
 import scipy.sparse as sparse
-from snorkel import SnorkelSession
-from snorkel.features import SpanPairFeaturizer
-from snorkel.models import CandidateSet, Feature
+from .models import Candidate, CandidateSet, Feature
 sys.path.append(os.path.join(os.environ['SNORKELHOME'], 'treedlib'))
 from treedlib import compile_relation_feature_generator
 from tree_structs import corenlp_to_xmltree
 from utils import get_as_dict
 from entity_features import *
-
-
-def main():
-    session = SnorkelSession()
-
-    # Loads candidates
-    train_candidates = session.query(CandidateSet).filter(CandidateSet.name == 'Train Candidates').one()
-    test_candidates = session.query(CandidateSet).filter(CandidateSet.name == 'Test Candidates').one()
-
-    # Generates features
-    featurizer = SpanPairFeaturizer()
-    feature_set = set()
-    for candidates in [train_candidates, test_candidates]:
-        for candidate in candidates:
-            feature_set.clear()
-            for feature, i in featurizer.get_feats((candidate,)):
-                f = Feature(candidate=candidate, name=feature)
-                if f not in feature_set:
-                    session.add(f)
-                    feature_set.add(f)
-
-    session.commit()
-
-
-def load_all_features(candidate_set, session):
-    """
-    Given a CandidateSet and Session, generates (Candidate, <feature name>) pairs for all Candidates in the set.
-    """
-    for x in session.query(Feature).join(Candidate).filter(Candidate.set == candidate_set):
-        yield x
 
 
 class SessionFeaturizer(object):
@@ -60,28 +28,62 @@ class SessionFeaturizer(object):
                     session.add(f)
                     feature_set.add(f)
         session.commit()
+
+    def load_feats(self, session, cand_set):
+        """Load features for this candidate set _sorting by candidate id_"""
+        for f in session.query(Feature).join(Candidate).filter(Candidate.set == cand_set).order_by(Candidate.id):
+            yield f
     
     def load_and_fit(self, session, candidate_set):
         """
-        Loads the features for a given Candidate Set and represents as a sparse matrix F,
-        where F[i,j] \in {0,1} indicates whether the ith candidate has the jth feature.
-        This method also refreshes *self.feat_index* and *self.inv_index*
+        Loads the features for a given Candidate Set and represents as a CSR sparse matrix F,
+        where F[i,j] \in {0,1} indicates whether the ith candidate has the jth feature,
+        also fitting the feature space to this candidate set, setting *self.feat_index* and *self.inv_index*.
         """
+        c_index = {}
         f_index = defaultdict(list)
-        for f, i in self.get_feats(candidates):
-            f_index[f].append(i)
+        for f in self.load_feats(session, candidate_set):
+
+            # Map the Candidate.id -> row index i
+            cid = f.candidate_id
+            if not c_index.has_key(cid):
+                c_index[cid] = len(c_index)
+            i = c_index[cid]
+
+            # Assemble temporary feature -> candidates mapping
+            f_index[f.name].append(i)
 
         # Assemble and return sparse feature matrix
         # Also assemble reverse index of feature matrix index -> feature verbose name
         self.feat_index = {}
         self.inv_index  = {}
-        F               = sparse.lil_matrix((len(candidates), len(f_index.keys())))
+        F               = sparse.lil_matrix((len(candidate_set), len(f_index.keys())))
         for j,f in enumerate(f_index.keys()):
             self.feat_index[f] = j
             self.inv_index[j]  = f
             for i in f_index[f]:
                 F[i,j] = 1
-        return F
+        return F.tocsr()
+
+    def load(self, session, candidate_set):
+        """
+        Loads the features for a given Candidate Set and represents as a CSR sparse matrix F,
+        where F[i,j] \in {0,1} indicates whether the ith candidate has the jth feature.
+        """
+        c_index = {}
+        F       = sparse.lil_matrix((len(candidate_set), len(self.feat_index.keys())))
+        for f in self.load_feats(session, candidate_set):
+
+            # Map the Candidate.id -> row index i
+            cid = f.candidate_id
+            if not c_index.has_key(cid):
+                c_index[cid] = len(c_index)
+            i = c_index[cid]
+
+            # Add to matrix
+            if self.feat_index.has_key(f.name):
+                F[i,self.feat_index[f.name]] = 1
+        return F.tocsr()
 
 
 class SpanFeaturizer(SessionFeaturizer):
@@ -94,67 +96,29 @@ class SpanFeaturizer(SessionFeaturizer):
         sent    = get_as_dict(span.context)
         xmltree = corenlp_to_xmltree(sent)
         sidxs   = range(span.get_word_start(), span.get_word_end() + 1)
+        if len(sidxs) > 0:
 
-        # Add DDLIB entity features
-        for f in get_ddlib_feats(sent, sidxs):
-            yield 'DDL_' + f
+            # Add DDLIB entity features
+            for f in get_ddlib_feats(sent, sidxs):
+                yield 'DDL_' + f
 
-        # Add TreeDLib entity features
-        for f in self.feature_generator(xmltree.root, sidxs):
-            yield 'TDL_' + f
-
-
-class Featurizer(object):
-    def __init__(self):
-        self.feat_index = None
-        self.inv_index  = None
-
-    def get_feats(self, candidates):
-        raise NotImplementedError()
-
-    def fit_transform(self, candidates):
-        """Assembles the set of features to be used, and applies this transformation to the candidates"""
-        # Assemble and return the sparse feature matrix
-        f_index = defaultdict(list)
-        for f, i in self.get_feats(candidates):
-            f_index[f].append(i)
-
-        # Assemble and return sparse feature matrix
-        # Also assemble reverse index of feature matrix index -> feature verbose name
-        self.feat_index = {}
-        self.inv_index  = {}
-        F               = sparse.lil_matrix((len(candidates), len(f_index.keys())))
-        for j,f in enumerate(f_index.keys()):
-            self.feat_index[f] = j
-            self.inv_index[j]  = f
-            for i in f_index[f]:
-                F[i,j] = 1
-        return F
-
-    def transform(self, candidates):
-        """Given feature set has already been fit, simply apply to candidates."""
-        F = sparse.lil_matrix((len(candidates), len(self.feat_index.keys())))
-        for f,i in self.get_feats(candidates):
-            if self.feat_index.has_key(f):
-                F[i,self.feat_index[f]] = 1
-        return F
+            # Add TreeDLib entity features
+            for f in self.feature_generator(xmltree.root, sidxs):
+                yield 'TDL_' + f
 
 
-class SpanPairFeaturizer(Featurizer):
+class SpanPairFeaturizer(SessionFeaturizer):
     """Featurizer for SpanPair objects"""
-
     def __init__(self):
         self.feature_generator = compile_relation_feature_generator()
         super(SpanPairFeaturizer, self).__init__()
 
-    def get_feats(self, span_pairs):
-        for i,sp in enumerate(span_pairs):
-            xmltree = corenlp_to_xmltree(get_as_dict(sp.span0.context))
-            s1_idxs = range(sp.span0.get_word_start(), sp.span0.get_word_end() + 1)
-            s2_idxs = range(sp.span1.get_word_start(), sp.span1.get_word_end() + 1)
+    def get_feats(self, span_pair):
+        xmltree = corenlp_to_xmltree(get_as_dict(span_pair.span0.context))
+        s1_idxs = range(span_pair.span0.get_word_start(), span_pair.span0.get_word_end() + 1)
+        s2_idxs = range(span_pair.span1.get_word_start(), span_pair.span1.get_word_end() + 1)
+        if len(s1_idxs) > 0 and len(s2_idxs) > 0:
 
             # Apply TDL features
             for f in self.feature_generator(xmltree.root, s1_idxs, s2_idxs):
-                yield 'TDL_' + f, i
-
-
+                yield 'TDL_' + f

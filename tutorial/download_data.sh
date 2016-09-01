727532c1b2f611e566405ab769d75005b58e7bfe
Stephen Bach <stephenhbach@gmail.com>
2016-09-01 15:12:02 -0700

    Changed to data download script.

---
+++ tutorial/download_data.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Downloading data..."
+mkdir downloads
+cd downloads
+wget http://www.biocreative.org/media/store/files/2016/CDR_Data.zip
+echo "Unzipping data..."
+unzip -qq CDR_Data.zip
+echo "Copying data to data directory..."
+cd ../
+cp downloads/CDR_Data/CDR.Corpus.v010516/CDR_TrainingSet.BioC.xml data/.
+cp downloads/CDR_Data/CDR.Corpus.v010516/CDR_DevelopmentSet.BioC.xml data/.
+cp downloads/CDR_Data/CDR.Corpus.v010516/CDR_TestSet.BioC.xml data/.
+echo "Deleting downloads directory..."
+rm -rf downloads
+echo "Done!"
+

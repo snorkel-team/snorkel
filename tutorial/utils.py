4a218b598b38d419fb3f962e9d94f2b8b3b0c313
Alex Ratner <ajratner@gmail.com>
2016-09-02 10:53:13 -0700

    External annotations loading code working in scratch form in notebook

--- tutorial/utils.py
+++ tutorial/utils.py
@@ -3,57 +3,70 @@ from snorkel.models import CandidateSet
 from snorkel.candidates import TemporarySpan
 
 
-def collect_pubtator_annotations(doc, sents, sep=" "):
-    """
-    Given a Document with PubTator/CDR annotations, and a corresponding set of Sentences,
-    extract a set of Ngram objects indexed according to **Sentence character indexing**
-    NOTE: Assume the sentences are provided in correct order & have standard separator.
-    """
-    sent_offsets = []
-    total = 0
-    for s in sents:
-        sent_offsets.append(total)
-        total += len(s.text)
+def get_docs_xml(filepath, doc_path=".//document", id_path=".//id/text()"):
+    xml = et.fromstring(open(filepath, 'rb').read())
+    return dict(zip(xml.xpath(id_path), xml.xpath(doc_path)))
 
-    # Get Ngrams
-    ngrams = []
-    annotations = et.fromstring(doc.attribs['root']).xpath('.//annotation')
+
+def get_CD_mentions_by_MESHID(doc_xml, sents):
+    """
+    Collect a set of Pubtator chemical-induced disease (CID) relation mention annotations.
+    Returns a dictionary of (sent_id, char_start, char_end) tuples indexed by MESH ID.
+    """
+    sent_offsets = [s.abs_char_offsets[0] for s in sents]
+
+    # Get unary mentions of diseases / chemicals
+    unary_mentions = {}
+    annotations = doc_xml.xpath('.//annotation')
     for a in annotations:
 
-        # Relation annotations
-        if len(a.xpath('./infon[@key="relation"]')) > 0:
+        # NOTE: Ignore CompositeRole individual mention annotations for now
+        comp_roles = a.xpath('./infon[@key="CompositeRole"]/text()')
+        comp_role = comp_roles[0] if len(comp_roles) > 0 else None
+        if comp_role == 'IndividualMention':
+            continue
 
-            # TODO: Pull these out!
-            type = a.xpath('./infon[@key="relation"]/text()')[0]
-            types = a.xpath('./infon[@key != "relation"]/@key')
-            mesh_ids = a.xpath('./infon[@key != "relation"]/text()')
-            pass
+        # Get basic annotation attributes
+        txt = a.xpath('./text/text()')[0]
+        offset = int(a.xpath('./location/@offset')[0])
+        length = int(a.xpath('./location/@length')[0])
+        type = a.xpath('./infon[@key="type"]/text()')[0]
+        mesh = a.xpath('./infon[@key="MESH"]/text()')[0]
+        
+        # Get sentence id and relative character offset
+        si = len(sent_offsets) - 1
+        for i,so in enumerate(sent_offsets):
+            if offset == so:
+                si = i
+                break
+            elif offset < so:
+                si = i - 1
+                break
+        sent       = sents[si]       
+        char_start = offset - sent_offsets[si]
+        char_end   = char_start + length - 1
+        
+        # Index by MESH ID as that is how relations refer to
+        unary_mentions[mesh] = (sent, char_start, char_end, txt)
+    return unary_mentions
 
-        # Mention annotations
-        else:
 
-            # NOTE: Ignore CompositeRole individual mention annotations for now
-            comp_roles = a.xpath('./infon[@key="CompositeRole"]/text()')
-            comp_role = comp_roles[0] if len(comp_roles) > 0 else None
-            if comp_role == 'IndividualMention':
-                continue
+def get_CID_relations(doc_xml, doc):
+    """
+    Given the doc XML and extracted unary mention tuples, return pairs of unary mentions that are annotated
+    as CID relations.
 
-            # Get basic annotation attributes
-            txt = a.xpath('./text/text()')[0]
-            offset = int(a.xpath('./location/@offset')[0])
-            length = int(a.xpath('./location/@length')[0])
-            type = a.xpath('./infon[@key="type"]/text()')[0]
-            mesh = a.xpath('./infon[@key="MESH"]/text()')[0]
-            
-            # Get sentence id and relative character offset
-            si = len(sent_offsets) - 1
-            for i,so in enumerate(sent_offsets):
-                if offset == so:
-                    si = i
-                    break
-                elif offset < so:
-                    si = i - 1
-                    break
-            ngrams.append(TemporarySpan(char_start=offset - sent_offsets[si], char_end=offset - sent_offsets[si] + length - 1, context=sents[si], meta={
-                'mesh_id': mesh, 'type': type, 'composite': comp_role}))
-    return ngrams
+    NOTE: This is somewhat ambiguous as relations are only marked at the entity level here...
+    """
+    unary_mentions = get_CD_mentions_by_MESHID(doc_xml, doc.sentences)
+    annotations    = doc_xml.xpath('.//relation')
+    for a in annotations:
+        try:
+            chemical = unary_mentions[a.xpath('./infon[@key="Chemical"]/text()')[0]]
+            disease  = unary_mentions[a.xpath('./infon[@key="Disease"]/text()')[0]]
+        except KeyError:
+            continue
+
+        # Only take relations in same sentence
+        if chemical[0] == disease[0]:
+            yield (chemical, disease)

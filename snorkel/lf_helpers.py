ac6440fc4324b0d48af2b621744012ee5a932a4c
Alex Ratner <ajratner@gmail.com>
2016-09-05 21:50:42 -0700

    Added some lf helpers

--- snorkel/lf_helpers.py
+++ snorkel/lf_helpers.py
@@ -1,18 +1,5 @@
 from .models import Span
-
-
-def get_matches(lf, candidate_set, match_values=[1,-1]):
-    """
-    A simple helper function to see how many matches (non-zero by default) an LF gets.
-    Returns the matched set, which can then be directly put into the Viewer.
-    """
-    matches = []
-    for c in candidate_set:
-        label = lf(c)
-        if label in match_values:
-            matches.append(c)
-    print "%s matches" % len(matches)
-    return matches
+from itertools import chain
 
 
 def get_text_splits(c):
@@ -52,8 +39,58 @@ def get_tagged_text(c):
 
 
 def get_text_between(c):
+    """
+    Returns the text between the two unary Spans of a binary-Span Candidate, where
+    both are in the same Sentence.
+    """
     chunks = get_text_splits(c)
     if len(chunks) == 5:
         return chunks[2]
     else:
         raise ValueError("Only applicable to binary Candidates")
+
+
+def get_left_tokens(c, window=3, attrib='words'):
+    """
+    Return the tokens within a window to the _left_ of the Candidate.
+    For higher-arity Candidates, defaults to the _first_ argument.
+    :param window: The number of tokens to the left of the first argument to return
+    :param attrib: The token attribute type (e.g. words, lemmas, poses)
+    """
+    span = c[0]
+    i    = span.get_word_start()
+    return span.parent._asdict()[attrib][max(0, i-window):i]
+
+
+def get_right_tokens(c, window=3, attrib='words'):
+    """
+    Return the tokens within a window to the _right_ of the Candidate.
+    For higher-arity Candidates, defaults to the _last_ argument.
+    :param window: The number of tokens to the right of the last argument to return
+    :param attrib: The token attribute type (e.g. words, lemmas, poses)
+    """
+    span = c[-1]
+    i    = span.get_word_end()
+    return span.parent._asdict()[attrib][i+1:i+1+window]
+
+
+def contains_token(c, tok, attrib='words'):
+    """
+    Checks if any of the contituent Spans contain a token
+    :param attrib: The token attribute type (e.g. words, lemmas, poses)
+    """
+    return tok in set(chain.from_iterable(span.get_attrib_tokens(attrib) for span in c.get_arguments()))
+
+
+def get_matches(lf, candidate_set, match_values=[1,-1]):
+    """
+    A simple helper function to see how many matches (non-zero by default) an LF gets.
+    Returns the matched set, which can then be directly put into the Viewer.
+    """
+    matches = []
+    for c in candidate_set:
+        label = lf(c)
+        if label in match_values:
+            matches.append(c)
+    print "%s matches" % len(matches)
+    return matches

17886729dd81f428655d7c747fbdd65d02764267
Alex Ratner <ajratner@gmail.com>
2016-09-05 22:45:39 -0700

    Adding data loaders

--- tutorial/utils.py
+++ tutorial/utils.py
@@ -2,6 +2,10 @@ import lxml.etree as et
 from snorkel.models import CandidateSet, split_stable_id
 from snorkel.candidates import TemporarySpan
 from collections import defaultdict
+import os
+import codecs
+import csv
+import bz2
 
 
 def get_docs_xml(filepath, doc_path=".//document", id_path=".//id/text()"):
@@ -81,3 +85,182 @@ def get_CID_relations(doc_xml, doc):
                 # Only take relations in same sentence
                 if chemical[0] == disease[0]:
                     yield (chemical, disease)
+
+
+#####################################################################
+# DICTIONARY LOADERS
+#####################################################################
+
+DICT_ROOT = os.environ['SNORKELHOME'] + '/tutorial/data/dicts/'
+
+
+def load_bioportal_csv_dictionary(filename):
+    '''BioPortal Ontologies:  http://bioportal.bioontology.org/'''
+    reader = csv.reader(open(filename,"rU"),delimiter=',', quotechar='"')
+    d = [line for line in reader]
+    
+    dictionary = {}
+    for line in d[1:]:
+        row = dict(zip(d[0],line))
+        dictionary[row["Preferred Label"]] = 1
+        dictionary.update({t:1 for t in row["Synonyms"].split("|")})
+        
+    return dictionary
+
+
+def load_disease_dictionary():  
+    dictfile = DICT_ROOT + "disease-names.v2.txt"
+    diseases = {line.strip().split("\t")[0]:1 for line in codecs.open(dictfile, 'rb',"utf-8").readlines()}
+    diseases = {word:1 for word in diseases if not word.isupper()}
+        
+    # Orphanet Rare Disease Ontology
+    ordo = load_bioportal_csv_dictionary(DICT_ROOT + "ordo.csv")
+    ordo = {word:1 for word in ordo if not word.isupper()}
+    diseases.update(ordo)
+
+    # Human Disease Ontology 
+    doid = load_bioportal_csv_dictionary(DICT_ROOT + "DOID.csv")
+    doid = {word:1 for word in doid if not word.isupper()}
+    diseases.update(doid)
+
+    # ------------------------------------------------------------
+    # remove cell dysfunction terms
+    dictfile = DICT_ROOT + "cell_molecular_dysfunction.txt"
+    terms = dict.fromkeys([line.strip().split("\t")[0] for line in open(dictfile).readlines()])
+    diseases = {word:1 for word in diseases if word not in terms} 
+
+    # remove geographic terms
+    dictfile = DICT_ROOT + "umls_geographic_areas.txt"
+    terms = dict.fromkeys([line.strip().split("\t")[0] for line in open(dictfile).readlines()])
+    diseases = {word:1 for word in diseases if word not in terms}
+
+    # ------------------------------------------------------------
+    # remove stopwords
+    dictfile = DICT_ROOT + "stopwords.txt"
+    stopwords = [line.strip().split("\t")[0] for line in open(dictfile).readlines()]
+    diseases = {word:1 for word in diseases if word.lower() not in stopwords}  
+        
+    return diseases
+
+
+def load_acronym_dictionary():     
+    dictfile = DICT_ROOT + "disease-names.v2.txt"
+    diseases = {line.strip().split("\t")[0]:1 for line in codecs.open(dictfile, 'rb', "utf-8").readlines()}
+    diseases = {word:1 for word in diseases if word.isupper()}
+        
+    # Orphanet Rare Disease Ontology
+    ordo = load_bioportal_csv_dictionary(DICT_ROOT + "ordo.csv")
+    ordo = {word:1 for word in ordo if word.isupper()}
+    diseases.update(ordo)
+        
+    # Human Disease Ontology 
+    doid = load_bioportal_csv_dictionary(DICT_ROOT + "DOID.csv")
+    doid = {word:1 for word in doid if word.isupper()}
+    diseases.update(doid)
+        
+    # filter by char length
+    diseases = {word:1 for word in diseases if len(word) > 1}
+        
+    return diseases
+
+
+def load_molecular_dysfunction():
+    dictfile = DICT_ROOT + "cell_molecular_dysfunction.txt"
+    terms = dict.fromkeys([line.strip().split("\t")[0] for line in open(dictfile).readlines()])
+    return terms
+
+
+def load_syndromes(): 
+    return {t.strip().lower():1 for t in codecs.open(DICT_ROOT + "syndromes.txt","rU",'utf-8').readlines()}
+
+
+def load_proteins_enzymes_genes():
+    return {t.strip().lower():1 for t in codecs.open(DICT_ROOT + "all.proteins_enzymes.txt","rU",'utf-8').readlines()}
+
+
+def load_chemdner_dictionary():
+    dict_fnames = [DICT_ROOT + "mention_chemical.txt",
+                   DICT_ROOT + "chebi.txt",
+                  DICT_ROOT + "addition.txt",
+                 DICT_ROOT + "train.chemdner.vocab.txt"]
+    chemicals = []
+    for fname in dict_fnames:
+        chemicals += [line.strip().split("\t")[0] for line in codecs.open(fname,"rU","utf-8").readlines()]
+
+    # load bzip files
+    fname = DICT_ROOT + "substance-sab-all.bz2"
+    chemicals += {line.strip().split("\t")[0]:1 for line in bz2.BZ2File(fname, 'rb').readlines()}.keys()
+
+    # remove stopwords
+    fname = DICT_ROOT + "stopwords.txt"
+    stopwords = {line.strip().split("\t")[0]:1 for line in open(fname,"rU").readlines()}
+    chemicals = {term:1 for term in chemicals if term not in stopwords}
+    return chemicals
+
+
+def load_specialist_abbreviations():
+    '''
+    Load UMLS SPECIALIST Lexicon of abbreviations. Format:
+    E0000048|AA|acronym|E0006859|achievement age|
+    '''
+    fpath = DICT_ROOT + "SPECIALIST.bz2"
+    d = [line.strip().strip("|").split("|") for line in bz2.BZ2File(fpath, 'rb').readlines()]
+    abbrv2text,text2abbrv =  {},{}
+    for row in d:
+        uid1,abbrv,atype,uid2,text = row
+        text = text.lower()
+        if atype not in ["acronym","abbreviation"]:
+            continue
+        if abbrv not in abbrv2text:
+            abbrv2text[abbrv] = {}
+        if text not in text2abbrv:
+            text2abbrv[text] = {}
+
+        abbrv2text[abbrv][text] = 1
+        text2abbrv[text][abbrv] = 1
+
+    return abbrv2text,text2abbrv
+
+
+def load_umls_dictionary():
+    umls_dict = {}
+    dict_fnames = [DICT_ROOT + "snomedct.disease_or_syndrome.txt",
+    DICT_ROOT + "snomedct.sign_or_symptom.txt",
+    DICT_ROOT + "snomedct.finding.txt",
+    DICT_ROOT + "mesh.disease_or_syndrome.txt",
+    DICT_ROOT + "mesh.sign_or_symptom.txt"]
+
+    for fname in dict_fnames:
+        sab,sty = fname.split("/")[-1].split(".")[:2]
+        if sab not in umls_dict:
+            umls_dict[sab] = {}
+        if sty not in umls_dict[sab]:
+            umls_dict[sab][sty] = {}
+        umls_dict[sab][sty] = {line.strip().split("\t")[0]:1 for line in codecs.open(fname, 'rU',"utf-8").readlines()}
+
+    return umls_dict
+
+
+def load_ctd_dictionary(filename, ignore_case=True):
+    '''Comparative Toxicogenomics Database'''
+    d = {}
+    header = ['DiseaseName', 'DiseaseID', 'AltDiseaseIDs', 'Definition', 
+              'ParentIDs', 'TreeNumbers', 'ParentTreeNumbers', 'Synonyms', 
+              'SlimMappings']
+        
+    synonyms = {}
+    dnames = {}
+    with codecs.open(filename,"rU","utf-8",errors="ignore") as fp:
+        for i,line in enumerate(fp):
+            line = line.strip()
+            if line[0] == "#":
+                continue
+            row = line.split("\t")
+            if len(row) != 9:
+                continue
+            row = dict(zip(header,row))
+             
+            synonyms.update( dict.fromkeys(row["Synonyms"].strip().split("|")))
+            dnames[row["DiseaseName"].strip()] = 1
+    
+    return {term.lower() if ignore_case else term:1 for term in synonyms.keys()+dnames.keys() if term}

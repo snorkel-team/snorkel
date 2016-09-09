20c4aec7a810cdefcfb11a521ae7121d516a6b85
Stephen Bach <stephenhbach@gmail.com>
2016-09-09 15:26:38 -0700

    Viewer makes an AnnotationKeySet for a user in addition to a key.

--- snorkel/viewer.py
+++ snorkel/viewer.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from .models import Label, AnnotationKey
+from .models import Label, AnnotationKey, AnnotationKeySet
 try:
     from IPython.core.display import display, Javascript
 except:
@@ -27,19 +27,8 @@ PAGE_HTML = u"""
 
 
 class Viewer(widgets.DOMWidget):
-    # TODO: Update this docstring
     """
-    Generic object for viewing and labeling candidate objects in their rendered contexts.
-    Takes in:
-        - A list of *contexts* (e.g. Sentence objects) having an id attribute;
-        - A list of *candidates( (e.g. Ngram mention spans), having a join attribute fn
-            (candidate_join_key_fn) such that contexts and candidates are joined on
-            context.id == candidate_join_key_fn(candidate)
-        - Optionally: a list of *gold annotations* of the same type as the candidates
-        - A max number of contexts to render (n_max)
-    By default, contexts with no candidates or gold annotations are filtered out, however
-    this can be disabled (filter_empty) and any filtering can be done prior to passing into
-    the Viewer object!
+    Generic object for viewing and labeling Candidate objects in their rendered Contexts.
     """
     _view_name         = Unicode('ViewerView').tag(sync=True)
     _view_module       = Unicode('viewer').tag(sync=True)
@@ -49,18 +38,51 @@ class Viewer(widgets.DOMWidget):
     _selected_cid      = Int().tag(sync=True)
 
     def __init__(self, candidates, session, gold=[], n_per_page=3, height=225, annotator_name=None):
+        """
+        Initializes a Viewer.
+
+        The Viewer uses the keyword argument annotator_name to define an AnnotationKeySet with that name, containing
+        a single AnnotationKey of the same name. If it already exists, it will be resused, but if the AnnotationKeySet
+        already exists and contains other keys, the Viewer will raise an error.
+
+        This AnnotationKeySet can be used to retrieve the labels with a LabelManager, and corresponding AnnotationKeys
+        can be grouped into a new AnnotationKeySet to manage the work of multiple annotators simultaneously.
+
+        :param candidates: A Python container of Candidates (e.g., not a CandidateSet, but candidate_set.candidates)
+        :param session: The SnorkelSession for the database backend
+        :param gold: Optional, Python container of Candidates that are know to have positive labels
+        :param n_per_page: Optional, number of Contexts to display per page
+        :param height: Optional, the height in pixels of the Viewer
+        :param annotator_name: Name of the human using the Viewer, for saving their work. Defaults to system username.
+        """
         super(Viewer, self).__init__()
         self.session = session
 
         # By default, use the username as annotator name
         name = annotator_name if annotator_name is not None else getpass.getuser()
 
-        # Gets or creates annotator record
-        self.annotator = self.session.query(AnnotationKey).filter(AnnotationKey.name == name).first()
-        if self.annotator is None:
-            self.annotator = AnnotationKey(name=name)
-            session.add(self.annotator)
+        # Sets up the AnnotationKeySet and AnnotationKey
+        key_set = self.session.query(AnnotationKeySet).filter(AnnotationKey.name == name).first()
+        if key_set is None:
+            key_set = AnnotationKeySet(name=name)
+            session.add(key_set)
             session.commit()
+
+        if len(key_set) > 1:
+            raise ValueError('AnnotationKeySet with name ' + unicode(name) + ' already exists and has more than one ' +
+                             'AnnotationKey. Please specify a new annotator_name.')
+        elif len(key_set) == 1:
+            if key_set.keys[0].name == name:
+                self.annotator = key_set.keys[0]
+            else:
+                raise ValueError('AnnotationKeySet named ' + unicode(name) + ' already exists and has one ' +
+                                 'AnnotationKey with a different name. Please specify a new annotator_name.')
+        else:
+            self.annotator = self.session.query(AnnotationKey).filter(AnnotationKey.name == name).first()
+            if self.annotator is None:
+                self.annotator = AnnotationKey(name=name)
+                session.add(self.annotator)
+                session.commit()
 
         # Viewer display configs
         self.n_per_page = n_per_page

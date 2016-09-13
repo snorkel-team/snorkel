f0f92df6fde456ea9eb6e4be0db71c21a809b12d
Braden Hancock <hancock.braden@gmail.com>
2016-09-12 17:51:14 -0700

    Add hardware_utils scoring function

--- tutorials/tables/hardware_utils.py
+++ tutorials/tables/hardware_utils.py
@@ -29,35 +29,50 @@ def load_extended_parts_dict(filename):
     return gold_parts_extended
 
 
-def load_hardware_labels(loader, candidates, filename, type, attrib):
+def get_gold_dict(filename, attrib, docs=None):
     with codecs.open(filename, encoding="utf-8") as csvfile:
         gold_reader = csv.reader(csvfile)
         gold_dict = defaultdict(int)
         for row in gold_reader:
             (doc, part, val, attr) = row
-            if attr == attrib:
-                key = (doc.upper(), part.upper(), val.upper())
-                gold_dict[key] += 1
+            if docs is None or doc.upper() in docs:
+                if attr == attrib:
+                    key = (doc.upper(), part.upper(), val.upper())
+                    gold_dict[key] += 1
+    return gold_dict
+
+def load_hardware_labels(loader, candidates, filename, attrib, attrib_class):
+    gold_dict = get_gold_dict(filename, attrib)
     
     for c in candidates:
         key = ((c[0].parent.document.name).upper(), (c[0].get_span()).upper(), (''.join(c[1].get_span().split())).upper())
         if key in gold_dict:
-            # TODO: fix this hard coding
-            loader.add({'part' : c[0], 'temp' : c[1]})
+            loader.add({'part' : c[0], attrib_class : c[1]})
 
-
-### TODO: DELETE ME ###
-# def load_hardware_labels(loader, file_name):
-#     # Get all the annotated Pubtator documents as XML trees
-#     doc_xmls = get_docs_xml(ROOT + file_name)
-#     for doc_id, doc_xml in doc_xmls.iteritems():
+def entity_level_f1(tp, fp, tn, fn, filename, corpus, attrib):
+    docs = []
+    for doc in corpus:
+        docs.append((doc.name).upper())
+    gold_dict = get_gold_dict(filename, attrib, docs)
     
-#         # Get the corresponding Document object
-#         stable_id = "%s::document:0:0" % doc_id
-#         doc       = session.query(Document).filter(Document.stable_id == stable_id).first()
-#         if doc is not None:
-        
-#             # Use custom script + loader to add
-#             for d in get_CID_unary_mentions(doc_xml, doc, 'Disease'):
-#                 # d is a dictionary with {'part_temp': TemporarySpan}
-#                 loader.add(d)
+    TP = FP = TN = FN = 0
+    for c in tp.union(fp):
+        key = ((c[0].parent.document.name).upper(), (c[0].get_span()).upper(), (''.join(c[1].get_span().split())).upper())
+        if key in gold_dict:
+            TP += 1
+        else:
+            FP += 1
+    FN = len(gold_dict) - TP
+
+    prec = TP / float(TP + FP) if TP + FP > 0 else float('nan')
+    rec  = TP / float(TP + FN) if TP + FN > 0 else float('nan')
+    f1   = 2 * (prec * rec) / (prec + rec) if prec + rec > 0 else float('nan')
+    print "========================================"
+    print "Scoring on Entity-Level Gold Data"
+    print "========================================"
+    print "Corpus Precision {:.3}".format(prec)
+    print "Corpus Recall    {:.3}".format(rec)
+    print "Corpus F1        {:.3}".format(f1)
+    print "----------------------------------------"
+    print "TP: {} | FP: {} | FN: {}".format(TP, FP, FN)
+    print "========================================\n"

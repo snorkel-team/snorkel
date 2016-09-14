1175601835252b46ecfa22a8173833aa750599c6
Braden Hancock <hancock.braden@gmail.com>
2016-09-13 17:36:30 -0700

    Allow train/dev/test to be 0 if specified

--- snorkel/utils.py
+++ snorkel/utils.py
@@ -147,23 +147,26 @@ def split_corpus(session, corpus, train=0.8, development=0.1, test=0.1, seed=Non
     num_development = int(development * n)
     num_test = n - (train + development)
 
-    train_corpus = Corpus(name=corpus.name + ' Training')
-    for doc in docs[:num_train]:
-        train_corpus.append(doc)
-    session.add(train_corpus)
-    print "%d Documents added to corpus %s" % (len(train_corpus), train_corpus.name)
+    if num_train > 0:
+        train_corpus = Corpus(name=corpus.name + ' Training')
+        for doc in docs[:num_train]:
+            train_corpus.append(doc)
+        session.add(train_corpus)
+        print "%d Documents added to corpus %s" % (len(train_corpus), train_corpus.name)
 
-    development_corpus = Corpus(name=corpus.name + ' Development')
-    for doc in docs[num_train:num_train + num_development]:
-        development_corpus.append(doc)
-    session.add(development_corpus)
-    print "%d Documents added to corpus %s" % (len(development_corpus), development_corpus.name)
+    if num_development > 0:
+        development_corpus = Corpus(name=corpus.name + ' Development')
+        for doc in docs[num_train:num_train + num_development]:
+            development_corpus.append(doc)
+        session.add(development_corpus)
+        print "%d Documents added to corpus %s" % (len(development_corpus), development_corpus.name)
 
-    test_corpus = Corpus(name=corpus.name + ' Test')
-    for doc in docs[num_train + num_development:]:
-        test_corpus.append(doc)
-    session.add(test_corpus)
-    print "%d Documents added to corpus %s" % (len(test_corpus), test_corpus.name)
+    if num_test > 0:
+        test_corpus = Corpus(name=corpus.name + ' Test')
+        for doc in docs[num_train + num_development:]:
+            test_corpus.append(doc)
+        session.add(test_corpus)
+        print "%d Documents added to corpus %s" % (len(test_corpus), test_corpus.name)
 
     session.commit()
 

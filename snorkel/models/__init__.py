8a35b2433b87104c94001c5baf0ccc7256b00f42
Stephen Bach <stephenhbach@gmail.com>
2016-09-21 15:39:56 -0400

    Doc formatting.

--- snorkel/models/__init__.py
+++ snorkel/models/__init__.py
@@ -10,24 +10,25 @@ The only exception is importing SnorkelBase or other classes in order to extend 
 To ensure that any additional data models are included in the storage backend, these must be imported
 and the extending subtypes defined before importing `snorkel.models`. For example, the correct way to
 define a new type of Context is
->>> from snorkel.models.context import Context
->>> from sqlalchemy import Column, String, ForeignKey
->>>
->>> class NewType(Context):
->>>     # Declares name for storage table
->>>     __tablename__ = 'newtype'
->>>     # Connects NewType records to generic Context records
->>>     id = Column(String, ForeignKey('context.id'))
->>>
->>>     # Polymorphism information for SQLAlchemy
->>>     __mapper_args__ = {
->>>         'polymorphic_identity': 'newtype',
->>>     }
->>>
->>>     # Rest of class definition here
->>>
->>> # The entire storage schema, including NewType, can now be initialized with the following import
->>> import snorkel.models
+.. code-block::
+    from snorkel.models.context import Context
+    from sqlalchemy import Column, String, ForeignKey
+
+    class NewType(Context):
+        # Declares name for storage table
+        __tablename__ = 'newtype'
+        # Connects NewType records to generic Context records
+        id = Column(String, ForeignKey('context.id'))
+
+        # Polymorphism information for SQLAlchemy
+        __mapper_args__ = {
+            'polymorphic_identity': 'newtype',
+        }
+
+        # Rest of class definition here
+
+    # The entire storage schema, including NewType, can now be initialized with the following import
+    import snorkel.models
 """
 from .meta import SnorkelBase, SnorkelSession, snorkel_engine, snorkel_postgres
 from .context import Context, Corpus, Document, Sentence, TemporarySpan, Span

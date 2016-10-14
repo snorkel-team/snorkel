26167697f4bc0bb2500fa8a5b57c0e3db7996686
Stephen Bach <stephenhbach@gmail.com>
2016-10-14 07:54:37 -0700

    Added missing package __init__ imports.

--- snorkel/learning/__init__.py
+++ snorkel/learning/__init__.py
@@ -3,5 +3,5 @@ Subpackage for Snorkel machine learning modules.
 """
 from .disc_learning import NoiseAwareModel, LogReg, LogRegSKLearn
 from .gen_learning import NaiveBayes, GenerativeModel, DEP_SIMILAR, DEP_FIXING, DEP_REINFORCING, DEP_EXCLUSIVE
-from .param_search import GridSearch, RandomSearch
+from .param_search import GridSearch, RandomSearch, ListParameter, RangeParameter
 from .utils import *

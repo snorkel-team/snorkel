ae49afa9a82cbb9438f01e9e485b0ef0345fcf30
Alex Ratner <ajratner@gmail.com>
2016-10-21 16:43:51 -0700

    Adding disease norm nbs

---
+++ snorkel/learning/learning_mn.py
@@ -0,0 +1,187 @@
+import numpy as np
+import scipy.sparse as sparse
+from scipy.optimize import minimize
+import warnings
+from learning_utils import sparse_abs
+from lstm import LSTMModel
+from sklearn import linear_model
+
+DEFAULT_MU = 1e-6
+DEFAULT_RATE = 0.01
+DEFAULT_ALPHA = 0.5
+
+def log_odds(p):
+    """This is the logit function"""
+    return np.log(p / (1.0 - p))
+
+def odds_to_prob(l):
+    """
+      This is the inverse logit function logit^{-1}:
+
+    l       = \log\frac{p}{1-p}
+    \exp(l) = \frac{p}{1-p}
+    p       = \frac{\exp(l)}{1 + \exp(l)}
+    """
+    return np.exp(l) / (1.0 + np.exp(l))
+
+
+def exact_marginals_single_candidate(X, w):
+    """
+    This function computes the marginal probabilities of each class (of D classes) for **a single candidate**
+    with true class Y; the output is a D-dim vector of these probabilities.
+
+    Here, X is a M x D matrix, where M is the number of LFs, and D is the number of possible; 
+    the ith row of X corresponds to the *distribution* of the LF's "vote" across possible values.
+    This is just the softmax:
+
+    P(Y=k | X; w) = exp(w^T X[:, k]) / \sum_l ( exp(w^T X[:, l]) )
+    
+    Our default convention is that if, for example, the ith LF votes only negatively on a value j
+    of this candidate, then this would be expressed as having uniform distribution over all cols. except j.
+    in row i of X.
+    """
+    z = np.exp(np.dot(w.T, X))
+    return z / z.sum()
+
+
+def compute_lf_accs(Xs, w):
+    """
+    This function computes the expected accuracies of each of the M LFs, outputting a M-dim vector.
+
+    Here, Xs is a **list** of matrices X, as defined in exact_marginals_single_candidate;
+    note that each X has the same number of rows (M), but varying number of columns.
+    
+    E[ accuracy of LF i ] =
+    """
+    M      = Xs[0].shape[0]
+    accs   = np.zeros((M,1))
+    n_pred = np.zeros(M)
+
+    w = w.reshape(-1,1)
+
+    # Iterate over the different LF distribution matrices for each candidate
+    for i, X in enumerate(Xs):
+        
+        # Get the predicted class distribution for the candidate
+        z = exact_marginals_single_candidate(X, w)
+
+        # Get the expected accuracy of the LFs for this candidate
+        # TODO: Check this...
+        accs += np.dot(X,z.T) / np.linalg.norm(z) # M X D * D
+
+        # Add whether there was a prediction made or not
+        # TODO: Check this...
+        n_pred += X.sum(1) #summing across rows 0/1
+
+    p_correct = (1. / (n_pred + 1e-8)).reshape(-1,1) * accs
+    return p_correct, n_pred
+
+
+class NoiseAwareModel(object):
+    """Simple abstract base class for a model."""
+    def __init__(self):
+        pass
+
+    def train(self, X, training_marginals=None, **hyperparams):
+        raise NotImplementedError()
+
+    def marginals(self, X):
+        raise NotImplementedError()
+
+    def predict(self, X, thresh=0.5):
+        """Return numpy array of elements in {-1,0,1} based on predicted marginal probabilities."""
+        return np.array([1 if p > thresh else -1 if p < thresh else 0 for p in self.marginals(X)])
+
+
+class LogReg(NoiseAwareModel):
+    """Logistic regression."""
+    def __init__(self, bias_term=False):
+        self.w         = None
+        self.bias_term = bias_term
+
+    def train(self, Xs, n_iter=1000, w0=None, rate=DEFAULT_RATE, alpha=DEFAULT_ALPHA, \
+            mu=DEFAULT_MU, tol=1e-6, verbose=True):
+        """
+        Xs is defined as in compute_lf_accs.
+
+        Perform SGD wrt the weights w
+        * n_iter:      Number of steps of SGD
+        * w0:          Initial value for weights w
+        * rate:        I.e. the SGD step size
+        * alpha:       Elastic net penalty mixing parameter (0=ridge, 1=lasso)
+        * mu:          Elastic net penalty
+        * tol:         For testing for SGD convergence, i.e. stopping threshold
+        """
+        # Set up stuff
+        N  = len(Xs)
+        M  = Xs[0].shape[0]
+        w0 = w0 if w0 is not None else np.zeros(M)
+
+        # Initialize training
+        w = w0.copy()
+        g = np.zeros(M)
+        l = np.zeros(M)
+        g_size = 0
+
+        # Gradient descent
+        if verbose:
+            print "Begin training for rate={}, mu={}".format(rate, mu)
+        for step in range(n_iter):
+
+            # Get the expected LF accuracies
+            p_correct, n_pred = compute_lf_accs(Xs, w)
+
+            # Get the "empirical log odds"; NB: this assumes one is correct, clamp is for sampling...
+            l = np.clip(log_odds(p_correct), -10, 10).flatten()
+
+            # SGD step with normalization by the number of samples
+            g0 = (n_pred * (w - l)) / np.sum(n_pred)
+
+            # Momentum term for faster training
+            g = 0.95*g0 + 0.05*g
+
+            # Check for convergence
+            wn     = np.linalg.norm(w, ord=2)
+            g_size = np.linalg.norm(g, ord=2)
+            if step % 250 == 0 and verbose:
+                print "\tLearning epoch = {}\tGradient mag. = {:.6f}".format(step, g_size)
+            if (wn < 1e-12 or g_size / wn < tol) and step >= 10:
+                if verbose:
+                    print "SGD converged for mu={} after {} steps".format(mu, step)
+                break
+
+            #print "w", w.shape
+            #print "g", g.shape
+            #print rate
+
+            #print w
+            #print g
+
+            #print "!!!!!",rate * g
+
+            # Update weights
+            w -= rate * g
+
+
+            # Apply elastic net penalty
+            w_bias    = w[-1]
+            soft      = np.abs(w) - rate * alpha * mu
+            ridge_pen = (1 + (1-alpha) * rate * mu)
+
+            #          \ell_1 penalty by soft thresholding        |  \ell_2 penalty
+            w = (np.sign(w)*np.select([soft>0], [soft], default=0)) / ridge_pen
+
+            # Don't regularize the bias term
+            if self.bias_term:
+                w[-1] = w_bias
+
+        # SGD did not converge
+        else:
+            if verbose:
+                print "Final gradient magnitude for rate={}, mu={}: {:.3f}".format(rate, mu, g_size)
+
+        # Return learned weights
+        self.w = w
+
+    def marginals(self, Xs):
+        return [exact_marginals_single_candidate(X, self.w) for X in Xs]
---
+++ tutorials/disease_norm/entity_norm.py
@@ -0,0 +1,244 @@
+from collections import defaultdict
+import sys
+import re
+import numpy as np
+from scipy.sparse import lil_matrix, identity
+from sklearn.preprocessing import normalize
+import random
+from nltk.stem.porter import PorterStemmer
+
+
+DEFAULT_IDF = 1.0
+
+class Vectorizer(object):
+    """
+    Generic class for vectorizing;by default, initializes over a list of phrases, and does not TF-IDF weighting
+    """
+    def __init__(self, phrases, other_phrases=None, stem=True, stopwords=set(), rgx_filter=r'\w+', min_len=1):
+        self.stem = stem
+        if self.stem:
+            self.stemmer = PorterStemmer()
+        self.stopwords  = stopwords
+        self.rgx_filter = rgx_filter
+        self.min_len    = min_len
+
+        self.word_index     = {}
+        self.inv_word_index = {}
+        self.word_idf       = defaultdict(lambda : DEFAULT_IDF)
+        self.word_to_cids   = defaultdict(set)
+
+        self._compile_dicts(phrases, other_phrases)
+
+    def _compile_dicts(self, phrases, other_phrases):
+
+        # Process the canonical dictionary cd
+        for phrase in phrases:
+            for word in self._split(phrase):
+
+                # Add to word index
+                if word not in self.word_index:
+                    self.word_index[word] = len(self.word_index)
+
+        # Construct inverted word index
+        for word, i in self.word_index.iteritems():
+            self.inv_word_index[i] = word
+
+        print "|V| = %s" % len(self.word_index)
+
+    def vectorize_phrases(self, phrases):
+        """
+        Transform a list of N phrases into a N x |V|+1 row-normalized sparse matrix
+        where each row represents a TF-IDF vectorized phrase.
+        NOTE: Reserve the last column for "UNK", i.e. OOV words
+        """
+        V = len(self.word_index)
+        X = lil_matrix((len(phrases), V + 1))
+        for i, phrase in enumerate(phrases):
+            for word in self._split(phrase):
+                j = self.word_index[word] if word in self.word_index else V
+                s = self.word_idf[word] if word in self.word_idf else DEFAULT_IDF
+                X[i, j] += s
+        X = X.tocsr()
+        return normalize(X)
+    
+    def _split(self, phrase):
+        for w in re.findall(self.rgx_filter, phrase.lower()):
+            if w not in self.stopwords and len(w) > self.min_len:
+                yield self.stemmer.stem(w) if self.stem else w
+
+
+class TFIDFVectorizer(Vectorizer):
+    """Basic TF_IDF extension"""
+    def _compile_dicts(self, phrases, other_phrases):
+        word_to_phrases = defaultdict(set)
+        for i, phrase in enumerate(phrases):
+            for word in self._split(phrase):
+
+                # Add to word index
+                if word not in self.word_index:
+                    self.word_index[word] = len(self.word_index)
+
+                word_to_phrases[word].add(i)
+
+        # Compute word IDF scores **based on canonical ID not term counts!**
+        N = len(phrases)
+        for word in self.word_index.keys():
+            self.word_idf[word] = np.log( N / float(len(word_to_phrases[word])) )
+
+        for phrase in other_phrases:
+            for word in self._split(phrase):
+                if word not in self.word_index:
+                    self.word_index[word] = len(self.word_index)
+
+                    # TODO: Should IDF = 0.0 by default?
+                    self.word_idf[word] = 0.0
+
+        # Construct inverted word index
+        for word, i in self.word_index.iteritems():
+            self.inv_word_index[i] = word
+
+        print "|V| = %s" % len(self.word_index)
+
+class CanonDictVectorizer(Vectorizer):
+    """
+    Class for vectorizing phrases using TF-IDF scheme based on a _canonical dictionary_.
+    The canonical dictionary has a set of _terms_ which each map to a _canonical ID_;
+    IDF is computed **at the canonical ID level**.
+    """
+    def _compile_dicts(self, cd, other_phrases):
+        
+        # Process the canonical dictionary cd
+        cids = set()
+        for term, cid in cd.iteritems():
+            cids.add(cid)
+            for word in self._split(term):
+
+                # Add to word index
+                if word not in self.word_index:
+                    self.word_index[word] = len(self.word_index)
+
+                # Add cid to word_to_cid dict
+                self.word_to_cids[word].add(cid)
+
+        # Compute word IDF scores **based on canonical ID not term counts!**
+        N = len(cids)
+        for word in self.word_index.keys():
+            self.word_idf[word] = np.log( N / float(len(self.word_to_cids[word])) )
+
+        # Add other OOD phrases, e.g. from training set
+        # TODO: How should these be weighted?
+        for phrase in other_phrases:
+            for word in self._split(phrase):
+                if word not in self.word_index:
+                    self.word_idf[word]   = DEFAULT_IDF
+                    self.word_index[word] = len(self.word_index)
+
+        # Construct inverted word index
+        for word, i in self.word_index.iteritems():
+            self.inv_word_index[i] = word
+
+        print "|V| = %s" % len(self.word_index)
+
+
+class SSIModel(object):
+    """
+    Given:
+        * X, an N x V phrase matrix
+        * y, a corresponding N-length list of gold CIDs
+        * D, an M x V dictionary term matrix
+        * d, a corresponding M-length list of CIDs
+    Run SGD to learn the matrix of weights W
+
+    Just initialize with (D, d); then use self.train(X,y)
+    """
+    def __init__(self, D, d):
+        self.D = D
+        self.d = d
+        
+        self.cids = frozenset(d)
+        self.V    = D.shape[1]
+        
+        # Index the dictionary terms (D) by cid (d)
+        self.cid_to_rows = defaultdict(list)
+        for i, cid in enumerate(self.d):
+            self.cid_to_rows[cid].append(i)
+    
+    def train(self, X, y, rate=1e-3, n_iter=3, n_iter_neg=10, sample_close_negs=True, verbose=True):
+        """
+        If sample_close_negs = True, negative terms will be sampled only from "close"
+        terms to the phrase, i.e. with _some_ direct word overlap.
+        """
+        N = X.shape[0]
+
+        # Build overlap matrix
+        if sample_close_negs:
+            print "Building close negatives dictionary..."
+            M          = X * self.D.T
+            close_negs = {}
+            for i in range(N):
+                close_negs[i] = set([self.d[j] for j in M.getrow(i).nonzero()[1]]).difference([y[i]])
+
+        # Initialize W = I
+        W = identity(self.V, format='csr')
+
+        # Cosine similarity term
+        b = 1.0
+
+        # Run SGD
+        for it in range(n_iter):
+            sys.stdout.write("\rIteration: %s" % it)
+
+            # Iterate in random order through the training examples
+            run = range(N)
+            random.shuffle(run)
+            for i in run:
+
+                # Randomly pick a tuple (x, t^+, t^-)
+                # First pick x, a random training phrase
+                x = X.getrow(i)
+
+                # NOTE: Skip OOD training examples!
+                cid = y[i]
+                if cid not in self.cid_to_rows:
+                    continue
+
+                # Next pick tp, a random dictionary term which maps to the correct CID for x
+                j  = random.choice(self.cid_to_rows[cid])
+                tp = self.D.getrow(j)
+
+                # Next pick tp, a random dictionary term which maps to an *incorrect* CID for x
+                # Take multiple samples for this training example
+                neg_cids = close_negs[i] if sample_close_negs and len(close_negs[i]) > 0 else self.cids.difference([cid])
+                for itn in range(n_iter_neg):
+                    cid_neg  = random.sample(neg_cids, 1)[0]
+                    k        = random.choice(self.cid_to_rows[cid_neg])
+                    tn       = self.D.getrow(k)
+
+                    # Take gradient step
+                    if 1 - (x * W * tp.T)[0,0] - b*(x * tp.T)[0,0] + (x * W * tn.T)[0,0] + b*(x * tn.T)[0,0] > 0:
+                        W = W + rate * ( x.T * tp - x.T * tn )
+                        b = b + rate * ( (x * tp.T)[0,0] - (x * tn.T)[0,0])
+        print "\n"
+        self.W = W
+        self.b = b
+
+    def predict(self, x, W=None, b=None):
+        """
+        Predict the MESH ID for a TF-IDF vectorized phrase
+        Note: A custom weight matrix can be substituted in; e.g. W=I is TF-IDF baseline.
+        """
+        W = self.W if W is None else W
+        b = self.b if b is None else b
+
+        s = x * W * self.D.T + self.b * x * self.D.T
+
+        if s.nnz == 0:
+            return None
+
+        # Note: We convert to COO format here so as to to do an OM-more efficient
+        # argmax over the raw data
+        s = s.tocoo()
+        if s.data.max() > 0.0:
+            return self.d[s.col[s.data.argmax()]]
+        else:
+            return None

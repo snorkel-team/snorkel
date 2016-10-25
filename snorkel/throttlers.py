36e796be713817be311751209c3d71cba913b95e
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2016-10-25 16:16:42 -0700

    SeparatingSpanThrottler

--- snorkel/throttlers.py
+++ snorkel/throttlers.py
@@ -31,7 +31,7 @@ class AlignmentThrottler(Throttler):
                 or span0.parent.cell.col.position == span1.parent.cell.col.position
 
     def _apply_infer(self, span0, span1):
-        # TODO: this needs to be made symmetric
+        # TODO: this needs to be made symmetric, or the induced span needs to be specified
         if self.axis in ('row', 'col'):
             aligned_cells = get_aligned_cells(span0.parent.cell, self.axis, infer=True)
         if self.axis is None:
@@ -39,8 +39,46 @@ class AlignmentThrottler(Throttler):
                           + get_aligned_cells(span0.parent.cell, 'col', infer=True)
         return span1.parent.cell in aligned_cells
 
-# def HeaderSpanThrottler(Throttler):
-#     """Filters spans that are not
+class SeparatingSpanThrottler(Throttler):
+    """Filter spans that are separated by spanning cells
+
+    We look for all the cells in between and check if any of them span the table.
+    """
+    def __init__(self, align_axis):
+        if align_axis not in ('row', 'col'): raise ValueError('Invalid axis: %s' % align_axis)
+        self.align_axis = align_axis
+
+    def __call__(self, argtuple):
+        # get the cells associated with each span
+        span0, span1 = argtuple
+        cell0, cell1 = span0.parent.cell, span1.parent.cell
+
+        # figure out which one comes first and which ones comes second
+        ax = 'row' if self.align_axis == 'col' else 'row'
+        top_cell = cell0 if getattr(cell0, ax).position < getattr(cell1, ax).position else cell1
+        bot_cell = cell0 if getattr(cell0, ax).position > getattr(cell1, ax).position else cell1
+        
+        # get the set of middle cells
+        min_ax = getattr(top_cell, ax).position
+        max_ax = getattr(bot_cell, ax).position
+        middle_cells = [ c for c in span0.parent.table.cells 
+                         if min_ax < getattr(c, ax).position < max_ax ]
+        if not middle_cells: return False
+
+        # if the two spans are separated by a spanning cell, we skip this pair
+        sp_ax = 'row' if ax == 'col' else 'row'
+        def _spans(cell, table, axis):
+            if axis == 'row' and len([c for c in table.cells if c.row == cell.row]) > 1:
+                return False
+            if axis == 'col' and len([c for c in table.cells if c.col == cell.col]) > 1:
+                return False
+            return True
+
+        # print '!!!', span0, span0.parent.cell.row.position, span0.parent.cell.col.position, span1, span1.parent.cell.row.position, span1.parent.cell.col.position
+        # for c in middle_cells:
+        #     print c.text, c.row.position, c.col.position, len([d for d in span0.parent.table.cells if c.row == d.row]), _spans(c, span0.parent.table, sp_ax)
+                             
+        return False if any(_spans(c, span0.parent.table, sp_ax) for c in middle_cells) else True
 
 # Reference derivative class (actual copy stored in hardware_utils.py)
 # class PartThrottler(Throttler):

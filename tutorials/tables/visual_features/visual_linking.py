9ebd05f9b267082527cb660195bdf8f4288ed75e
Ines Chami <ineschami@DN51q043.SUNet>
2016-10-26 22:00:44 -0700

    added diplay boxes function in visual_linking.py

--- tutorials/tables/visual_features/visual_linking.py
+++ tutorials/tables/visual_features/visual_linking.py
@@ -1,6 +1,9 @@
 import numpy as np
 from collections import OrderedDict
-from editdistance import eval as editdist
+#from editdistance import eval as editdist
+import subprocess
+import os
+import cv2
 
 def calculate_offset(listA, listB, seedSize, maxOffset):
     wordsA = zip(*listA[:seedSize])[1]
@@ -14,13 +17,29 @@ def calculate_offset(listA, listB, seedSize, maxOffset):
     return int(np.median(offsets))
 
 
-def display_box(pdf_path, boxes):
+def display_box(pdf_path, boxes, page_width=612, page_height=792):
     """
     Displays each of the bounding boxes passed in 'boxes' on the pdf pointed 
     to by pdf_path
     """
     # boxes is a list of 5-tuples (page, top, left, bottom, right)
-
+    basename = subprocess.check_output('basename {} .pdf'.format(pdf_path), shell=True)
+    dirname = subprocess.check_output('dirname {}'.format(pdf_path), shell=True)
+    im_path = dirname.rstrip() + '/' + basename.rstrip()
+    page_nb = boxes[0][0] # take the page number of the first box in the list
+    os.system('pdftoppm -f {} -l {} -jpeg {} {}'.format(page_nb, page_nb, pdf_path, im_path))
+    im_path += '-{}.jpg'.format(page_nb)
+    img = cv2.resize(cv2.imread(im_path),(page_width,page_height))
+    # Plot bounding boxes
+    for p, top, left, bottom, right in boxes:
+	    if not (p == page_nb):
+		    raise Exception("Can not display bounding boxes that are not in the same page.")
+	    cv2.rectangle(img,(int(float(left)),int(float(top))),(int(float(right)),int(float(bottom))),(255,0,0),1)
+    cv2.imshow('Bounding boxes',img)
+    cv2.waitKey() # press any key to exit the opencv output 
+    cv2.destroyAllWindows() 
+    # delete image
+    os.system('rm {}'.format(im_path))
 
 
 def link_lists(listA, listB, searchMax=100, editCost=20, offsetCost=1, offsetInertia=5):

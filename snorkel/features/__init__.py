34e2af65aa30c99a1b61d8f9ce012a74d2e72ccf
henryre <henry.ehrenberg@outlook.com>
2016-11-07 16:26:36 -0800

    Add relative feature imports

--- snorkel/features/__init__.py
+++ snorkel/features/__init__.py
@@ -5,4 +5,11 @@ Subpackage for Snorkel featurization.
 from .context_features import get_doc_count_feats, get_sentence_count_feats
 from .entity_features import *
 from .generic_features import get_feats_from_matrix
-from .relative_features import *
+from .relative_features import (
+	get_first_document_span_feats,
+	get_first_document_span_feats_stopwords,
+	get_span_feats,
+	get_span_feats_stopwords,
+	get_span_splits,
+	get_span_splits_stopwords,
+)

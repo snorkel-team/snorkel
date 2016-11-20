70338c6ef68b0671d5eb13e02622128e5e1802ff
Alexander Ratner <ajratner@gmail.com>
2016-11-20 01:05:14 -0800

    For now, we skip reloading labels if the corresponding Candidate does not exist

--- snorkel/db_helpers.py
+++ snorkel/db_helpers.py
@@ -1,24 +1,48 @@
-from .models import Corpus, CandidateSet, AnnotationKeySet, ParameterSet, AnnotatorLabel, Label
+from .models import Corpus, CandidateSet, AnnotationKeySet, ParameterSet, AnnotatorLabel, Label, Context
 from .queries import get_or_create_single_key_set
 from sqlalchemy.orm import object_session
 
 
-# TODO
-def reload_annotator_labels(session, candidate_class, context_classes, annotator_name):
-    """Reloads stable annotator labels into the Label table"""
-    aks, ak = get_or_create_single_key_set(annotator_name)
-
+def reload_annotator_labels(session, candidate_class, annotator_name):
+    """
+    Reloads stable annotator labels into the Label table
+    Note that this function currently *does not* handle creating necessary Contexts / Candidates.
+    """
+    aks, ak = get_or_create_single_key_set(session, annotator_name)
+    
+    labels = []
+    missed = []
     for al in session.query(AnnotatorLabel).filter(AnnotatorLabel.annotator_name == annotator_name).all():
 
         # Check for labeled Contexts
         contexts = []
         for stable_id in al.context_stable_ids:
             context = session.query(Context).filter(Context.stable_id == stable_id).first()
+            if context:
+                contexts.append(context)
+        if len(contexts) < len(al.context_stable_ids):
+            missed.append(al)
+            continue
 
-            # If context does not exist, create it
-            # TODO: This is a little bit involved...
-            if context is None:
-                pass  # TODO
+        # Check for Candidate
+        candidate_query = session.query(candidate_class)
+        for i, arg in enumerate(candidate_class.__argnames__):
+            candidate_query = candidate_query.filter(getattr(candidate_class, arg) == contexts[i])
+        candidate = candidate_query.first()
+        if candidate is None:
+            missed.append(al)
+            continue
+
+        # Check for Label, otherwise create
+        label = session.query(Label).filter(Label.key == ak).filter(Label.candidate == candidate).first()
+        if label is None:
+            label = Label(candidate=candidate, key=ak, value=al.value)
+            session.add(label)
+            labels.append(label)
+
+    session.commit()
+    print "Labels created: %s\nLabels skipped: %s" % (len(labels), len(missed))
+    return labels, missed
 
 
 def cascade_delete_set(s):

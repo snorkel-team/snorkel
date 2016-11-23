6f049f2d7ccc77720022d5a2138e63eb6e090800
Ines Chami <ineschami@MacBook-Pro-de-Ines.local>
2016-11-23 14:29:35 -0800

    replaced PIL by wand library, allows to display bounding boxes without external file

--- snorkel/visual.py
+++ snorkel/visual.py
@@ -1,5 +1,4 @@
 from .models import Phrase
-import os
 import re
 import subprocess
 from collections import OrderedDict, defaultdict
@@ -8,7 +7,10 @@ from timeit import default_timer as timer
 
 import numpy as np
 import pandas as pd
-from PIL import Image, ImageDraw
+from wand.image import Image
+from wand.drawing import Drawing
+from wand.display import display
+from wand.color import Color
 from bs4 import BeautifulSoup
 from editdistance import eval as editdist
 from selenium import webdriver
@@ -340,31 +342,32 @@ class VisualLinker():
         if self.verbose:
             print "Updated coordinates in snorkel.db"
 
-    def display_boxes(self, boxes, page_num=1, display=True, alternate_colors=False):
+    def display_boxes(self, boxes, page_num=1, display_img=True, alternate_colors=False):
         """
         Displays each of the bounding boxes passed in 'boxes' on an image of the pdf
         pointed to by pdf_file
         # boxes is a list of 5-tuples (page, top, left, bottom, right)
         """
-        if display:
-            (img, img_path) = self.pdf_to_img(page_num)
-            colors = ['blue', 'red']
+        if display_img:
+            img = self.pdf_to_img(page_num)
+            colors = [Color('blue'), Color('red')]
         boxes_per_page = defaultdict(int)
         boxes_by_page = defaultdict(list)
         for i, (page, top, left, bottom, right) in enumerate(boxes):
             boxes_per_page[page] += 1
             boxes_by_page[page].append((top, left, bottom, right))
-        if display:
-            draw = ImageDraw.Draw(img)
+        if display_img:
+            draw = Drawing()
+            draw.fill_color = Color('rgba(0, 0, 0, 0.0)')
             for j, (top, left, bottom, right) in enumerate(boxes_by_page[page_num]):
-                color = colors[j % 2] if alternate_colors else colors[0]
-                draw.rectangle(((left, top), (right, bottom)), outline=color)
+                draw.stroke_color = colors[j % 2] if alternate_colors else colors[0]
+                draw.rectangle(left=left, top=top, right=right, bottom=bottom)
+            draw(img)
         print "Boxes per page: total (unique)"
         for (page, count) in sorted(boxes_per_page.items()):
             print "Page %d: %d (%d)" % (page, count, len(set(boxes_by_page[page])))
-        if display:
-            img.show()
-            os.system('rm {}'.format(img_path))  # delete image
+        if display_img:
+            display(img)
 
     def display_candidates(self, candidates, page_num=1, display=True):
         """
@@ -388,15 +391,10 @@ class VisualLinker():
         self.display_boxes(boxes, page_num=page_num, display=display)
 
     def pdf_to_img(self, page_num):
+        img = Image(filename='{}[{}]'.format(self.pdf_file, page_num-1))
         page_width, page_height = self.pdf_dim
-        basename = subprocess.check_output("basename '{}' .pdf".format(self.pdf_file), shell=True)
-        dirname = subprocess.check_output("dirname '{}'".format(self.pdf_file), shell=True)
-        img_path = dirname.rstrip() + '/' + basename.rstrip()
-        os.system(
-                "pdftoppm -f {} -l {} -singlefile -jpeg '{}' '{}'".format(page_num, page_num, self.pdf_file, img_path))
-        img_path += '.jpg'
-        img = Image.open(img_path).resize((page_width, page_height), Image.ANTIALIAS)
-        return img, img_path
+        img.resize(page_width, page_height)
+        return img
 
     def create_pdf(self, document_name, text):
         pdf_file = self.pdf_path + document_name + '.pdf'

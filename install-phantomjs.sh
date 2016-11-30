898682218ffe7c152002604031fa5b191d1b3519
Ines Chami <ineschami@DNab4046e2.stanford.edu>
2016-11-30 15:22:29 -0800

    fixed download phantomjs from url bug

--- install-phantomjs.sh
+++ install-phantomjs.sh
@@ -1,6 +1,9 @@
 phantomjs="phantomjs-2.1.1-macosx"
 url=https://bitbucket.org/ariya/phantomjs/downloads/$phantomjs.zip
-wget $url 
+if type curl &>/dev/null; then
+    curl -RLO $url
+elif type wget &>/dev/null; then
+    wget -N -nc $url
 unzip $phantomjs.zip
 rm $phantomjs.zip
 mv $phantomjs phantomjs

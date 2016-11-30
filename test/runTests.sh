b5cf1fd377dd2a771a7841fb7cbdf846ad4b7ee7
Ines Chami <ineschami@DNab4046e2.stanford.edu>
2016-11-30 15:06:55 -0800

    added run test script

---
+++ test/runTests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+TESTS_PATH=$SNORKELHOME/test
+
+if [ $# -eq 0 ]
+    then
+	    echo "Running all tests"
+	    for test in $TESTS_PATH/*Tests.py
+	    do
+	            echo "Test: $test"
+	            python $test
+	    done
+else
+    test=$1
+    echo "Running Test: $test"
+    python $TESTS_PATH/$test
+fi

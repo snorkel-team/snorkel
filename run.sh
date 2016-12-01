885c173bea663bfa47275532b7c867f53bb90180
Ines Chami <ineschami@DN0a25b433.SUNet>
2016-12-01 12:46:37 -0800

    added install-poppler script

--- run.sh
+++ run.sh
@@ -18,13 +18,22 @@ fi
 # Make sure phantomjs is installed
 PHANTOMJS="phantomjs/bin/phantomjs"
 if [ ! -f "$PHANTOMJS" ]; then
-    read -p "phantomjs [default] not found- install now?  [y/n] " yn
+    read -p "phantomjs not found- install now?  [y/n] " yn
     case $yn in
         [Yy]* ) echo "Installing phantomjs..."; ./install-phantomjs.sh;;
         [Nn]* ) ;;
     esac
 fi
 
+# Make sure poppler is installed
+POPPLER="poppler"
+if [ ! -f "$POPPLER" ]; then
+    read -p "poppler not found- install now?  [y/n] " yn
+    case $yn in
+        [Yy]* ) echo "Installing poppler..."; ./install-poppler.sh;;
+        [Nn]* ) ;;
+    esac
+fi
 # Launch jupyter notebook!
 echo "Launching Jupyter Notebook..."
 jupyter notebook

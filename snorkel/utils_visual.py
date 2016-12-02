82da89885897ced0a063e4e35dc05b5ab3dd06d3
Ines Chami <ineschami@DN51q00o.SUNet>
2016-12-01 16:32:57 -0800

    added display boxes in visual utils

--- snorkel/utils_visual.py
+++ snorkel/utils_visual.py
@@ -1,15 +1,38 @@
 import subprocess
 from bs4 import BeautifulSoup
+from collections import defaultdict
 from wand.image import Image
+from wand.drawing import Drawing
+from wand.display import display
+from wand.color import Color
 
 
-def display_boxes(boxes, page_num=1, display_img=True, alternate_colors=False):
+def display_boxes(pdf_file, boxes, page_num=1, display_img=True, alternate_colors=False):
     """
     Displays each of the bounding boxes passed in 'boxes' on an image of the pdf
     pointed to by pdf_file
     boxes is a list of 5-tuples (page, top, left, bottom, right)
     """
-    pass
+    if display_img:
+        img = pdf_to_img(pdf_file, page_num)
+        colors = [Color('blue'), Color('red')]
+    boxes_per_page = defaultdict(int)
+    boxes_by_page = defaultdict(list)
+    for i, (page, top, left, bottom, right) in enumerate(boxes):
+        boxes_per_page[page] += 1
+        boxes_by_page[page].append((top, left, bottom, right))
+    if display_img:
+        draw = Drawing()
+        draw.fill_color = Color('rgba(0, 0, 0, 0.0)')
+        for j, (top, left, bottom, right) in enumerate(boxes_by_page[page_num]):
+            draw.stroke_color = colors[j % 2] if alternate_colors else colors[0]
+            draw.rectangle(left=left, top=top, right=right, bottom=bottom)
+        draw(img)
+    print "Boxes per page: total (unique)"
+    for (page, count) in sorted(boxes_per_page.items()):
+        print "Page %d: %d (%d)" % (page, count, len(set(boxes_by_page[page])))
+    if display_img:
+        display(img)
 
 
 def display_candidates(candidates, page_num=1, display=True):

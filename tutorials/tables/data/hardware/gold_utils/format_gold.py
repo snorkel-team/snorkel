ae670c185921dafb53df491a5c286d5818cc099c
Braden Hancock <hancock.braden@gmail.com>
2016-12-07 10:48:31 -0800

    Update directory structure for new gold

--- tutorials/tables/data/hardware/gold_utils/format_gold.py
+++ tutorials/tables/data/hardware/gold_utils/format_gold.py
@@ -92,66 +92,24 @@ def format_gold(raw_gold_file, formatted_gold_file):
 				('dc_gain_min', dc_gain_min, gain_normalizer)]
 			for name, attr, normalizer in name_attr_norm:
 				if 'N/A' not in attr:
-					print part_num, attr
 					for a in attr.split(';'):
 						if len(a.strip())>0:
 							writer.writerow([doc_name, part_num, name, normalizer(a)])
 
-			# if polarity != 'N/A':
-			# 	for p in polarity.split(delim):
-			# 		writer.writerow([doc_name, part_num, "polarity", polarity_normalizer(polarity)])
-			# if ce_v_max != 'N/A':
-			# 	for v in ce_v_max.split(delim):
-			# 		writer.writerow([doc_name, part_num, "ce_v_max", voltage_normalizer(v_ceo)])
-			# if cb_v_max != 'N/A':
-			# 	for v in cb_v_max.split(delim):
-			# 		writer.writerow([doc_name, part_num, "cb_v_max", voltage_normalizer(v_cbo)])
-			# if eb_v_max != 'N/A':
-			# 	for v in eb_v_max.split(delim):
-			# 	writer.writerow([doc_name, part_num, "eb_v_max", voltage_normalizer(v_ebo)])
-			# if "N/A" not in current:
-			# 	writer.writerow([doc_name, part_num, "c_current_max", current_normalizer(current)])
-			# if "N/A" not in dev_dissipation:
-			# 	for dissipation in dev_dissipation.split(";"):
-			# 		writer.writerow([doc_name, part_num, "dev_dissipation", dissipation_normalizer(dissipation)])
-			# if "N/A" not in min_temp:
-			# 	writer.writerow([doc_name, part_num, "stg_temp_min", temperature_normalizer(min_temp)])
-			# if "N/A" not in max_temp:
-			# 	writer.writerow([doc_name, part_num, "stg_temp_max", temperature_normalizer(max_temp)])
-			# if "N/A" not in dev_gain:
-			# 	for gain in dev_gain.split(";"):
-			# 		if("@" in gain):
-			# 			writer.writerow([doc_name, part_num, "dc_gain_min", gain_normalizer(gain)])
-'''
-def test_normalizer(raw_gold_file):
-	with open(raw_gold_file, "r") as csvinput:
-		reader = csv.reader(csvinput)
-		next(reader, None) # Skip header row
-		for line in reader:
-			(pdf_name, part_family, part_num, manufacturer,
-			 polarity, ce_v_max, cb_v_max, eb_v_max, c_current_max,
-			 dev_dissipation, stg_temp_min, stg_temp_max, dc_gain_min,
-			 notes, annotator) = line
-
-			if stg_temp_min != 'N/A':
-				print polarity_normalizer(polarity)
-				# print temperature_normalizer(stg_temp_min)
-				# print temperature_normalizer(stg_temp_max)
-'''
 
 def main():
     # Transform the test set
 	raw_gold = os.environ['SNORKELHOME']+ '/tutorials/tables/data/hardware/gold_raw/test_gold_raw.csv'
-	formatted_gold = os.environ['SNORKELHOME']+'/tutorials/tables/data/hardware/hardware_test/hardware_test_gold.csv'
+	formatted_gold = os.environ['SNORKELHOME']+'/tutorials/tables/data/hardware/test/hardware_test_gold.csv'
 	format_gold(raw_gold, formatted_gold)
 
     # Transform the dev set
 	raw_gold = os.environ['SNORKELHOME']+ '/tutorials/tables/data/hardware/gold_raw/old_dev_gold_raw.csv'
-	formatted_gold = os.environ['SNORKELHOME']+'/tutorials/tables/data/hardware/hardware_dev/hardware_dev_gold.csv'
+	formatted_gold = os.environ['SNORKELHOME']+'/tutorials/tables/data/hardware/dev/hardware_dev_gold.csv'
 	format_old_dev_gold(raw_gold, formatted_gold)
     # NOTE: This MUST come after format_old_dev_gold so it can append to the previous file.
 	raw_gold = os.environ['SNORKELHOME']+ '/tutorials/tables/data/hardware/gold_raw/new_dev_gold_raw.csv'
-	formatted_gold = os.environ['SNORKELHOME']+'/tutorials/tables/data/hardware/hardware_dev/hardware_dev_gold.csv'
+	formatted_gold = os.environ['SNORKELHOME']+'/tutorials/tables/data/hardware/dev/hardware_dev_gold.csv'
 	format_new_dev_gold(raw_gold, formatted_gold)
 
 if __name__=='__main__':

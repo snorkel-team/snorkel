0801950bdbb88adc06a130107b4ece68218a0b4c
Braden Hancock <hancock.braden@gmail.com>
2016-12-09 16:02:54 -0800

    Loosen visual alignment definition

--- snorkel/utils_visual.py
+++ snorkel/utils_visual.py
@@ -36,10 +36,12 @@ def bbox_horz_aligned(box1, box2):
     vertical range of the other
     """
     if not (box1 and box2): return False
-    center1 = (box1.bottom + box1.top) / 2.0
-    center2 = (box2.bottom + box2.top) / 2.0
-    return ((center1 >= box2.top and center1 <= box2.bottom) or
-            (center2 >= box1.top and center2 <= box1.bottom))
+    # NEW: any overlap counts
+    return box1.top <= box2.bottom and box2.top <= box1.bottom
+    # center1 = (box1.bottom + box1.top) / 2.0
+    # center2 = (box2.bottom + box2.top) / 2.0
+    # return ((center1 >= box2.top and center1 <= box2.bottom) or
+    #         (center2 >= box1.top and center2 <= box1.bottom))
 
 
 def bbox_vert_aligned(box1, box2):
@@ -48,10 +50,12 @@ def bbox_vert_aligned(box1, box2):
     horizontal range of the other
     """
     if not (box1 and box2): return False
-    center1 = (box1.right + box1.left) / 2.0
-    center2 = (box2.right + box2.left) / 2.0
-    return ((center1 >= box2.left and center1 <= box2.right) or
-            (center2 >= box1.left and center2 <= box1.right))
+    # NEW: any overlap counts
+    return box1.left <= box2.right and box2.left <= box1.right
+    # center1 = (box1.right + box1.left) / 2.0
+    # center2 = (box2.right + box2.left) / 2.0
+    # return ((center1 >= box2.left and center1 <= box2.right) or
+    #         (center2 >= box1.left and center2 <= box1.right))
 
 
 def bbox_vert_aligned_left(box1, box2):

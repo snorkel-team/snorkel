9d2f9726083caaa06e3f94e5047a0835f2db33f1
henryre <henry.ehrenberg@outlook.com>
2017-01-12 20:55:01 -0800

    Remove FG messages, set default rate and reg param

--- snorkel/learning/gen_learning.py
+++ snorkel/learning/gen_learning.py
@@ -189,25 +189,21 @@ class GenerativeModel(object):
     optional_names = ('lf_prior', 'lf_propensity', 'lf_class_propensity')
     dep_names = ('dep_similar', 'dep_fixing', 'dep_reinforcing', 'dep_exclusive')
 
-    def train(self, L, y=None, deps=(), init_acc = 1.0, epochs=100, step_size=0.001, decay=0.99, reg_param=0.1, reg_type=2, verbose=False,
+    def train(self, L, y=None, deps=(), init_acc = 1.0, epochs=100, step_size=None, decay=0.99, reg_param=0.1, reg_type=2, verbose=False,
               truncation=10, burn_in=50, timer=None):
-        print "Processing"
+	step_size = step_size or 1.0 / L.shape[0]
+	reg_param_scaled = reg_param / L.shape[0]
         self._process_dependency_graph(L, deps)
-        print "Compiling"
         weight, variable, factor, ftv, domain_mask, n_edges = self._compile(L, y, init_acc)
-        print "Initializing FG"
         fg = NumbSkull(n_inference_epoch=0, n_learning_epoch=epochs, stepsize=step_size, decay=decay,
-                       reg_param=reg_param, regularization=reg_type, truncation=truncation,
+                       reg_param=reg_param_scaled, regularization=reg_type, truncation=truncation,
                        quiet=(not verbose), verbose=verbose, learn_non_evidence=True, burn_in=burn_in)
-        print "Loading FG"
         fg.loadFactorGraph(weight, variable, factor, ftv, domain_mask, n_edges)
         if timer is not None:
             timer.start()
-        print "Learning FG"
         fg.learning(out=False)
         if timer is not None:
             timer.end()
-        print "Processing weights"
         self._process_learned_weights(L, fg)
 
     def marginals(self, L):
@@ -337,7 +333,6 @@ class GenerativeModel(object):
         ftv = np.zeros(n_edges, FactorToVar)
         domain_mask = np.zeros(n_vars, np.bool)
 
-        print "\tInitialized"
 
         #
         # Compiles weight matrix
@@ -358,7 +353,6 @@ class GenerativeModel(object):
             weight[i]['isFixed'] = False
             weight[i]['initialValue'] = np.float64(0.0)
 
-        print "\tWeight matrix set"
 
         #
         # Compiles variable matrix
@@ -369,7 +363,6 @@ class GenerativeModel(object):
             variable[i]["dataType"] = 0
             variable[i]["cardinality"] = 2
 
-        print "\tVariable matrix step 1"
 
         for i in range(m):
             for j in range(n):
@@ -387,7 +380,6 @@ class GenerativeModel(object):
                 variable[index]["dataType"] = 0
                 variable[index]["cardinality"] = 3
 
-        print "\tVariable matrix step 2"
 
         #
         # Compiles factor and ftv matrices
@@ -412,7 +404,6 @@ class GenerativeModel(object):
             ftv_off = 0
             w_off = 0
 
-        print "\tPriors set"
 
         # Factors over labeling function outputs
         f_off, ftv_off, w_off = self._compile_output_factors(L, factor, f_off, ftv, ftv_off, w_off, "DP_GEN_LF_ACCURACY",
@@ -437,7 +428,6 @@ class GenerativeModel(object):
                                                                      optional_name_map[optional_name][0],
                                                                      optional_name_map[optional_name][1])
 
-        print "\tOutput factors set"
 
         # Factors for labeling function dependencies
         dep_name_map = {
@@ -469,7 +459,6 @@ class GenerativeModel(object):
                                                                   dep_name_map[dep_name][0],
                                                                   dep_name_map[dep_name][1])
 
-        print "\tDeps set"
 
         return weight, variable, factor, ftv, domain_mask, n_edges
 

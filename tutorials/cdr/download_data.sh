cf4334a88ae3ee238ed92385ef7d6cdd29e05d68
henryre <henry.ehrenberg@outlook.com>
2017-01-19 11:56:32 -0800

    Update CDR downloader
    Former-commit-id: eabbddb24ea7db8eaae19464952508ae0e667d65

--- tutorials/cdr/download_data.sh
+++ tutorials/cdr/download_data.sh
@@ -12,5 +12,11 @@ cp downloads/CDR_Data/CDR.Corpus.v010516/CDR_DevelopmentSet.BioC.xml data/.
 cp downloads/CDR_Data/CDR.Corpus.v010516/CDR_TestSet.BioC.xml data/.
 echo "Deleting downloads directory..."
 rm -rf downloads
+echo "Combining data files..."
+{
+	head -69462 data/CDR_TrainingSet.BioC.xml;
+	tail -70514 data/CDR_DevelopmentSet.BioC.xml | head -70512;
+	tail -71613 data/CDR_TestSet.BioC.xml;
+} > data/CDR.BioC.xml
 echo "Done!"
 

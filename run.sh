9f858aec9611273819c5cd451a30110cbbfa36aa
senwu <senwu@stanford.edu>
2017-03-21 16:37:20 -0700

    update set_env and run scripts

--- run.sh
+++ run.sh
@@ -15,6 +15,34 @@ if [ ! -f "$PARSER" ]; then
     esac
 fi
 
+# Make sure phantomjs is installed
+PHANTOMJS="phantomjs/bin/phantomjs"
+if [ ! -f "$PHANTOMJS" ]; then
+    read -p "phantomjs not found- install now?  [y/n] " yn
+    case $yn in
+        [Yy]* ) echo "Installing phantomjs..."; ./install-phantomjs.sh;;
+        [Nn]* ) ;;
+    esac
+fi
+
+# Make sure poppler is installed
+POPPLER=$(which pdfinfo)
+size=${#POPPLER}
+if [ $size -eq 0 ]; then
+    read -p "poppler not found- install now?  [y/n] " yn
+    case $yn in
+        [Yy]* ) echo "Installing poppler..."; ./install-poppler.sh;;
+        [Nn]* ) ;;
+    esac
+fi
+
+# Launch Stanford CoreNLP Server
+java -Xmx8g -cp "parser/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer --port $JAVANLPPORT --timeout 600000 > /dev/null &
+CORENLPPID=$!
+
 # Launch jupyter notebook!
 echo "Launching Jupyter Notebook..."
 jupyter notebook
+
+# Shut down Stanford CoreNLP Server
+kill $CORENLPPID

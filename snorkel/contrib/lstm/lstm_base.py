8dcb977e851d730b18ee4c1fe11591b780081890
henryre <henry.ehrenberg@outlook.com>
2017-03-28 12:37:52 -0700

    LSTM attention and other improvements

--- snorkel/contrib/lstm/lstm_base.py
+++ snorkel/contrib/lstm/lstm_base.py
@@ -1,9 +1,14 @@
 import numpy as np
+import random
 import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
 
 from snorkel.learning import LabelBalancer, TFNoiseAwareModel
-from utils import get_bi_rnn_output, SymbolTable
+from utils import f1_score, get_bi_rnn_output, SymbolTable
 from time import time
+
+
+SD = 0.1
 
 
 class LSTMBase(TFNoiseAwareModel):
@@ -13,17 +18,18 @@ class LSTMBase(TFNoiseAwareModel):
     def __init__(self, save_file=None, name='LSTMBase', seed=None, n_threads=4):
         """Base class for LSTM"""
         # Define metadata
-        self.mx_len          = None # Max sentence length
-        self.dim             = None # Embedding dimension
-        self.n_v             = None # Vocabulary size
-        self.lr              = None # Learning rate
-        self.word_dict       = SymbolTable() # Symbol table for dictionary
+        self.mx_len    = None # Max sentence length
+        self.dim       = None # Embedding dimension
+        self.n_v       = None # Vocabulary size
+        self.lr        = None # Learning rate
+        self.attn      = None # Attention window
+        self.word_dict = SymbolTable() # Symbol table for dictionary
         # Define input layers
-        self.sentences       = None
-        self.sentence_length = None
-        self.train_marginals = None
-        self.keep_prob       = None
-        self.seed            = seed
+        self.sentences        = None
+        self.sentence_lengths = None
+        self.train_marginals  = None
+        self.keep_prob        = None
+        self.seed             = seed
         # Super constructor
         super(LSTMBase, self).__init__(
             n_threads=n_threads, save_file=save_file, name=name
@@ -44,45 +50,50 @@ class LSTMBase(TFNoiseAwareModel):
         return x_batch, len_batch
 
     def _embedding_init(self, s):
-        return tf.random_normal((self.n_v-1, self.dim), seed=s)
+        return tf.random_normal((self.n_v-1, self.dim), stddev=SD, seed=s)
 
     def _build(self):
         """Get feed forward step, loss function, and optimizer for LSTM"""
         # Define input layers
-        self.sentences       = tf.placeholder(tf.int32, [None, None])
-        self.sentence_length = tf.placeholder(tf.int32, [None])
-        self.train_marginals = tf.placeholder(tf.float32, [None])
-        self.keep_prob       = tf.placeholder(tf.float32)
+        self.sentences        = tf.placeholder(tf.int32, [None, None])
+        self.sentence_lengths = tf.placeholder(tf.int32, [None])
+        self.train_marginals  = tf.placeholder(tf.float32, [None])
+        self.keep_prob        = tf.placeholder(tf.float32)
         # Seeds
-        s               = self.seed
-        s1, s2, s3, s4  = [s, s+1, s+2, s+3] if s is not None else [None] * 4
+        s = self.seed
+        s1, s2, s3, s4 = [None] * 4 if s is None else [s+i for i in range(4)]
         # Embedding layer
-        emb_var   = tf.cast(tf.Variable(self._embedding_init(s1)), tf.float32)
+        emb_var   = tf.Variable(self._embedding_init(s1))
         embedding = tf.concat([tf.zeros([1, self.dim]), emb_var], axis=0)
         inputs    = tf.nn.embedding_lookup(embedding, self.sentences)
         # Build RNN graph
         batch_size = tf.shape(self.sentences)[0]
-        with tf.variable_scope("LSTM") as scope:
-            if s2 is not None:
-                tf.set_random_seed(s2)
+        rand_name  = "LSTM_{0}".format(random.randint(0, 1e12)) # Obscene hack
+        init = tf.contrib.layers.xavier_initializer(seed=s2)
+        with tf.variable_scope(rand_name, reuse=False, initializer=init):
             # Build LSTM cells
-            fw_cell = tf.contrib.rnn.BasicLSTMCell(self.dim)
-            bw_cell = tf.contrib.rnn.BasicLSTMCell(self.dim)
+            fw_cell = rnn.BasicLSTMCell(self.dim, reuse=False)
+            bw_cell = rnn.BasicLSTMCell(self.dim, reuse=False)
+            # Add attention if needed
+            if self.attn:
+                fw_cell = rnn.AttentionCellWrapper(fw_cell, self.attn)
+                bw_cell = rnn.AttentionCellWrapper(bw_cell, self.attn)
             # Construct RNN
             initial_state_fw = fw_cell.zero_state(batch_size, tf.float32)
             initial_state_bw = bw_cell.zero_state(batch_size, tf.float32)
             rnn_out, _ = tf.nn.bidirectional_dynamic_rnn(
-                fw_cell, bw_cell, inputs, sequence_length=self.sentence_length,
+                fw_cell, bw_cell, inputs,
+                sequence_length=self.sentence_lengths,
                 initial_state_fw=initial_state_fw,
                 initial_state_bw=initial_state_bw,
                 time_major=False               
             )
         # Get potentials
-        potentials = get_bi_rnn_output(rnn_out, self.dim, self.sentence_length)
+        potentials = get_bi_rnn_output(rnn_out, self.dim, self.sentence_lengths)
         # Compute activation
-        potentials_dropout = tf.nn.dropout(potentials, self.keep_prob)
-        W = tf.Variable(tf.random_normal((2*self.dim, 1), stddev=0.1, seed=s3))
-        b = tf.Variable(tf.random_normal((1,), stddev=0.1, seed=s4))
+        potentials_dropout = tf.nn.dropout(potentials, self.keep_prob, seed=s3)
+        W = tf.Variable(tf.random_normal((2*self.dim, 1), stddev=SD, seed=s4))
+        b = tf.Variable(0., dtype=tf.float32)
         h_dropout = tf.squeeze(tf.matmul(potentials_dropout, W)) + b
         # Noise-aware loss
         self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
@@ -94,20 +105,23 @@ class LSTMBase(TFNoiseAwareModel):
         self.prediction = tf.nn.sigmoid(h_dropout)
 
     def train(self, candidates, marginals, n_epochs=25, lr=0.01, dropout=0.5,
-        dim=50, batch_size=256, max_sentence_length=None, rebalance=False,
-        print_freq=5):
+        dim=50, attn_window=None, batch_size=256, max_sentence_length=None,
+        rebalance=False, dev_candidates=None, dev_labels=None, print_freq=5):
         """Train LSTM model
-            @candidates: list Candidate objects for training
-            @marginals:  array of marginal probabilities for each Candidate
+            @candidates: list of Candidate objects for training
+            @marginals: array of marginal probabilities for each Candidate
             @n_epochs: number of training epochs
             @lr: learning rate
-            @dropout: keep probability for dropout layer; if None, no dropout
+            @dropout: keep probability for dropout layer (no dropout if None)
             @dim: embedding dimension
+            @attn_window: attention window length (no attention if 0 or None)
             @batch_size: batch size for mini-batch SGD
             @max_sentence_length: maximum sentence length for candidates
-            @rebalance: bool or fraction of positive examples desired
-                        If True, defaults to standard 0.5 class balance.
-                        If False, no class balancing.
+            @rebalance: bool or fraction of positive examples for training
+                        - if True, defaults to standard 0.5 class balance
+                        - if False, no class balancing
+            @dev_candidates: list of Candidate objects for evaluation
+            @dev_labels: array of labels for each dev Candidate
             @print_freq: number of epochs after which to print status
         """
         verbose = print_freq > 0
@@ -118,16 +132,28 @@ class LSTMBase(TFNoiseAwareModel):
         # Text preprocessing
         train_data = self._preprocess_data(candidates, extend=True)
         # Get training indices
+        np.random.seed(self.seed)
         train_idxs = LabelBalancer(marginals).get_train_idxs(rebalance)
         x_train    = [train_data[j] for j in train_idxs]
         y_train    = np.ravel(marginals)[train_idxs]
         # Get max sentence size
         self.mx_len = max_sentence_length or max(len(x) for x in x_train)
         # Build model
-        self.dim = dim
-        self.lr  = lr
-        self.n_v = self.word_dict.len()
+        self.dim  = dim
+        self.lr   = lr
+        self.n_v  = self.word_dict.len()
+        self.attn = attn_window
         self._build()
+        # Get dev data
+        dev_data, dev_gold = None, None
+        if dev_candidates is not None and dev_labels is not None:
+            dev_data = self._preprocess_data(dev_candidates, extend=False)
+            dev_gold = np.ravel(dev_labels)
+            if not ((dev_gold >= 0).all() and (dev_gold <= 1).all()):
+                raise Exception("Dev labels should be in [0, 1]")
+            print("[{0}] Loaded {1} candidates for evaluation".format(
+                self.name, len(dev_data)
+            ))
         # Run mini-batch SGD
         n = len(x_train)
         batch_size = min(batch_size, n)
@@ -149,27 +175,35 @@ class LSTMBase(TFNoiseAwareModel):
                 y_b        = y_train[i:i+batch_size]
                 # Run training step and evaluate loss function                  
                 epoch_loss.append(self.session.run([self.loss, self.train_fn], {
-                    self.sentences:       x_b,
-                    self.sentence_length: len_b,
-                    self.train_marginals: y_b,
-                    self.keep_prob:       dropout or 1.0,
+                    self.sentences:        x_b,
+                    self.sentence_lengths: len_b,
+                    self.train_marginals:  y_b,
+                    self.keep_prob:        dropout or 1.0,
                 })[0])
             # Print training stats
             if verbose and (t % print_freq == 0 or t in [0, (n_epochs-1)]):
-                print("[{0}] Epoch {1} ({2:.2f}s)\tAverage loss={3:.6f}".format(
+                msg = "[{0}] Epoch {1} ({2:.2f}s)\tAverage loss={3:.6f}".format(
                     self.name, t, time() - st, np.mean(epoch_loss)
-                ))
+                )
+                if dev_data is not None:
+                    dev_p    = self._marginals_preprocessed(dev_data)
+                    f1, _, _ = f1_score(dev_p, dev_gold)
+                    msg     += '\tDev F1={0:.2f}'.format(100. * f1)
+                print msg
         if verbose:
             print("[{0}] Training done ({1:.2f}s)".format(self.name, time()-st))
+
+    def _marginals_preprocessed(self, test_data):
+        x, x_len = self._make_tensor(test_data)
+        return np.ravel(self.session.run(self.prediction, {
+            self.sentences:        x,
+            self.sentence_lengths: x_len,
+            self.keep_prob:        1.0,
+        }))
 
     def marginals(self, test_candidates):
         """Get likelihood of tagged sequences represented by test_candidates
             @test_candidates: list of lists representing test sentence
         """
         test_data = self._preprocess_data(test_candidates, extend=False)
-        x, x_len = self._make_tensor(test_data)
-        return np.ravel(self.session.run(self.prediction, {
-            self.sentences:       x,
-            self.sentence_length: x_len,
-            self.keep_prob:       1.0,
-        }))
+        return self._marginals_preprocessed(test_data)

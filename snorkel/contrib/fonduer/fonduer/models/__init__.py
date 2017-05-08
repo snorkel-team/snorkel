abf1dee2285aea4787f3ce2bdee29b77712502e4
Luke Hsiao <lwhsiao@stanford.edu>
2017-05-08 14:15:20 -0700

    Clean up fonduer.models __init__.py imports.

--- snorkel/contrib/fonduer/fonduer/models/__init__.py
+++ snorkel/contrib/fonduer/fonduer/models/__init__.py
@@ -1,9 +1,7 @@
-from .....models.meta import SnorkelBase, SnorkelSession, snorkel_engine, snorkel_postgres
-from .....models.context import construct_stable_id, split_stable_id
-from .....models.candidate import Candidate, candidate_subclass
-from .....models.annotation import Feature, FeatureKey, Label, LabelKey, GoldLabel, GoldLabelKey, StableLabel, Prediction, PredictionKey
-from .....models.parameter import Parameter
-from .context import Webpage, Table, Cell, Phrase, TemporaryImplicitSpan, ImplicitSpan, Document
+from .....models.meta import SnorkelSession, SnorkelBase, snorkel_engine
+from .....models.context import Document
+
+from .context import Webpage, Table, Cell, Phrase, TemporaryImplicitSpan, ImplicitSpan
 
 # Use sqlalchemy to create tables for the new context types used by Fonduer
 SnorkelBase.metadata.create_all(snorkel_engine)

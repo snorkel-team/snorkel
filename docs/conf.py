80f685e39cd3f28b989ce80883c2fbb09da2f44e
ajratner <ajratner@gmail.com>
2017-07-09 11:45:07 -0700

    Attempting to fix autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,14 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+# Mock imports for troublesome modules (i.e. any that use C code)
+# See: http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.

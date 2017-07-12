99c6eee2c12712a05b2c02f399ea346a882bafeb
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-07-12 13:58:09 -0400

    Adding initial scripts to run from command line

---
+++ revere.py
@@ -0,0 +1,25 @@
+"""Extracting symptoms from diseases
+"""
+from snorkel import SnorkelSession
+from snorkel.parser import XMLMultiDocPreprocessor
+from snorkel.parser import CorpusParser
+from utils import TaggerOneTagger
+from snorkel.models import Document, Sentence
+
+data_file_path = 'tutorials/cdr/data/CDR.RevereTest.BioC.xml'
+
+SNORKEL_SESSION = SnorkelSession()
+
+doc_preprocessor = XMLMultiDocPreprocessor(
+    path=data_file_path,
+    doc='.//document',
+    text='.//passage/text/text()',
+    id='.//id/text()'
+)
+
+tagger_one = TaggerOneTagger()
+corpus_parser = CorpusParser(fn=tagger_one.tag)
+corpus_parser.apply(list(doc_preprocessor))
+
+print("Documents:", SNORKEL_SESSION.query(Document).count())
+print("Sentences:", SNORKEL_SESSION.query(Sentence).count())

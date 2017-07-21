1d4e2b372672abe5e5891fe10b4171ff56ccf581
Luke Hsiao <lwhsiao@stanford.edu>
2017-07-20 23:44:46 -0700

    Update the output of bar in utils

--- snorkel/utils.py
+++ snorkel/utils.py
@@ -16,18 +16,12 @@ class ProgressBar(object):
 
     def bar(self, i):
         """Assumes i ranges through [0, N-1]"""
-<<<<<<< HEAD
         new_tick = i/self.update_interval
         if int(new_tick) != int(self.current_tick):
             b = int(np.ceil((i / self.nf) * self.length))
-            sys.stdout.write("\r[%s%s] %d%%" % ("="*b, " "*(self.length-b), int(100*(i / self.nf))))
-=======
-        if i in self.ticks:
-            b = int(np.ceil(((i+1) / self.nf) * self.length))
             sys.stdout.write(
                 "\r[{0}{1}] {2}%".format(
-                    "="*b, " "*(self.length-b), int(100*((i+1) / self.nf))))
->>>>>>> master
+                    "="*b, " "*(self.length-b), int(100*((i) / self.nf))))
             sys.stdout.flush()
         self.current_tick = new_tick
 

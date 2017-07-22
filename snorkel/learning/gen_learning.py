Fixes unfinished / incorrect marginals calculation for scoped categorical; removes need to reload cardinalities; passes test/learning tests

snorkel/learning/gen_learning.py
@@ -189,22 +189,9 @@ class GenerativeModel(Classifier):
         # else as constant value.
         self.cardinalities = self.cardinality * np.ones(m, dtype=np.int64)
         self.candidate_ranges = candidate_ranges
-        if candidate_ranges is not None:
-            for i in range(m):
-                c_range = candidate_ranges[i]
-
-                # Confirm that the candidate range has only unique values
-                assert len(c_range) == len(set(c_range))
-                self.cardinalities[i] = len(c_range)
-
-                # Re-map the values of L[i, :]
-                # Assumes L is csr_sparse format at this point
-                for j in range(L[i].data.shape[0]):
-                    val = L[i].data[j]
-                    if val not in c_range:
-                        raise ValueError("""Value {0} is not in supplied range 
-                            for candidate at index {1}""".format(val, i))
-                    L[i, L[i].indices[j]] = c_range.index(val) + 1
+        if self.candidate_ranges is not None:
+            L, self.cardinalities, _ = self._remap_scoped_categoricals(L, 
+                self.candidate_ranges)
 
         # Shuffle the data points, cardinalities, and candidate_ranges
         idxs = range(m)
@@ -246,7 +233,7 @@ class GenerativeModel(Classifier):
 
         # Store info from factor graph
         if self.candidate_ranges is not None:
-            self.cardinality_for_stats = max(self.cardinalities)
+            self.cardinality_for_stats = int(max(self.cardinalities))
         else:
             self.cardinality_for_stats = self.cardinality
         self.learned_weights = fg.factorGraphs[0].weight_value
@@ -265,6 +252,36 @@ class GenerativeModel(Classifier):
         self.fg = fg
         self.nlf = n
         self.cardinality = cardinality
+
+    def _remap_scoped_categoricals(self, L_in, candidate_ranges):
+        """
+        Remap the values of each individual candidate so that they have dense
+        support, returning the remapped label matrix, cardinalities, and
+        inverse mapping.
+        """
+        L = L_in.copy()
+        m, n = L.shape
+        cardinalities = np.ones(m)
+        mappings = []
+        for i in range(m):
+            c_range = candidate_ranges[i]
+
+            # Confirm that the candidate range has only unique values
+            assert len(c_range) == len(set(c_range))
+            cardinalities[i] = len(c_range)
+
+            # Create the inverse mapping
+            mappings.append(dict([(a + 1, b) for a, b in enumerate(c_range)]))
+
+            # Re-map the values of L[i, :]
+            # Assumes L is csr_sparse format at this point
+            for j in range(L[i].data.shape[0]):
+                val = L[i].data[j]
+                if val not in c_range:
+                    raise ValueError("""Value {0} is not in supplied range 
+                        for candidate at index {1}""".format(val, i))
+                L[i, L[i].indices[j]] = c_range.index(val) + 1
+        return L, cardinalities, mappings
 
     def learned_lf_stats(self):
         """
@@ -338,7 +355,7 @@ class GenerativeModel(Classifier):
 
         return DataFrame(stats)
 
-    def marginals(self, L):
+    def marginals(self, L, candidate_ranges=None):
         """
         Given an M x N label matrix, returns marginal probabilities for each
         candidate, depending on classification setting:
@@ -407,18 +424,26 @@ class GenerativeModel(Classifier):
         else:
             all_marginals = []
 
+            # Handle the scoped categorical case, otherwise get cardinalities
+            # from self.cardinality
+            if candidate_ranges is not None:
+                L, cardinalities, mappings = self._remap_scoped_categoricals(L, 
+                    candidate_ranges)
+            else:
+                cardinalities = self.cardinality * np.ones(m)
+
             # Get the marginal (posterior) probability for each candidate
             for i in range(m):
-                marginals = np.zeros(self.cardinalities[i], dtype=np.float64)
+                marginals = np.zeros(cardinalities[i], dtype=np.float64)
                 # NB: class priors not currently available for categoricals
                 l_i = L[i].tocoo()
                 for l_index1 in range(l_i.nnz):
                     data_j, j = l_i.data[l_index1], l_i.col[l_index1]
                     if (data_j != 0):
-                        if not 1 <= data_j <= self.cardinalities[i]:
+                        if not 1 <= data_j <= cardinalities[i]:
                             raise ValueError(
                                 """Illegal value at %d, %d: %d. Must be in 0 to 
-                                %d.""" % (i, j, data_j, self.cardinalities[i]))
+                                %d.""" % (i, j, data_j, cardinalities[i]))
                         # NB: LF class propensity not currently available
                         # for categoricals
                         marginals[int(data_j - 1)] += \
@@ -432,11 +457,11 @@ class GenerativeModel(Classifier):
 
             # If candidate_ranges not None, remap back to original values and
             # return as sparse matrix
-            if self.candidate_ranges is not None:
+            if candidate_ranges is not None:
                 M = sparse.coo_matrix((m, self.cardinality), dtype=np.float64)
                 for i, marginals in enumerate(all_marginals):
                     for j, p in enumerate(marginals):
-                        M[i, self.mappings[j-1]] = p
+                        M[i, mappings[i][j]] = p
             else:
                 M = np.vstack(all_marginals)
             return M
@@ -601,7 +626,7 @@ class GenerativeModel(Classifier):
                     variable[index]["initialValue"] = data - 1
                 else:
                     raise ValueError("Invalid labeling function output in cell (%d, %d): %d. "
-                                     "Valid values are 0 to %d. " % (i, j, data, self.cardinality))
+                                     "Valid values are 0 to %d. " % (i, j, data, self.cardinalities[i]))
 
         #
         # Compiles factor and ftv matrices
@@ -800,7 +825,10 @@ class GenerativeModel(Classifier):
         # Save other model hyperparameters needed to rebuild model
         save_path2 = os.path.join(save_dir, "{0}.hps.pkl".format(model_name))
         with open(save_path2, 'wb') as f:
-            dump({'cardinality': self.cardinality}, f)
+            dump({
+                'cardinality': self.cardinality,
+                'cardinality_for_stats': self.cardinality_for_stats
+            }, f)
 
         if verbose:
             print("[{0}] Model saved as <{1}>.".format(self.name, model_name))
@@ -814,7 +842,8 @@ class GenerativeModel(Classifier):
         save_path2 = os.path.join(save_dir, "{0}.hps.pkl".format(model_name))
         with open(save_path2, 'rb') as f:
             hps = load(f)
-            self.cardinality = hps['cardinality']
+            for k, v in hps.iteritems():
+                setattr(self, k, v)
         if verbose:
             print("[{0}] Model <{1}> loaded.".format(self.name, model_name))
 
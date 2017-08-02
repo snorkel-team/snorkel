54a745012d7059744392062925179c89b998c9a3
Braden Hancock <hancock.braden@gmail.com>
2017-08-02 00:20:11 -0700

    Remove model and model_config imports for now.

--- snorkel/contrib/babble/__init__.py
+++ snorkel/contrib/babble/__init__.py
@@ -1,8 +1,8 @@
 from .babble import Babbler
 from .grammar import Rule, Grammar, Parse
 from .helpers import *
-from .model import CDRModel
-from .model_config import configuration
+# from .model import CDRModel
+# from .model_config import configuration
 from .semparser import SemanticParser, Explanation
 from .snorkel_grammar import sem_to_str
 from .utils import link_explanation_candidates

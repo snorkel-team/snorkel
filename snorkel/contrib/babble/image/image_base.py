86dcc5d166176a4165bcd54fcefcd1a210368c28
Braden Hancock <hancock.braden@gmail.com>
2017-08-04 13:31:13 -0700

    Add new image rules.

--- snorkel/contrib/babble/image/image_base.py
+++ snorkel/contrib/babble/image/image_base.py
@@ -7,28 +7,41 @@ lexical_rules = (
     [Rule('$Y', w, ('.int', 1)) for w in ['y']] +
     [Rule('$Box', w, '.box') for w in ['box']] +
 
-    [Rule('$Top', w, ('.string', 'top')) for w in ['top', 'upper']] +
-    [Rule('$Bottom', w, ('.string', 'bottom')) for w in ['bottom', 'lower']] +
+    [Rule('$Top', w, ('.string', 'top')) for w in ['top', 'upper', 'uppermost', 'highest']] +
+    [Rule('$Bottom', w, ('.string', 'bottom')) for w in ['bottom', 'lower', 'lowest']] +
     [Rule('$Left', w, ('.string', 'left')) for w in ['left']] +
     [Rule('$Right', w, ('.string', 'right')) for w in ['right']] +
-    [Rule('$Below', w, '.below') for w in ['below', 'under']]
+
+    [Rule('$Below', w, '.below') for w in ['below', 'under']] +
+    [Rule('$Above', w, '.above') for w in ['above', 'on top of']] +
+
+    [Rule('$Center', w, '.center') for w in ['center', 'middle']] +
+    [Rule('$Corner', w, '.corner') for w in ['corner']]
 )
 
 unary_rules = [
     Rule('$BoxId', '$X', sems0),
     Rule('$BoxId', '$Y', sems0),
-    Rule('$Edge', '$Top', sems0),
-    Rule('$Edge', '$Bottom', sems0),
-    Rule('$Edge', '$Left', sems0),
-    Rule('$Edge', '$Right', sems0),
+    Rule('$Side', '$Top', sems0),
+    Rule('$Side', '$Bottom', sems0),
+    Rule('$Side', '$Left', sems0),
+    Rule('$Side', '$Right', sems0),
+
+    Rule('$Point', '$Center', sems0),
+    Rule('$Point', '$Corner', sems0),
+    Rule('$Geometry', '$Point', sems0),
+    Rule('$Geometry', '$Edge', sems0),
+
     Rule('$DirectionCompare', '$Below', sems0),
+    Rule('$DirectionCompare', '$Above', sems0),
 ]
     
 compositional_rules = [
     Rule('$Bbox', '$Box $BoxId', sems_in_order),
-    Rule('$Float', '$Edge $Bbox', lambda (edge, bbox): ('.edge', bbox, edge)),
+    Rule('$Edge', '$Side $Bbox', lambda (edge, bbox): ('.edge', bbox, edge)),
     
-    Rule('$Bool', '$Float $DirectionCompare $Float', lambda (f1, cmp, f2): (cmp, f1, f2)),
+    Rule('$GeometryToBool', '$DirectionCompare $Geometry', sems_in_order),
+    Rule('$Bool', '$Geometry $GeometryToBool', lambda (geom, cmp): ('.call', cmp, geom)),
 
     Rule('$BboxToBool', '$Equals $Bbox', sems_in_order),
     Rule('$BboxToBool', '$NotEquals $Bbox', sems_in_order),
@@ -40,7 +53,11 @@ rules = lexical_rules + unary_rules + compositional_rules
 ops = {
     '.box': lambda int_: lambda c: c['candidate'][int_(c)],
     '.edge': lambda bbox_, side_: lambda c: c['helpers']['extract_edge'](bbox_(c), side_(c)),
-    '.below': lambda f1, f2: lambda c: c['helpers']['is_below'](f1(c), f2(c)),
+    '.center': lambda bbox_: lambda c: c['helpers']['extract_center'](bbox_(c)),
+    '.corner': lambda bbox_, s1, s2: lambda c: c['helpers']['extract_center'](bbox_(c), s1(c), s2(c)),
+
+
+    '.below': lambda g2: lambda c2: lambda g1: lambda c1: c1['helpers']['is_below'](g1(c1), g2(c2)),
 }
 
 image_grammar = GrammarMixin(
--- test/babble/image_explanations.py
+++ test/babble/image_explanations.py
@@ -1,7 +1,13 @@
 from snorkel.contrib.babble import Explanation
 
-# Box X = Person
-# Box Y = Bike
+from temp_image_class import BBox
+
+# Box X = Person (category_id = 1)
+# Box Y = Bike (category_id = 2)
+A = BBox({'bbox': (100, 100, 50, 25), 'category_id': 1}, None)
+B = BBox({'bbox': (100, 125, 50, 75), 'category_id': 2}, None)
+
+x_below_y = (B, A)
 
 boxes = [
     # Boxes
@@ -25,13 +31,13 @@ boxes = [
     Explanation(
         condition="bottom edge of Box X is below bottom edge of Box Y",
         label=False,
-        candidate=11,
+        candidate=x_below_y,
         semantics=None),
-    Explanation(
-        condition="bottom edge of Box X is below bottom edge of Box Y",
-        label=False,
-        candidate=11,
-        semantics=None),        
+    # Explanation(
+    #     condition="bottom edge of Box X is below bottom edge of Box Y",
+    #     label=False,
+    #     candidate=11,
+    #     semantics=None),        
     # # Natural 
     # Explanation(
     #     condition="Box X is very much larger than box Y.",

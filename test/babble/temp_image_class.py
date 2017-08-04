d6d1240b193184bcb284e88e768a86dd941b9bb5
Paroma Varma <varma.paroma@gmail.com>
2017-08-04 13:55:29 -0700

    updating image class in test loc

--- test/babble/temp_image_class.py
+++ test/babble/temp_image_class.py
@@ -70,3 +70,83 @@ class BBox(object):
         def bottom_edge(self):
             pass
         
+class BBox(object):
+    
+    def __init__(self, attr_dict, idx):
+        self.parent_idx = idx
+        
+        x,y,w,h = attr_dict['bbox']
+        label = attr_dict['category_id']
+        
+        self.width = w
+        self.height = h
+        
+        self.top = y
+        self.left = x
+        self.bottom = self.top+h
+        self.right = self.left+w
+        
+        def area(self):
+            return self.width*self.height
+        
+        def perimeter(self):
+            return 2*(self.width+self.height)
+        
+        def top_edge(self):
+            return self.top
+            
+        def bottom_edge(self):
+            pass
+        
+        def left_edge(self):
+            pass
+        
+        def right_edge(self):
+            pass
+        
+        def bottom_edge(self):
+            pass
+        
+class BBox(object):
+    
+    def __init__(self, attr_dict, idx):
+        self.parent_idx = idx
+        
+        x,y,w,h = attr_dict['bbox']
+        label = attr_dict['category_id']
+        
+        self.width = w
+        self.height = h
+        
+        self.top = y
+        self.left = x
+        self.bottom = self.top+h
+        self.right = self.left+w
+        
+        def area(self):
+            return self.width*self.height
+        
+        def perimeter(self):
+            return 2*(self.width+self.height)
+        
+        def top_edge(self):
+            return self.top
+            
+        def bottom_edge(self):
+            pass
+        
+        def left_edge(self):
+            pass
+        
+        def right_edge(self):
+            pass
+        
+        def bottom_edge(self):
+            pass
+        
+class Point(object):
+    
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        

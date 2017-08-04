3a343a1ff095b6b638cbfac52fea1df5735fd74b
Paroma Varma <varma.paroma@gmail.com>
2017-08-04 13:54:16 -0700

    added extractors with point object return

--- snorkel/contrib/babble/image/image_helpers.py
+++ snorkel/contrib/babble/image/image_helpers.py
@@ -1,10 +1,37 @@
+from image_objects import Point
+#Point takes in x and y (left/right and top/bottom)
+
 def extract_edge(bbox, side):
-    return getattr(bbox, side)
+    coord = getattr(bbox, side)
 
-def is_below(b1, b2):
-    return b1 > b2
+    if side in ['top','bottom']:
+        return Point(None,coord)
+    elif side in ['left','right']:
+        return Point(coord,None)
+    else:
+        raise Exception('Side is invalid')
+        
+        
+def extract_corner(bbox, side1, side2):
+    coord1 = getattr(bbox, side1)
+    coord2 = getattr(bbox, side2)
 
-helpers = {
-    'extract_edge': extract_edge,
-    'is_below': is_below,
-}
+    if side1 in ['top','bottom']:
+        if side2 in ['left','right']:
+            return Point(coord2,coord1)
+        else:
+            raise Exception('Side 2 is invalid')
+            
+    elif side1 in ['left','right']:
+        if side2 in ['top','bottom']:
+            return Point(coord1,coord2)
+        else:
+            raise Exception('Side 2 is invalid')
+    else:
+        raise Exception('Side 1 is invalid')
+
+        
+def extract_center(bbox):
+    coordx = (getattr(bbox,'left') + getattr(bbox,'right')/2.
+    coordy = (getattr(bbox,'top') + getattr(bbox,'bottom')/2.
+    return Point(coordx,coordy)

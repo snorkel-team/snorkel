20b0a63deb134c9258825f0a44032e1e47bfb033
jasontlam <jason.lam@live.ca>
2017-08-06 14:02:53 -0700

    Update elastics.py

--- elastics.py
+++ elastics.py
@@ -1,42 +1,35 @@
-
 from snorkel import SnorkelSession
-from snorkel.models import Document, Sentence
-from elasticsearch import helpers, Elasticsearch
-import math
+from elasticsearch import helpers, Elasticsearch, client
 
+#intialize snorkel and elasticsearch
 session = SnorkelSession()
 es = Elasticsearch()
 
-h=500.0
-numDocs = session.query(Document).count()
-iterations=  int(math.ceil(numDocs/h))
 
-for j in range(0,iterations):	
-	print j
-	actions = [
-	{
-	    "_index": j,
-	    "_type": p.id,
-		"sentence":i.text,
+count = 0
 
-	}
-	for p in session.query(Document).limit(h).offset(h*j) for i in p.sentences
-	]
-	helpers.bulk(es, actions,request_timeout=1000)
-
-searchResult = es.search(index = "_all",body={
+for p in session.query(Document):
+	for i in p.sentences:
+		count+=1
+		#index every sentence
+		es.index(index="corpus", doc_type="articles", id=count, body = {
+		                'lineNum': count,
+		                'sentence': i.text,
+		            })
+#search query 
+searchResult = es.search(index = "corpus",body={
 	"query": {
-		"match_phrase": {
+		"match": {
 			"sentence": {
-				"query":"Tina married Mathew",
-				"slop":10
+				"query":"married children" 
 				}
 		}
 	}
 	})
-
+#print the sentence that matches query
 print "Results : "
 print searchResult['hits']['total']
 for items in searchResult['hits']['hits']:
 	print "-------------------"
 	print items['_source']['sentence']
+

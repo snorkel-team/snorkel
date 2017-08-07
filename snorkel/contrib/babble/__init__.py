5de464a44fe2293a7988b8f28fab30d36943d387
Braden Hancock <hancock.braden@gmail.com>
2017-08-07 10:09:59 -0700

    Separate Explanation object into separate file.

--- snorkel/contrib/babble/__init__.py
+++ snorkel/contrib/babble/__init__.py
@@ -1,7 +1,8 @@
 from .babble import Babbler
+from .explanation import Explanation
 from .grammar import Rule, Grammar, Parse
 # from .model import CDRModel
 # from .model_config import configuration
-from .semparser import SemanticParser, Explanation
+from .semparser import SemanticParser
 from .snorkel_grammar import sem_to_str
 from .utils import link_explanation_candidates
--- snorkel/contrib/babble/semparser.py
+++ snorkel/contrib/babble/semparser.py
@@ -5,46 +5,7 @@ from core import core_grammar, sem_to_str
 from text import text_grammar
 from image import image_grammar
 from grammar import Grammar, validate_semantics
-
-class Explanation(object):
-    def __init__(self, condition, label, candidate=None, name=None, 
-                 semantics=None, paraphrase=None):
-        """
-        Constructs an Explanation object.
-
-        :param condition: A string explanation that expresses a Boolean 
-            condition (e.g., "The sentence is at least 5 words long.")
-        :param label: The Boolean label to apply to candidates for which the 
-            condition evaluates to True.
-        :param candidate: A candidate that the explanation is consistent with.
-        :param name: The name of this explanation.
-        :param semantics: The intended semantic representation of the 
-            explanation (if known).
-        :param paraphrase: A paraphrase of the explanation.
-        """
-        assert(isinstance(condition, basestring))
-        self.condition = condition
-        assert(isinstance(label, bool))
-        self.label = label
-        self.candidate = candidate
-        self.name = name
-        self.semantics = semantics
-        self.paraphrase = paraphrase
-
-    def __repr__(self):
-        if self.name:
-            return 'Explanation("%s: %s, %s")' % (self.name, self.label, self.condition)
-        else:
-            return 'Explanation("%s, %s")' % (self.label, self.condition)
-    
-    def display(self):
-        print 'Explanation'
-        print('%-12s %s' % ('condition', self.condition))
-        print('%-12s %d' % ('label', self.label))
-        print('%-12s %s' % ('candidate', self.candidate))
-        print('%-12s %s' % ('name', self.name))
-        print('%-12s %s' % ('semantics', self.semantics))
-
+from explanation import Explanation
 
 class SemanticParser(object):
     """

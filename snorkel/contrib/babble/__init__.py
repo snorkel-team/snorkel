27ff63e4c417ff1cfa598847a9a73e754b8c4346
Braden Hancock <hancock.braden@gmail.com>
2017-08-07 10:21:15 -0700

    Fix errant import of sem_to_str.

--- snorkel/contrib/babble/__init__.py
+++ snorkel/contrib/babble/__init__.py
@@ -1,8 +1,8 @@
 from .babble import Babbler
+from .core import sem_to_str
 from .explanation import Explanation
 from .grammar import Rule, Grammar, Parse
 # from .model import CDRModel
 # from .model_config import configuration
 from .semparser import SemanticParser
-from .snorkel_grammar import sem_to_str
 from .utils import link_explanation_candidates

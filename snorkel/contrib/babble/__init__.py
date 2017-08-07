151f867cb37c2782c46804ddde77de97462c81b7
Braden Hancock <hancock.braden@gmail.com>
2017-08-07 14:39:21 -0700

    Remove unused comments.

--- snorkel/contrib/babble/__init__.py
+++ snorkel/contrib/babble/__init__.py
@@ -2,8 +2,6 @@ from .babble import Babbler
 from .core import sem_to_str
 from .explanation import Explanation
 from .grammar import Rule, Grammar, Parse
-# from .model import CDRModel
-# from .model_config import configuration
 from .models import SnorkelModel, BabbleModel, SpouseModel, configuration
 from .semparser import SemanticParser
 from .utils import link_explanation_candidates

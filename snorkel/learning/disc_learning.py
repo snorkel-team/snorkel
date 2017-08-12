6cf10a84fb45956998b140d438658bc44273121d
ajratner <ajratner@gmail.com>
2017-08-11 18:16:29 -0700

    Fixes batch size bug

--- snorkel/learning/disc_learning.py
+++ snorkel/learning/disc_learning.py
@@ -251,7 +251,11 @@ class TFNoiseAwareModel(Classifier):
             # Iterate over batches
             batch_marginals = []
             for b in range(0, N, batch_size):
-                batch_marginals.append(self._marginals_batch(X[b:b+batch_size]))
+                batch = self._marginals_batch(X[b:b+batch_size])
+                # Note: Make sure a list is returned!
+                if min(b+batch_size, N) - b == 1:
+                    batch = np.array([batch])
+                batch_marginals.append(batch)
             return np.concatenate(batch_marginals)
 
     def save(self, model_name=None, save_dir='checkpoints', verbose=True,

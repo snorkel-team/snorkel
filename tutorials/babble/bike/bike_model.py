9a0b11cee135ac9d9bfbff91dac7568063e78338
Paroma Varma <varma.paroma@gmail.com>
2017-08-25 10:35:41 -0700

    loading gold for val and train

--- tutorials/babble/bike/bike_model.py
+++ tutorials/babble/bike/bike_model.py
@@ -35,27 +35,34 @@ class BikeModel(BabbleModel):
     def load_gold(self, anns_path=None, annotator_name='gold'):
         if anns_path:
             self.anns_path = anns_path
-        labels_by_candidate = np.load(
+        validation_labels_by_candidate = np.load(
             self.anns_path + 'labels_by_candidate.npy').tolist()
+        train_labels_by_candidate = np.load(
+            self.anns_path + 'train_labels_by_candidate.npy').tolist()
 
-        for candidate_hash, label in labels_by_candidate.items():
-            set_name, image_idx, bbox1_idx, bbox2_idx = candidate_hash.split(':')
-            source = {'train': 0, 'val': 1}[set_name]
-            stable_id_1 = "{}:{}::bbox:{}".format(source, image_idx, bbox1_idx)
-            stable_id_2 = "{}:{}::bbox:{}".format(source, image_idx, bbox2_idx)
-            context_stable_ids = "~~".join([stable_id_1, stable_id_2])
-            query = self.session.query(StableLabel).filter(StableLabel.context_stable_ids == context_stable_ids)
-            query = query.filter(StableLabel.annotator_name == annotator_name)
-            label = 1 if label else -1
-            if query.count() == 0:
-                self.session.add(StableLabel(
-                    context_stable_ids=context_stable_ids,
-                    annotator_name=annotator_name,
-                    value=label))
+        def assign_gold_labels(labels_by_candidate):
+            for candidate_hash, label in labels_by_candidate.items():
+                set_name, image_idx, bbox1_idx, bbox2_idx = candidate_hash.split(':')
+                source = {'train': 0, 'val': 1}[set_name]
+                stable_id_1 = "{}:{}::bbox:{}".format(source, image_idx, bbox1_idx)
+                stable_id_2 = "{}:{}::bbox:{}".format(source, image_idx, bbox2_idx)
+                context_stable_ids = "~~".join([stable_id_1, stable_id_2])
+                query = self.session.query(StableLabel).filter(StableLabel.context_stable_ids == context_stable_ids)
+                query = query.filter(StableLabel.annotator_name == annotator_name)
+                label = 1 if label else -1
+                if query.count() == 0:
+                    self.session.add(StableLabel(
+                        context_stable_ids=context_stable_ids,
+                        annotator_name=annotator_name,
+                        value=label))
 
-        self.session.commit()
-        reload_annotator_labels(self.session, self.candidate_class, 
-            annotator_name, split=1, filter_label_split=False)
+            self.session.commit()
+            reload_annotator_labels(self.session, self.candidate_class, 
+                annotator_name, split=source, filter_label_split=False)
+            
+            
+        assign_gold_labels(validation_labels_by_candidate)
+        assign_gold_labels(train_labels_by_candidate)
 
 
     def babble(self, explanations, user_lists={}, gold_labels=None, **kwargs):

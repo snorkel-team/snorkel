490909c05f3fa8831fb0045e33b9e10c296d450a
Cong <kongvc@gmail.com>
2017-09-17 21:30:15 +0900

    apt install bzip2 for miniconda installer

--- install/conda.sh
+++ install/conda.sh
@@ -4,6 +4,7 @@ set -ex
 [ -e $HOME/miniconda/envs/py2snorkel ] && exit
 
 curl --retry 10 -RL https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -o ~/download/miniconda.sh
+apt-get update -qy && apt-get install -qy bzip2
 bash ~/download/miniconda.sh -b -f -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
 conda update --yes conda

f234ee77f437ec94f168b089cd7eb84c4ac7fb98
Braden Hancock <hancock.braden@gmail.com>
2017-10-18 16:32:19 -0700

    Set best generative model params for bike.

--- tutorials/babble/bike/config.py
+++ tutorials/babble/bike/config.py
@@ -13,17 +13,18 @@ config = {
     
     # GENERATIVE
     'gen_params_range': {
-        'step_size'                   : [1e-3, 1e-5],
-        'decay'                       : [0.9, 0.99],
-        'reg_param'                   : [0.0, 0.01, 0.1, 0.25, 0.5],
-        'epochs'                      : [50, 100, 200]
+        # 'step_size'                   : [1e-3, 1e-5],
+        # 'decay'                       : [0.9, 0.99],
+        # 'reg_param'                   : [0.0, 0.01, 0.1, 0.25, 0.5],
+        # 'epochs'                      : [50, 100, 200]
         # 'LF_acc_prior_weights'        : [None], # Used iff lf_prior = True        
     },
     'gen_params_default': {
-    	'decay'    : 0.95,
-        'epochs'   : 50,
-        'reg_param': 0.1,
-        'LF_acc_prior_weight_default' : [0.5], # [0.5, 1.0, 1.5] = (73%, 88%, 95%) # Used iff lf_prior = True
+    	'step_size': 1e-5,
+        'decay'    : 0.9,
+        'epochs'   : 100,
+        'reg_param': 0.01,
+        'LF_acc_prior_weight_default' : 0.5, # [0.5, 1.0, 1.5] = (73%, 88%, 95%) # Used iff lf_prior = True
         'init_class_prior' : 0, # 0 = 50% pos, -1.15 = 9% pos # Used iff class_prior = True
     },
 

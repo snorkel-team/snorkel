0b9b133daa04c674e800efce4475b17039a5c5ba
Braden Hancock <hancock.braden@gmail.com>
2017-10-20 11:08:57 -0700

    Add drink config.

--- tutorials/babble/drink/config.py
+++ tutorials/babble/drink/config.py
@@ -11,12 +11,31 @@ config = {
 
     'tune_b': False,
 
+    # GENERATIVE
+    'gen_model_search_space': 1,
+    'gen_params_range': {
+        'step_size'                   : [1e-5],
+        'decay'                       : [0.9],
+        'reg_param'                   : [0.01],
+        'epochs'                      : [50]
+        # 'LF_acc_prior_weights'        : [None], # Used iff lf_prior = True        
+    },
+    'gen_params_default': {
+    	'step_size': 1e-5,
+        'decay'    : 0.9,
+        'epochs'   : 100,
+        'reg_param': 0.01,
+        'LF_acc_prior_weight_default' : 0.5, # [0.5, 1.0, 1.5] = (73%, 88%, 95%) # Used iff lf_prior = True
+        'init_class_prior' : 0, # 0 = 50% pos, -1.15 = 9% pos # Used iff class_prior = True
+    },
+
+    # DISCRIMINATIVE
     'disc_model_class': 'inception_v3',
     'print_freq': 1,
     'optimizer': 'adam',
+    'opt_epsilon': 1.0,
     'disc_params_search': {
-        'lr'        : [1, 50],
-        'max_steps' : [3, 5]
-    },
-    
+        'lr'        : [.01, .1, 1, 10, 100],
+        'max_steps' : [100, 200, 500]
+    },   
 }

6a374c153d33a2a09a3088b47c3e5d5f87d4a0ce
Braden Hancock <hancock.braden@gmail.com>
2017-11-06 13:55:12 -0800

    Add new disc model flags to run.py.

--- snorkel/contrib/babble/pipelines/run.py
+++ snorkel/contrib/babble/pipelines/run.py
@@ -63,6 +63,11 @@ if __name__ == '__main__':
     argparser.add_argument('--disc_params_search:lr', type=float, action='append')
     argparser.add_argument('--disc_params_search:weight_decay', type=float, action='append')
     argparser.add_argument('--disc_params_search:max_steps', type=int, action='append')
+    
+    argparser.add_argument('--disc_params_default:batch_size', type=int, action='append')
+    argparser.add_argument('--disc_params_default:n_epochs', type=int, action='append')
+    argparser.add_argument('--disc_params_default:lr', type=float, action='append')
+    argparser.add_argument('--disc_params_default:rebalance', type=float, action='append')
 
 
     # Search

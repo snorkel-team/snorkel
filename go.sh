1bc020e1af7a09a3eb7b4b83c78f829fcd6fe5c0
Braden Hancock <hancock.braden@gmail.com>
2017-11-27 16:42:06 -0800

    Update go.sh with separate report dir for each run.

--- go.sh
+++ go.sh
@@ -1,31 +1,41 @@
-source set_env.sh
-
 DOMAIN=$1
 EXP=$2
 
 DATE=`date +"%m_%d_%y"`
 TIME=`date +"%H_%M_%S"`
+
 LOGDIR="logs/$DATE"
 mkdir -p $LOGDIR
-LOGFILE="$LOGDIR/run_log_${DOMAIN}_${EXP}_${TIME}.log"
-echo "Saving log to '$LOGFILE'"
 
-REPORTS_DIR="reports/$DATE/${DOMAIN}_${EXP}/"
+REPORTS_DIR="reports/$DATE"
 mkdir -p $REPORTS_DIR
-echo "Saving reports to '$REPORTS_DIR'"
-echo ""
-echo "Note: If you are not starting at stage 0, confirm database exists already."
 
 echo ""
 echo "<TEST:>"
 echo ""
+
+for temp in 1 2
+do
+
+RUN="${DOMAIN}_${EXP}_${TIME}_${TEMP}"
+
+REPORTS_SUBDIR="$REPORTS_DIR/$RUN/"
+mkdir -p $REPORTS_SUBDIR
+echo "Saving reports to '$REPORTS_SUBDIR'"
+
+LOGFILE="$LOGDIR/$RUN.log"
+echo "Saving log to '$LOGFILE'"
+
 python -u snorkel/contrib/babble/pipelines/run.py \
     --domain $DOMAIN \
-    --reports_dir $REPORTS_DIR \
-    --start_at 7 \
+    --reports_dir $REPORTS_SUBDIR \
+    --start_at 8 \
     --end_at 10 \
     --supervision traditional \
     --gen_model_search_space 1 \
-    --disc_model_search_space 1 \
-    --disc_params_range:max_steps 1 \
-    --seed 1023 --verbose --no_plots |& tee -a $LOGFILE
+    --max_train 500 \
+    --disc_model_class logreg \
+    --disc_model_search_space 2 \
+    --disc_params_range:n_epochs 2 \
+    --verbose --no_plots |& tee -a $LOGFILE
+done

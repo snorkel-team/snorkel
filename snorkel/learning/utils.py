87e615bae1b266ac963ba8d9f0168ccd9ace9f48
ajratner <ajratner@gmail.com>
2017-12-21 23:25:50 -0500

    Fixing Python 2 bug in learning.utils; adding python 3.6 to Travis

--- snorkel/learning/utils.py
+++ snorkel/learning/utils.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from future import standard_library
 standard_library.install_aliases()
-from builtins import *
 
 import os
 import math

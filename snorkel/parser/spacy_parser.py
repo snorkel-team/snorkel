3ed65330fe8d4e40c0ca4c8ac5fe0340c91b70eb
Shawn Roberts <roberts.shawn513@gmail.com>
2018-02-18 16:43:34 -0500

    updated spacy_parser.py for spacy 2.0

--- snorkel/parser/spacy_parser.py
+++ snorkel/parser/spacy_parser.py
@@ -12,7 +12,10 @@ try:
     import spacy
     from spacy.cli import download
     from spacy import util
-    from spacy.deprecated import resolve_model_name
+    try:
+        spacy_version=int(spacy.__version__[0])
+    except:
+        spacy_version=1
 except:
     raise Exception("spaCy not installed. Use `pip install spacy`.")
 
@@ -57,8 +60,12 @@ class Spacy(Parser):
         self.num_threads = num_threads
 
         self.pipeline = []
-        for proc in annotators:
-            self.pipeline += [self.model.__dict__[proc]]
+        if spacy_version==1:
+            for proc in annotators:
+                self.pipeline += [self.model.__dict__[proc]]
+        else:
+            for i,proc in enumerate(['tagger','parser','ner']):
+                self.pipeline += [self.model.pipeline[i][1]]
 
     @staticmethod
     def model_installed(name):
@@ -68,8 +75,7 @@ class Spacy(Parser):
         :return:
         '''
         data_path = util.get_data_path()
-        model_name = resolve_model_name(name)
-        model_path = data_path / model_name
+        model_path = data_path / name
         return model_path.exists()
 
     @staticmethod

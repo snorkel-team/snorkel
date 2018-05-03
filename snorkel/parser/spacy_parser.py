ff1785e35c749f6674b15fc9b2cec96b4fc0718c
Stephen Bach <stephenhbach@gmail.com>
2018-05-03 15:33:54 -0700

    More robust checking for Spacy language models.

--- snorkel/parser/spacy_parser.py
+++ snorkel/parser/spacy_parser.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import *
 
+import pkg_resources
+from pathlib import Path
 from collections import defaultdict
 from snorkel.models import construct_stable_id
 from snorkel.parser import Parser, ParserConnection
@@ -13,9 +15,9 @@ try:
     from spacy.cli import download
     from spacy import util
     try:
-        spacy_version=int(spacy.__version__[0])
+        spacy_version = int(spacy.__version__[0])
     except:
-        spacy_version=1
+        spacy_version = 1
 except:
     raise Exception("spaCy not installed. Use `pip install spacy`.")
 
@@ -60,24 +62,50 @@ class Spacy(Parser):
         self.num_threads = num_threads
 
         self.pipeline = []
-        if spacy_version==1:
+        if spacy_version == 1:
             for proc in annotators:
                 self.pipeline += [self.model.__dict__[proc]]
         else:
-            annotators=[i if i!='entity' else 'ner' for i in annotators]
-            for i,proc in enumerate(annotators):
+            annotators=[i if i != 'entity' else 'ner' for i in annotators]
+            for i, proc in enumerate(annotators):
                 self.pipeline += [self.model.pipeline[i][1]]
+
+    @staticmethod
+    def is_package(name):
+        """Check if string maps to a package installed via pip.
+        name (unicode): Name of package.
+        RETURNS (bool): True if installed package, False if not.
+
+        From https://github.com/explosion/spaCy/blob/master/spacy/util.py
+
+        """
+        name = name.lower()  # compare package name against lowercase name
+        packages = pkg_resources.working_set.by_key.keys()
+        for package in packages:
+            if package.lower().replace('-', '_') == name:
+                return True
+            return False
 
     @staticmethod
     def model_installed(name):
         '''
         Check if spaCy language model is installed
+
+        From https://github.com/explosion/spaCy/blob/master/spacy/util.py
+
         :param name:
         :return:
         '''
         data_path = util.get_data_path()
-        model_path = data_path / name
-        return model_path.exists()
+        if not data_path or not data_path.exists():
+            raise IOError("Can't find spaCy data path: %s" % str(data_path))
+        if name in set([d.name for d in data_path.iterdir()]):
+            return True
+        if Spacy.is_package(name): # installed as package
+            return True
+        if Path(name).exists(): # path to model data directory
+            return True
+        return False
 
     @staticmethod
     def load_lang_model(lang):

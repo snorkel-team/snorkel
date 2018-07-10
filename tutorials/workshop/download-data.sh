6089dd8462c16a25d082e4cb9d17d40adb449102
Jason Alan Fries <fries.jason@gmail.com>
2018-07-09 22:14:34 -0700

    Added database instance download script. Added instructions to first tutorial to make db install clearer

---
+++ tutorials/workshop/download-data.sh
@@ -0,0 +1,6 @@
+#
+# Download database instance
+# snorkel.db created with Workshop_5_Advanced_Preprocessing.ipynb
+#
+wget https://www.dropbox.com/s/2tbk0zy8ezys4q9/snorkel.db.bz2
+bunzip2 snorkel.db.bz2

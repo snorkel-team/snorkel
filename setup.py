87bbe204c31712bf3d6de5055545ee0ba4d697f0
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-12 01:52:08 +0000

    Add removed files to commit

--- setup.py
+++ setup.py
@@ -1,46 +1,34 @@
 import os
-import re
 
-import setuptools
+from setuptools import find_packages, setup
 
-directory = os.path.dirname(os.path.abspath(__file__))
-
-# Extract version information
-path = os.path.join(directory, 'snorkel', '__init__.py')
-with open(path) as read_file:
-    text = read_file.read()
-pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.MULTILINE)
-version = pattern.search(text).group(1)
-
-# Extract long_description
-path = os.path.join(directory, 'README.md')
-with open(path) as read_file:
+with open("README.md") as read_file:
     long_description = read_file.read()
 
-setuptools.setup(
-    name='snorkel',
-    version=version,
-    url='https://github.com/HazyResearch/snorkel',
-    description='A system for quickly generating training data with weak supervision',
-    long_description_content_type='text/markdown',
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="snorkel",
+    version="0.9.0",
+    url="https://github.com/HazyResearch/snorkel",
+    description="A system for quickly generating training data with weak supervision",
+    long_description_content_type="text/markdown",
     long_description=long_description,
-    license='Apache License 2.0',
-    packages=setuptools.find_packages(),
+    license="Apache License 2.0",
+    packages=find_packages(),
     include_package_data=True,
-
-    keywords='machine-learning ai information-extraction weak-supervision',
+    keywords="machine-learning ai information-extraction weak-supervision",
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
     ],
-
-    project_urls={  # Optional
-        'Homepage': 'https://hazyresearch.github.io/snorkel/',
-        'Source': 'https://github.com/HazyResearch/snorkel/',
-        'Bug Reports': 'https://github.com/HazyResearch/snorkel/issues',
-        'Citation': 'https://doi.org/10.14778/3157794.3157797',
+    project_urls={
+        "Homepage": "https://hazyresearch.github.io/snorkel/",
+        "Source": "https://github.com/HazyResearch/snorkel/",
+        "Bug Reports": "https://github.com/HazyResearch/snorkel/issues",
+        "Citation": "https://doi.org/10.14778/3157794.3157797",
     },
 )
--- tutorials/workshop/download-data.sh
+++
@@ -1,6 +0,0 @@
-#
-# Download database instance
-# snorkel.db created with Workshop_5_Advanced_Preprocessing.ipynb
-#
-wget https://www.dropbox.com/s/2tbk0zy8ezys4q9/snorkel.db.bz2
-bunzip2 snorkel.db.bz2

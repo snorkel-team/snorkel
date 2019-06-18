07e8f668ea728ef8ef188814722312f5a15b008e
Braden Hancock <hancock.braden@gmail.com>
2019-06-18 16:59:40 -0700

    Rename utils test to avoid naming conflict

--- test/mtl/test_mtl_utils.py
+++ test/mtl/test_mtl_utils.py
@@ -5,51 +5,51 @@ import torch
 from snorkel.mtl.utils import list_to_tensor, pad_batch
 
 
-class DatasetTest(unittest.TestCase):
+class UtilsTest(unittest.TestCase):
     def test_pad_batch(self):
         batch = [torch.Tensor([1, 2]), torch.Tensor([3]), torch.Tensor([4, 5, 6])]
         padded_batch, mask_batch = pad_batch(batch)
 
-        self.assertTure(
+        self.assertTrue(
             torch.equal(padded_batch, torch.Tensor([[1, 2, 0], [3, 0, 0], [4, 5, 6]]))
         )
-        self.assertTure(
+        self.assertTrue(
             torch.equal(mask_batch, torch.Tensor([[0, 0, 1], [0, 1, 1], [0, 0, 0]]))
         )
 
         padded_batch, mask_batch = pad_batch(batch, max_len=2)
 
-        self.assertTure(
+        self.assertTrue(
             torch.equal(padded_batch, torch.Tensor([[1, 2], [3, 0], [4, 5]]))
         )
-        self.assertTure(torch.equal(mask_batch, torch.Tensor([[0, 0], [0, 1], [0, 0]])))
+        self.assertTrue(torch.equal(mask_batch, torch.Tensor([[0, 0], [0, 1], [0, 0]])))
 
         padded_batch, mask_batch = pad_batch(batch, pad_value=-1)
 
-        self.assertTure(
+        self.assertTrue(
             torch.equal(
                 padded_batch, torch.Tensor([[1, 2, -1], [3, -1, -1], [4, 5, 6]])
             )
         )
-        self.assertTure(
+        self.assertTrue(
             torch.equal(mask_batch, torch.Tensor([[0, 0, 1], [0, 1, 1], [0, 0, 0]]))
         )
 
         padded_batch, mask_batch = pad_batch(batch, left_padded=True)
 
-        self.assertTure(
+        self.assertTrue(
             torch.equal(padded_batch, torch.Tensor([[0, 1, 2], [0, 0, 3], [4, 5, 6]]))
         )
-        self.assertTure(
+        self.assertTrue(
             torch.equal(mask_batch, torch.Tensor([[1, 0, 0], [1, 1, 0], [0, 0, 0]]))
         )
 
         padded_batch, mask_batch = pad_batch(batch, max_len=2, left_padded=True)
 
-        self.assertTure(
+        self.assertTrue(
             torch.equal(padded_batch, torch.Tensor([[1, 2], [0, 3], [5, 6]]))
         )
-        self.assertTure(torch.equal(mask_batch, torch.Tensor([[0, 0], [1, 0], [0, 0]])))
+        self.assertTrue(torch.equal(mask_batch, torch.Tensor([[0, 0], [1, 0], [0, 0]])))
 
     def test_list_to_tensor(self):
         # list of 1-D tensor with the different length

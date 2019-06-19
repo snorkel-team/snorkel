1bfce5ff54b5cba83341f67258eb860d484e2172
Alex Ratner <ajratner@gmail.com>
2019-06-19 12:58:47 -0700

    Refactored synthetic data generators: - Changed to util fns - Moved to snorkel - Added tests

--- tutorials/synthetic/synthetic.py
+++ tutorials/synthetic/synthetic.py
@@ -1,68 +1,61 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+from snorkel.labeling.apply import LFApplier
+from snorkel.synthetic.synthetic_data import (
+    generate_mog_dataset,
+    generate_single_feature_LFs,
+)
+
 #%% [markdown]
 # ## Simple Synthetic Data Example
 # In this example, we generate a simple synthetic dataset, and then:
 #
 #   1. Apply labeling functions of various accuracies to it
 #   2. Train a `LabelModel` to reweight and combine their outputs
-#   3. And finally train an `EndModel` using the generated training labels 
+#   3. And finally train an `EndModel` using the generated training labels
 
 #%%
-import numpy as np
-import matplotlib.pyplot as plt
-
-# TODO: Turn this into a utility function
-# Generate d-dim data from two Gaussians
+# Generating a simple mixture of gaussians dataset of d-dim one-hot vectors
+# as a pandas DataFrame
 n = 1000
 d = 10
 cov = np.diag(np.random.random(d))
-X_pos = np.random.multivariate_normal(np.ones(d), cov, int(np.floor(n/2)))
-X_neg = np.random.multivariate_normal(-1 * np.ones(d), cov, int(np.ceil(n/2)))
-
-# Combine and shuffle
-X = np.vstack([X_pos, X_neg])
-Y = np.concatenate([
-    np.ones(int(np.floor(n/2))),
-    -1 * np.ones(int(np.ceil(n/2)))
-])
-order = list(range(n))
-np.random.shuffle(order)
-X = X[order]
-Y = Y[order]
+cov[0, 0] = 0.0
+data = generate_mog_dataset(n, d, cov=cov)
 
 #%%
+X_pos = np.vstack([d.x for d in data if d.y == 1])
+X_neg = np.vstack([d.x for d in data if d.y == -1])
+
 # Plotting the first two dimensions of the data
-plt.scatter(X_pos[:,0], X_pos[:,1], color='red')
-plt.scatter(X_neg[:,0], X_neg[:,1], color='blue')
+plt.scatter(X_pos[:, 0], X_pos[:, 1], color="red")
+plt.scatter(X_neg[:, 0], X_neg[:, 1], color="blue")
 plt.show()
 
 #%%
-from snorkel.labeling.lf import LabelingFunction
-from functools import partial
-
-def lf_template(x, index=0):
-    if x[index] > 0:
-        return 1
-    else:
-        return -1
-
-# Generate a set of m LFs that each label based on one feature of the data
-m = int(d/2)
-LFs = []
-for i in range(m):
-    LFs.append(LabelingFunction(f"LF_feat_{i}", partial(lf_template, index=i)))
+# Generate a set of m LFs that each fire based on a single feature
+m = 5
+abstain_rate = 0.0
+LFs = generate_single_feature_LFs(m, abstain_rate=abstain_rate)
 
 #%%
-from snorkel.labeling.apply import LFApplier
-
 # Apply the labeling functions to the data
 lf_applier = LFApplier(LFs)
-L = lf_applier.apply(X)
+L = lf_applier.apply(data)
 
 #%%
 # Compute the true (empirical) LF accuracies
 Ld = np.array(L.todense())
+Y = np.array([d.y for d in data])
 accs = Ld.T @ Y / n
+accs
 
 #%%
 # Run the LabelModel to estimate the LF accuracies
+# TODO
+
+#%%
+# Run the EndModel to make final predictions
 # TODO

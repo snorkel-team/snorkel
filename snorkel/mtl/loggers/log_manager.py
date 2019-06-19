730d912f19d868ce01e979f959aadb7e16a8edb4
Braden Hancock <hancock.braden@gmail.com>
2019-06-19 09:55:29 -0700

    Move logger_default_config out of __init__.py

--- snorkel/mtl/loggers/log_manager.py
+++ snorkel/mtl/loggers/log_manager.py
@@ -5,12 +5,27 @@ from typing import Dict
 
 import torch
 
-from snorkel.mtl.loggers import logging_default_config
 from snorkel.mtl.loggers.checkpointer import Checkpointer
 from snorkel.mtl.loggers.log_writer import LogWriter
 from snorkel.mtl.loggers.tensorboard_writer import TensorBoardWriter
 from snorkel.mtl.utils import recursive_merge_dicts
 from snorkel.types import Config
+
+logger_default_config = {
+    "counter_unit": "batches",  # [points, batches, epochs]
+    "log_dir": "logs",  # The path to the directory under which logs will be written
+    "evaluation_freq": 2,  # Evaluate performance every this many counter_units
+    "writer_config": {"writer": "tensorboard", "verbose": True},  # [json, tensorboard]
+    "checkpointing": True,
+    "checkpointer_config": {
+        "checkpoint_dir": None,
+        "checkpoint_factor": 1,  # Checkpoint every this many evaluations
+        "checkpoint_metric": "model/train/all/loss:min",
+        "checkpoint_task_metrics": None,  # task_metric_name:mode
+        "checkpoint_runway": 0,  # checkpointing runway (no checkpointing before k unit)
+        "checkpoint_clear": True,  # whether to clear intermediate checkpoints
+    },
+}
 
 
 class LogManager(object):
@@ -24,7 +39,7 @@ class LogManager(object):
 
     def __init__(self, config: Config, n_batches_per_epoch: int) -> None:
 
-        self.config = recursive_merge_dicts(logging_default_config, config)
+        self.config = recursive_merge_dicts(logger_default_config, config)
         self.n_batches_per_epoch = n_batches_per_epoch
 
         # Create log directory for this run

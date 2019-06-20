eb80ca77f83f583c45a131aa5236d2ed3848bb18
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-20 16:37:25 -0400

    Add LambdaPreprocessor and decorator (#1136)
    * Add LambdaPreprocessor and decorator
    
    * Address comments

--- test/labeling/preprocess/test_core.py
+++ test/labeling/preprocess/test_core.py
@@ -1,7 +1,7 @@
 import unittest
 from types import SimpleNamespace
 
-from snorkel.labeling.preprocess import Preprocessor, PreprocessorMode
+from snorkel.labeling.preprocess import Preprocessor, PreprocessorMode, preprocessor
 from snorkel.types import FieldMap
 
 
@@ -15,45 +15,40 @@ class SplitWordsPreprocessor(Preprocessor):
         return dict(lower=text.lower(), words=text.split())
 
 
-class SquarePreprocessor(Preprocessor):
-    def __init__(self, x_field: str, squared_x_field: str) -> None:
-        super().__init__(dict(x=x_field), dict(x=squared_x_field))
-
-    def preprocess(self, x: float) -> FieldMap:  # type: ignore
-        return dict(x=x ** 2)
+@preprocessor
+def square(num: float) -> FieldMap:
+    return dict(num_squared=num ** 2)
 
 
 class TestPreprocessorCore(unittest.TestCase):
     def _get_x(self) -> SimpleNamespace:
-        return SimpleNamespace(a=8, b="Henry has fun")
+        return SimpleNamespace(num=8, text="Henry has fun")
 
     def test_numeric_preprocessor(self) -> None:
-        square = SquarePreprocessor("a", "c")
         square.set_mode(PreprocessorMode.NAMESPACE)
         x_preprocessed = square(self._get_x())
-        self.assertEqual(x_preprocessed.a, 8)
-        self.assertEqual(x_preprocessed.b, "Henry has fun")
-        self.assertEqual(x_preprocessed.c, 64)
+        self.assertEqual(x_preprocessed.num, 8)
+        self.assertEqual(x_preprocessed.text, "Henry has fun")
+        self.assertEqual(x_preprocessed.num_squared, 64)
 
     def test_text_preprocessor(self) -> None:
-        split_words = SplitWordsPreprocessor("b", "c", "d")
+        split_words = SplitWordsPreprocessor("text", "text_lower", "text_words")
         split_words.set_mode(PreprocessorMode.NAMESPACE)
         x_preprocessed = split_words(self._get_x())
-        self.assertEqual(x_preprocessed.a, 8)
-        self.assertEqual(x_preprocessed.b, "Henry has fun")
-        self.assertEqual(x_preprocessed.c, "henry has fun")
-        self.assertEqual(x_preprocessed.d, ["Henry", "has", "fun"])
+        self.assertEqual(x_preprocessed.num, 8)
+        self.assertEqual(x_preprocessed.text, "Henry has fun")
+        self.assertEqual(x_preprocessed.text_lower, "henry has fun")
+        self.assertEqual(x_preprocessed.text_words, ["Henry", "has", "fun"])
 
     def test_preprocessor_same_field(self) -> None:
-        split_words = SplitWordsPreprocessor("b", "b", "c")
+        split_words = SplitWordsPreprocessor("text", "text", "text_words")
         split_words.set_mode(PreprocessorMode.NAMESPACE)
         x_preprocessed = split_words(self._get_x())
-        self.assertEqual(x_preprocessed.a, 8)
-        self.assertEqual(x_preprocessed.b, "henry has fun")
-        self.assertEqual(x_preprocessed.c, ["Henry", "has", "fun"])
+        self.assertEqual(x_preprocessed.num, 8)
+        self.assertEqual(x_preprocessed.text, "henry has fun")
+        self.assertEqual(x_preprocessed.text_words, ["Henry", "has", "fun"])
 
     def test_preprocessor_mode(self) -> None:
-        square = SquarePreprocessor("a", "c")
         x = self._get_x()
 
         square.set_mode(18)  # type: ignore

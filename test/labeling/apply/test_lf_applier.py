f287cd9277ed1e1c7a745af4429cbb3894a7c788
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-20 16:37:25 -0400

    Add LambdaPreprocessor and decorator (#1136)
    * Add LambdaPreprocessor and decorator
    
    * Address comments

--- test/labeling/apply/test_lf_applier.py
+++ test/labeling/apply/test_lf_applier.py
@@ -9,36 +9,32 @@ import pyarrow.parquet as pq
 
 from snorkel.labeling.apply import LFApplier, PandasLFApplier
 from snorkel.labeling.lf import labeling_function
-from snorkel.labeling.preprocess import Preprocessor
+from snorkel.labeling.preprocess import preprocessor
 from snorkel.labeling.preprocess.nlp import SpacyPreprocessor
 from snorkel.types import DataPoint, FieldMap
 
 
-class SquarePreprocessor(Preprocessor):
-    def __init__(self, x_field: str, squared_x_field: str) -> None:
-        super().__init__(dict(x=x_field), dict(x=squared_x_field))
-
-    def preprocess(self, x: float) -> FieldMap:  # type: ignore
-        return dict(x=x ** 2)
+@preprocessor
+def square(num: float) -> FieldMap:
+    return dict(num_squared=num ** 2)
 
 
-square = SquarePreprocessor(x_field="a", squared_x_field="a")
 spacy = SpacyPreprocessor(text_field="text", doc_field="doc")
 
 
 @labeling_function()
 def f(x: DataPoint) -> int:
-    return 1 if x.a > 42 else 0
+    return 1 if x.num > 42 else 0
 
 
 @labeling_function(preprocessors=[square])
 def fp(x: DataPoint) -> int:
-    return 1 if x.a > 42 else 0
+    return 1 if x.num_squared > 42 else 0
 
 
 @labeling_function(resources=dict(db=[3, 6, 9]))
 def g(x: DataPoint, db: List[int]) -> int:
-    return 1 if x.a in db else 0
+    return 1 if x.num in db else 0
 
 
 @labeling_function(preprocessors=[spacy])
@@ -61,25 +57,25 @@ L_TEXT_EXPECTED = np.array([[1, 0], [1, 1]])
 
 class TestLFApplier(unittest.TestCase):
     def test_lf_applier(self) -> None:
-        data_points = [SimpleNamespace(a=a) for a in DATA]
+        data_points = [SimpleNamespace(num=num) for num in DATA]
         applier = LFApplier([f, g])
         L = applier.apply(data_points)
         np.testing.assert_equal(L.toarray(), L_EXPECTED)
 
     def test_lf_applier_preprocessor(self) -> None:
-        data_points = [SimpleNamespace(a=a) for a in DATA]
+        data_points = [SimpleNamespace(num=num) for num in DATA]
         applier = LFApplier([f, fp])
         L = applier.apply(data_points)
         np.testing.assert_equal(L.toarray(), L_PREPROCESS_EXPECTED)
 
     def test_lf_applier_pandas(self) -> None:
-        df = pd.DataFrame(dict(a=DATA))
+        df = pd.DataFrame(dict(num=DATA))
         applier = PandasLFApplier([f, g])
         L = applier.apply(df)
         np.testing.assert_equal(L.toarray(), L_EXPECTED)
 
     def test_lf_applier_pandas_preprocessor(self) -> None:
-        df = pd.DataFrame(dict(a=DATA))
+        df = pd.DataFrame(dict(num=DATA))
         applier = PandasLFApplier([f, fp])
         L = applier.apply(df)
         np.testing.assert_equal(L.toarray(), L_PREPROCESS_EXPECTED)
@@ -91,7 +87,7 @@ class TestLFApplier(unittest.TestCase):
         np.testing.assert_equal(L.toarray(), L_TEXT_EXPECTED)
 
     def test_lf_applier_pandas_parquet(self) -> None:
-        table_write = pa.Table.from_pandas(pd.DataFrame(dict(a=DATA)))
+        table_write = pa.Table.from_pandas(pd.DataFrame(dict(num=DATA)))
         stream = pa.BufferOutputStream()
         pq.write_table(table_write, stream)
         buffer = stream.getvalue()
--- test/labeling/test_lf.py
+++ test/labeling/test_lf.py
@@ -4,40 +4,37 @@ from types import SimpleNamespace
 from typing import List
 
 from snorkel.labeling.lf import LabelingFunction, labeling_function
-from snorkel.labeling.preprocess import Preprocessor, PreprocessorMode
+from snorkel.labeling.preprocess import PreprocessorMode, preprocessor
 from snorkel.types import DataPoint, FieldMap
 
 
-class SquarePreprocessor(Preprocessor):
-    def __init__(self, x_field: str, squared_x_field: str) -> None:
-        super().__init__(dict(x=x_field), dict(x=squared_x_field))
-
-    def preprocess(self, x: float) -> FieldMap:  # type: ignore
-        return dict(x=x ** 2)
+@preprocessor
+def square(num: float) -> FieldMap:
+    return dict(num=num ** 2)
 
 
 def f(x: DataPoint) -> int:
-    return 1 if x.a > 42 else 0
+    return 1 if x.num > 42 else 0
 
 
 def g(x: DataPoint, db: List[int]) -> int:
-    return 1 if x.a in db else 0
+    return 1 if x.num in db else 0
 
 
 class TestLabelingFunctionCore(unittest.TestCase):
     def _run_lf(self, lf: LabelingFunction) -> None:
-        x_43 = SimpleNamespace(a=43)
-        x_19 = SimpleNamespace(a=19)
+        x_43 = SimpleNamespace(num=43)
+        x_19 = SimpleNamespace(num=19)
         self.assertEqual(lf(x_43), 1)
         self.assertEqual(lf(x_19), 0)
 
     def _run_lf_raise(self, lf: LabelingFunction) -> None:
-        x_none = SimpleNamespace(a=None)
+        x_none = SimpleNamespace(num=None)
         with self.assertRaises(TypeError):
             lf(x_none)
 
     def _run_lf_no_raise(self, lf: LabelingFunction) -> None:
-        x_none = SimpleNamespace(a=None)
+        x_none = SimpleNamespace(num=None)
         self.assertEqual(lf(x_none), 0)
 
     def test_labeling_function(self) -> None:
@@ -57,12 +54,11 @@ class TestLabelingFunctionCore(unittest.TestCase):
         self._run_lf_no_raise(lf)
 
     def test_labeling_function_preprocessor(self) -> None:
-        p = SquarePreprocessor(x_field="a", squared_x_field="a")
-        lf = LabelingFunction(name="my_lf", f=f, preprocessors=[p, p])
+        lf = LabelingFunction(name="my_lf", f=f, preprocessors=[square, square])
         lf.set_preprocessor_mode(PreprocessorMode.NAMESPACE)
-        x_43 = SimpleNamespace(a=43)
-        x_6 = SimpleNamespace(a=6)
-        x_2 = SimpleNamespace(a=2)
+        x_43 = SimpleNamespace(num=43)
+        x_6 = SimpleNamespace(num=6)
+        x_2 = SimpleNamespace(num=2)
         self.assertEqual(lf(x_43), 1)
         self.assertEqual(lf(x_6), 1)
         self.assertEqual(lf(x_2), 0)
@@ -77,7 +73,7 @@ class TestLabelingFunctionCore(unittest.TestCase):
     def test_labeling_function_decorator(self) -> None:
         @labeling_function()
         def lf(x: DataPoint) -> int:
-            return 1 if x.a > 42 else 0
+            return 1 if x.num > 42 else 0
 
         self.assertEqual(lf.name, "lf")
         self._run_lf(lf)
@@ -86,7 +82,7 @@ class TestLabelingFunctionCore(unittest.TestCase):
     def test_labeling_function_decorator_args(self) -> None:
         @labeling_function(name="my_lf", fault_tolerant=True)
         def lf(x: DataPoint) -> int:
-            return 1 if x.a > 42 else 0
+            return 1 if x.num > 42 else 0
 
         self.assertEqual(lf.name, "my_lf")
         self._run_lf(lf)

48438012789d4df3ea7e8c982f4b15492fc5f69e
Braden Hancock <hancock.braden@gmail.com>
2019-06-20 12:07:28 -0700

    Add comment to mtl/modules/utils.py

--- snorkel/mtl/modules/utils.py
+++ snorkel/mtl/modules/utils.py
@@ -1,8 +1,6 @@
 """
 For compatibility with SuperGLUE tutorials, we we use these wrappers around
 standard pytorch functional operators.
-TODO: Redesign MTL model construction so that regular F.cross_entropy and
-F.softmax can be used directly.
 """
 
 
@@ -10,6 +8,8 @@ import torch.nn.functional as F
 
 
 def ce_loss(module_name, outputs, Y, active):
+    # Subtract 1 from Ys to account for Snorkel reserving the label 0 for abstains
+    # (but F.cross_entropy() expect 0-indexed labels)
     return F.cross_entropy(outputs[module_name][0][active], (Y.view(-1) - 1)[active])
 
 

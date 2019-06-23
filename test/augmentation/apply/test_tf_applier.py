89d034d88c73d157d52a7e19fe31697e87c4c714
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-23 01:45:14 -0400

    Add augmentation module (#1153)
    * Add augmentation module
    
    * Address comments
    
    * Add augmentation to synthetic tutorial (#1155)
    
    * Add augmentation to synthetic tutorial
    
    * Update synthetic unit tests
    
    * Updates
    
    * Change LambdaMapper signature (#1158)

---
+++ test/augmentation/apply/test_tf_applier.py
@@ -0,0 +1,126 @@
+import unittest
+from types import SimpleNamespace
+
+import pandas as pd
+
+from snorkel.augmentation.apply import PandasTFApplier, TFApplier
+from snorkel.augmentation.policy import RandomAugmentationPolicy
+from snorkel.augmentation.tf import transformation_function
+from snorkel.types import DataPoint
+
+
+@transformation_function
+def square(x: DataPoint) -> DataPoint:
+    x.num = x.num ** 2
+    return x
+
+
+@transformation_function
+def modify_in_place(x: DataPoint) -> DataPoint:
+    x.d["my_key"] = 0
+    return x
+
+
+policy = RandomAugmentationPolicy(1, sequence_length=2)
+policy_modify_in_place = RandomAugmentationPolicy(1, sequence_length=1)
+
+
+DATA = [1, 2, 3]
+DATA_IN_PLACE_EXPECTED = [
+    dict(my_key=(1 + i // 3) if i % 3 == 0 else 0) for i in range(9)
+]
+
+
+# NB: reconstruct each time to avoid inplace updates
+def get_data_dict():
+    return [dict(my_key=num) for num in DATA]
+
+
+class TestTFApplier(unittest.TestCase):
+    def _get_x_namespace(self):
+        return [SimpleNamespace(num=num) for num in DATA]
+
+    def _get_x_namespace_dict(self):
+        return [SimpleNamespace(d=d) for d in get_data_dict()]
+
+    def test_tf_applier(self):
+        data = self._get_x_namespace()
+        applier = TFApplier([square], policy, k=1, keep_original=False)
+        data_augmented = applier.apply(data)
+        vals = [x.num for x in data_augmented]
+        self.assertEqual(vals, [1, 16, 81])
+        self.assertEqual([x.num for x in data], DATA)
+
+    def test_tf_applier_multi(self):
+        data = self._get_x_namespace()
+        applier = TFApplier([square], policy, k=2, keep_original=False)
+        data_augmented = applier.apply(data)
+        vals = [x.num for x in data_augmented]
+        self.assertEqual(vals, [1, 1, 16, 16, 81, 81])
+        self.assertEqual([x.num for x in data], DATA)
+
+    def test_tf_applier_keep_original(self):
+        data = self._get_x_namespace()
+        applier = TFApplier([square], policy, k=2, keep_original=True)
+        data_augmented = applier.apply(data)
+        vals = [x.num for x in data_augmented]
+        self.assertEqual(vals, [1, 1, 1, 2, 16, 16, 3, 81, 81])
+        self.assertEqual([x.num for x in data], DATA)
+
+    def test_tf_applier_keep_original_modify_in_place(self):
+        data = self._get_x_namespace_dict()
+        applier = TFApplier(
+            [modify_in_place], policy_modify_in_place, k=2, keep_original=True
+        )
+        data_augmented = applier.apply(data)
+        self.assertTrue(
+            all(x.d == d for x, d in zip(data_augmented, DATA_IN_PLACE_EXPECTED))
+        )
+        self.assertTrue(all(x.d == y for x, y in zip(data, get_data_dict())))
+
+
+class TestPandasTFApplier(unittest.TestCase):
+    def _get_x_df(self):
+        return pd.DataFrame(dict(num=DATA))
+
+    def _get_x_df_dict(self):
+        return pd.DataFrame(dict(d=get_data_dict()))
+
+    def test_tf_applier_pandas(self):
+        df = self._get_x_df()
+        applier = PandasTFApplier([square], policy, k=1, keep_original=False)
+        df_augmented = applier.apply(df)
+        df_expected = pd.DataFrame(dict(num=[1, 16, 81]), index=[0, 1, 2])
+        self.assertTrue(df_augmented.equals(df_expected))
+        self.assertEqual(df.num.tolist(), DATA)
+
+    def test_tf_applier_pandas_multi(self):
+        df = self._get_x_df()
+        applier = PandasTFApplier([square], policy, k=2, keep_original=False)
+        df_augmented = applier.apply(df)
+        df_expected = pd.DataFrame(
+            dict(num=[1, 1, 16, 16, 81, 81]), index=[0, 0, 1, 1, 2, 2]
+        )
+        self.assertTrue(df_augmented.equals(df_expected))
+        self.assertEqual(df.num.tolist(), DATA)
+
+    def test_tf_applier_pandas_keep_original(self):
+        df = self._get_x_df()
+        applier = PandasTFApplier([square], policy, k=2, keep_original=True)
+        df_augmented = applier.apply(df)
+        df_expected = pd.DataFrame(
+            dict(num=[1, 1, 1, 2, 16, 16, 3, 81, 81]), index=[0, 0, 0, 1, 1, 1, 2, 2, 2]
+        )
+        self.assertTrue(df_augmented.equals(df_expected))
+        self.assertEqual(df.num.tolist(), DATA)
+
+    def test_tf_applier_pandas_modify_in_place(self):
+        df = self._get_x_df_dict()
+        applier = PandasTFApplier(
+            [modify_in_place], policy_modify_in_place, k=2, keep_original=True
+        )
+        df_augmented = applier.apply(df)
+        idx = [0, 0, 0, 1, 1, 1, 2, 2, 2]
+        df_expected = pd.DataFrame(dict(d=DATA_IN_PLACE_EXPECTED), index=idx)
+        self.assertTrue(df_augmented.equals(df_expected))
+        self.assertEqual(df.d.tolist(), get_data_dict())
--- tutorials/synthetic/synthetic.py
+++ tutorials/synthetic/synthetic.py
@@ -2,12 +2,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from snorkel.augmentation.apply import PandasTFApplier
+from snorkel.augmentation.policy import RandomAugmentationPolicy
 from snorkel.labeling.analysis import lf_empirical_accuracies
-from snorkel.labeling.apply import LFApplier
+from snorkel.labeling.apply import PandasLFApplier
 from snorkel.labeling.model.label_model import LabelModel
 from snorkel.synthetic.synthetic_data import (
     generate_mog_dataset,
-    generate_single_feature_LFs,
+    generate_resampling_tfs,
+    generate_single_feature_lfs,
 )
 
 #%% [markdown]
@@ -19,32 +22,42 @@ from snorkel.synthetic.synthetic_data import (
 #   3. And finally train an `EndModel` using the generated training labels
 
 #%%
-# Generating a simple mixture of gaussians dataset of d-dim one-hot vectors
-# as a pandas DataFrame
+# Generating a simple mixture of gaussians dataset of d-dim vectors
+# as a list of SimpleNamespaces
 n = 10000
 d = 10
+n_noise_dim = 2
 cov = np.diag(np.random.random(d))
-data = generate_mog_dataset(n, d, cov=cov)
+data = generate_mog_dataset(n, d, cov=cov, n_noise_dim=n_noise_dim)
 
 #%%
-X_pos = np.vstack([d.x for d in data if d.y == 1])
-X_neg = np.vstack([d.x for d in data if d.y == 2])
+X_pos = np.array(data[data.y == 1].x.tolist())
+X_neg = np.array(data[data.y == 2].x.tolist())
 
 # Plotting the first two dimensions of the data
 plt.scatter(X_pos[:, 0], X_pos[:, 1], color="red")
 plt.scatter(X_neg[:, 0], X_neg[:, 1], color="blue")
 plt.show()
 
+
+#%%
+m = 4
+tfs = generate_resampling_tfs(list(range(d, d + n_noise_dim)))
+policy = RandomAugmentationPolicy(len(tfs), sequence_length=1)
+tf_applier = PandasTFApplier(tfs, policy, k=1, keep_original=True)
+data_augmented = tf_applier.apply(data)
+
+
 #%%
 # Generate a set of m LFs that each fire based on a single feature
 m = 10
 abstain_rate = 0.0
-LFs = generate_single_feature_LFs(m, abstain_rate=abstain_rate)
+lfs = generate_single_feature_lfs(m, abstain_rate=abstain_rate)
 
 #%%
 # Apply the labeling functions to the data
-lf_applier = LFApplier(LFs)
-L = lf_applier.apply(data)
+lf_applier = PandasLFApplier(lfs)
+L = lf_applier.apply(data_augmented)
 
 #%%
 # Run the LabelModel to estimate the LF accuracies
@@ -53,7 +66,7 @@ label_model.train_model(L)
 
 #%%
 # Compare to the true (empirical) accuracies
-Y = np.array([d.y for d in data])
+Y = data_augmented.y.values.astype(int)
 accs_emp = lf_empirical_accuracies(L, Y)
 accs_est = label_model.get_accuracies()
 print(f"Avg. LF accuracy estimation error: {np.mean(np.abs(accs_emp - accs_est))}")

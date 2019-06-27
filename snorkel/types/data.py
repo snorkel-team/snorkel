35ec9d566560a51e74ee846dd067527ac33689a5
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-27 16:08:26 -0700

    Remove PyArrow-based test (#1169)
    * Remove PyArrow-based test
    
    * Add testing and typing to analysis.py (#1168)
    
    * Add testing and typing to analysis.py
    
    * Style fixes
    
    * Standardize pytest call
    
    * Small patches
    
    * Use Pandas testing utils (#1170)

--- snorkel/types/data.py
+++ snorkel/types/data.py
@@ -1,7 +1,13 @@
-from typing import Any, Collection, Mapping
+from typing import Any, Collection, Mapping, Union
+
+import numpy as np
+import scipy.sparse as sparse
+from torch import Tensor
 
 DataPoint = Any
 DataPoints = Collection[DataPoint]
 
 Field = Any
 FieldMap = Mapping[str, Field]
+
+ArrayLike = Union[np.ndarray, list, sparse.spmatrix, Tensor]

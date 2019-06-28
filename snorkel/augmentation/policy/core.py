90e55d609c44b7941e77d92f3ff020b72eac25ae
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-28 13:30:12 -0700

    Add docstrings for augmentation module (#1177)
    * Add docstrings for augmentation module
    
    * Add some more color
    
    * Imputed comments

--- snorkel/augmentation/policy/core.py
+++ snorkel/augmentation/policy/core.py
@@ -4,17 +4,66 @@ import numpy as np
 
 
 class AugmentationPolicy:
-    def __init__(self, n_tfs: int):
+    """Base class for augmentation policies.
+
+    Augmentation policies generate sequences of indices, corresponding
+    to a sequence of transformation functions to be run on a data point.
+
+    Parameters
+    ----------
+    n_tfs
+        Total number of TFs
+
+    Raises
+    ------
+    NotImplementedError
+        Subclasses need to implement the `generate` method
+    """
+
+    def __init__(self, n_tfs: int) -> None:
         self._n = n_tfs
 
     def generate(self) -> List[int]:
+        """Generate a sequence of TF indices.
+
+        Returns
+        -------
+        List[int]
+            Indices of TFs to run on data point in order.
+
+        Raises
+        ------
+        NotImplementedError
+            Subclasses need to implement this method
+        """
         raise NotImplementedError
 
 
 class RandomAugmentationPolicy(AugmentationPolicy):
+    """Naive random augmentation policy.
+
+    Samples sequences of TF indices a specified length uniformly
+    at random from the total number of TFs. This is a common
+    baseline approach to data augmentation.
+
+    Parameters
+    ----------
+    n_tfs
+        Total number of TFs
+    sequence_length
+        Number of TFs to run on each data point
+    """
+
     def __init__(self, n_tfs: int, sequence_length: int = 1) -> None:
         self._k = sequence_length
         super().__init__(n_tfs)
 
     def generate(self) -> List[int]:
+        """Generate a sequence of TF indices by sampling uniformly at random.
+
+        Returns
+        -------
+        List[int]
+            Indices of TFs to run on data point in order.
+        """
         return np.random.choice(self._n, size=self._k).tolist()

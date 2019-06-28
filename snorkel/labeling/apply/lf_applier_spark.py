78e842057dfa5fc6e6c5648cf7dbd73bb290db24
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-28 15:14:49 -0700

    Add docstrings for labeling module (#1175)
    * Add docstrings for labeling module
    
    * Format
    
    * Add some more color
    
    * Add attributes to LF
    
    * Address comments

--- snorkel/labeling/apply/lf_applier_spark.py
+++ snorkel/labeling/apply/lf_applier_spark.py
@@ -12,7 +12,29 @@ from .lf_applier import BaseLFApplier, RowData, apply_lfs_to_data_point
 
 
 class SparkLFApplier(BaseLFApplier):
+    """LF applier for a Spark RDD.
+
+    Data points are stored as `Row`s in an RDD, and a Spark
+    `map` job is submitted to execute the LFs. A common
+    way to obtain an RDD is via a PySpark DataFrame. For an
+    example usage with AWS EMR instructions, see
+    `test/labeling/apply/lf_applier_spark_test_script.py`.
+    """
+
     def apply(self, data_points: RDD) -> sparse.csr_matrix:  # type: ignore
+        """Label PySpark RDD of data points with LFs.
+
+        Parameters
+        ----------
+        data_points
+            PySpark RDD containing data points to be labeled by LFs
+
+        Returns
+        -------
+        sparse.csr_matrix
+            Sparse matrix of labels emitted by LFs
+        """
+
         def map_fn(args: Tuple[DataPoint, int]) -> RowData:
             return apply_lfs_to_data_point(*args, lfs=self._lfs)
 

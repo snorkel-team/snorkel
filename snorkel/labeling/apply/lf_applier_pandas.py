e8ce58705d3ed6d48afc63edd6c73b2275224bb8
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-28 15:14:49 -0700

    Add docstrings for labeling module (#1175)
    * Add docstrings for labeling module
    
    * Format
    
    * Add some more color
    
    * Add attributes to LF
    
    * Address comments

--- snorkel/labeling/apply/lf_applier_pandas.py
+++ snorkel/labeling/apply/lf_applier_pandas.py
@@ -15,6 +15,20 @@ PandasRowData = List[Tuple[int, int]]
 
 
 def apply_lfs_to_data_point(x: DataPoint, lfs: List[LabelingFunction]) -> PandasRowData:
+    """Label a single data point with a set of LFs.
+
+    Parameters
+    ----------
+    x
+        Data point to label
+    lfs
+        Set of LFs to label `x` with
+
+    Returns
+    -------
+    RowData
+        A list of (LF index, label) tuples
+    """
     labels = []
     for j, lf in enumerate(lfs):
         y = lf(x)
@@ -24,7 +38,27 @@ def apply_lfs_to_data_point(x: DataPoint, lfs: List[LabelingFunction]) -> Pandas
 
 
 class PandasLFApplier(BaseLFApplier):
+    """LF applier for a Pandas DataFrame.
+
+    Data points are stored as `Series` in a DataFrame. The LFs
+    are executed via a `pandas.DataFrame.apply` call, which
+    is single-process and can be slow for large DataFrames.
+    For large datasets, consider `DaskLFApplier` or `SparkLFApplier`.
+    """
+
     def apply(self, df: DataFrame) -> sparse.csr_matrix:  # type: ignore
+        """Label Pandas DataFrame of data points with LFs.
+
+        Parameters
+        ----------
+        data_points
+            Pandas DataFrame containing data points to be labeled by LFs
+
+        Returns
+        -------
+        sparse.csr_matrix
+            Sparse matrix of labels emitted by LFs
+        """
         self._set_lf_preprocessor_mode(PreprocessorMode.PANDAS)
         apply_fn = partial(apply_lfs_to_data_point, lfs=self._lfs)
         tqdm.pandas()

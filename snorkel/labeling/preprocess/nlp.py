cba76615f4cf68ac5850372c400f61df7c102717
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-28 04:21:55 +0000

    Add docstrings for labeling module

--- snorkel/labeling/preprocess/nlp.py
+++ snorkel/labeling/preprocess/nlp.py
@@ -11,6 +11,21 @@ EN_CORE_WEB_SM = "en_core_web_sm"
 
 
 class SpacyPreprocessor(Preprocessor):
+    """Preprocessor that parses input text via a SpaCy model.
+
+    Parameters
+    ----------
+    text_field
+        Name of data point text field to input
+    doc_field
+        Name of data point field to output parsed document to
+    language
+        SpaCy model to load, by default EN_CORE_WEB_SM.
+        See https://spacy.io/usage/models#usage
+    disable
+        List of pipeline components to disable, by default DEFAULT_DISABLE.
+        See https://spacy.io/usage/processing-pipelines#disabling
+    """
     def __init__(
         self,
         text_field: str,
@@ -22,4 +37,16 @@ class SpacyPreprocessor(Preprocessor):
         self._nlp = spacy.load(language, disable=disable)
 
     def run(self, text: str) -> FieldMap:  # type: ignore
+        """Run the SpaCy model on input text.
+
+        Parameters
+        ----------
+        text
+            Text of document to parse
+
+        Returns
+        -------
+        FieldMap
+            Mapping containing parsed document
+        """
         return dict(doc=self._nlp(text))

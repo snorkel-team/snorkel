4e4ead8fb85861e7e920aee4ad5f5756b105b9da
Paroma Varma <varma.paroma@gmail.com>
2019-06-28 15:22:47 -0700

    add test case

--- test/labeling/model/test_baseline_model.py
+++ test/labeling/model/test_baseline_model.py
@@ -11,7 +11,7 @@ from snorkel.labeling.model.baselines import (
 
 class BaselineModelTest(unittest.TestCase):
     def test_random_vote(self):
-        L = np.array([[1, 1, 3, 1], [2, 4, 1, 2], [1, 3, 1, 2]]).T
+        L = np.array([[1, 2, 1], [0, 4, 3], [3, 0, 0], [1, 2, 2]])
         rand_voter = RandomVoter()
         rand_voter.train_model()
         Y_p = rand_voter.predict_proba(L)
@@ -19,7 +19,7 @@ class BaselineModelTest(unittest.TestCase):
         self.assertGreaterEqual(Y_p.min(), 0.0)
 
     def test_majority_class_vote(self):
-        L = np.array([[1, 1, 2, 1], [2, 2, 1, 2], [1, 1, 1, 2]]).T
+        L = np.array([[1, 2, 1], [2, 2, 1], [2, 2, 1], [0, 0, 2]])
         mc_voter = MajorityClassVoter()
         mc_voter.train_model(balance=[0.8, 0.2])
         Y_p = mc_voter.predict_proba(L)
@@ -28,9 +28,9 @@ class BaselineModelTest(unittest.TestCase):
         self.assertTrue((Y_p == Y_p_true).all())
 
     def test_majority_label_vote(self):
-        L = np.array([[1, 1, 2, 1], [2, 2, 1, 2], [1, 1, 1, 2]]).T
+        L = np.array([[1, 2, 1], [1, 2, 1], [2, 1, 1], [0, 0, 2]])
         ml_voter = MajorityLabelVoter()
-        ml_voter.train_model(balance=[0.8, 0.2])
+        ml_voter.train_model()
         Y_p = ml_voter.predict_proba(L)
 
         Y_p_true = np.array([[1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [0.0, 1.0]])

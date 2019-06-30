58da01289b9a208f1ca03302ab44831fd98d56d5
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-06-30 19:20:13 +0000

    isort

--- snorkel/augmentation/policy/__init__.py
+++ snorkel/augmentation/policy/__init__.py
@@ -1,6 +1,6 @@
 from .core import (  # noqa: F401
-    AugmentationPolicy,
     ApplyAllAugmentationPolicy,
     ApplyOneAugmentationPolicy,
+    AugmentationPolicy,
     RandomAugmentationPolicy,
 )

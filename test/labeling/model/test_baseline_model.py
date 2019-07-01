1c2b98a67382235e98da92c22a0a90ad1897e19c
Paroma Varma <varma.paroma@gmail.com>
2019-07-01 10:18:46 -0700

    Fix typo

--- test/labeling/model/test_baseline_model.py
+++ test/labeling/model/test_baseline_model.py
@@ -1,5 +1,4 @@
 import unittest
-
 import numpy as np
 
 from snorkel.labeling.model.baselines import (
@@ -25,7 +24,7 @@ class BaselineModelTest(unittest.TestCase):
         Y_p = mc_voter.predict_proba(L)
 
         Y_p_true = np.array([[1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [1.0, 0.0]])
-        np.testing.assert_array_almost_equal(Y_p, Y_p_true))
+        np.testing.assert_array_almost_equal(Y_p, Y_p_true)
 
     def test_majority_label_vote(self):
         L = np.array([[1, 2, 1], [1, 2, 1], [2, 1, 1], [0, 0, 2]])
@@ -34,7 +33,7 @@ class BaselineModelTest(unittest.TestCase):
         Y_p = ml_voter.predict_proba(L)
 
         Y_p_true = np.array([[1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
-        np.testing.assert_array_almost_equalY_p, Y_p_true))
+        np.testing.assert_array_almost_equal(Y_p, Y_p_true)
 
 
 if __name__ == "__main__":

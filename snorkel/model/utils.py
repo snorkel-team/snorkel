c660728944b94ea3416292ba58b3d2fffdf2c73f
Braden Hancock <hancock.braden@gmail.com>
2019-07-01 16:04:24 -0700

    Remove unused utilities

--- snorkel/model/utils.py
+++ snorkel/model/utils.py
@@ -1,7 +1,5 @@
-import argparse
 import copy
 import random
-from collections import defaultdict
 
 import numpy as np
 import torch
@@ -27,28 +25,6 @@ class MetalDataset(Dataset):
 
     def __len__(self):
         return len(self.X)
-
-
-def label_matrix_to_one_hot(L, k=None):
-    """Converts a 2D [n,m] label matrix into an [n,m,k] one hot 3D tensor
-
-    Note that in the returned 3D matrix, abstain votes continue to be
-    represented by 0s, not 1s.
-
-    Args:
-        L: a [n,m] label matrix with categorical labels (0 = abstain)
-        k: the number of classes that could appear in L
-            if None, k is inferred as the max element in L
-    """
-    n, m = L.shape
-    if k is None:
-        k = L.max()
-    L_onehot = torch.zeros(n, m, k + 1)
-    for i, row in enumerate(L):
-        for j, k in enumerate(row):
-            if k > 0:
-                L_onehot[i, j, k - 1] = 1
-    return L_onehot
 
 
 def recursive_merge_dicts(x, y, misses="report", verbose=None):
@@ -134,134 +110,6 @@ def recursive_transform(x, test_func, transform):
         if isinstance(v, dict):
             recursive_transform(v, test_func, transform)
     return x
-
-
-def add_flags_from_config(parser, config_dict):
-    """
-    Adds a flag (and default value) to an ArgumentParser for each parameter in a config
-    """
-
-    def OrNone(default):
-        def func(x):
-            # Convert "none" to proper None object
-            if x.lower() == "none":
-                return None
-            # If default is None (and x is not None), return x without conversion as str
-            elif default is None:
-                return str(x)
-            # Otherwise, default has non-None type; convert x to that type
-            else:
-                return type(default)(x)
-
-        return func
-
-    def str2bool(string):
-        if string == "0" or string.lower() == "false":
-            return False
-        elif string == "1" or string.lower() == "true":
-            return True
-        else:
-            raise Exception(f"Invalid value {string} for boolean flag")
-
-    for param in config_dict:
-        # Blacklist certain config parameters from being added as flags
-        if param in ["verbose"]:
-            continue
-        default = config_dict[param]
-        try:
-            if isinstance(default, dict):
-                parser = add_flags_from_config(parser, default)
-            elif isinstance(default, bool):
-                parser.add_argument(f"--{param}", type=str2bool, default=default)
-            elif isinstance(default, list):
-                if len(default) > 0:
-                    # pass a list as argument
-                    parser.add_argument(
-                        f"--{param}",
-                        action="append",
-                        type=type(default[0]),
-                        default=default,
-                    )
-                else:
-                    parser.add_argument(f"--{param}", action="append", default=default)
-            else:
-                parser.add_argument(f"--{param}", type=OrNone(default), default=default)
-        except argparse.ArgumentError:
-            print(
-                f"Could not add flag for param {param} because it was already present."
-            )
-    return parser
-
-
-def padded_tensor(items, pad_idx=0, left_padded=False, max_len=None):
-    """Create a padded [n, ?] Tensor from a potentially uneven iterable of Tensors.
-    Modified from github.com/facebookresearch/ParlAI
-
-    Args:
-        items: (list) the items to merge and pad
-        pad_idx: (int) the value to use for padding
-        left_padded: (bool) if True, pad on the left instead of the right
-        max_len: (int) if not None, the maximum allowable item length
-
-    Returns:
-        padded_tensor: (Tensor) the merged and padded tensor of items
-    """
-    # number of items
-    n = len(items)
-    # length of each item
-    lens = [len(item) for item in items]
-    # max seq_len dimension
-    max_seq_len = max(lens) if max_len is None else max_len
-
-    output = items[0].new_full((n, max_seq_len), pad_idx)
-
-    for i, (item, length) in enumerate(zip(items, lens)):
-        if left_padded:
-            # place at end
-            output[i, max_seq_len - length :] = item
-        else:
-            # place at beginning
-            output[i, :length] = item
-
-    return output
-
-
-# DEPRECATION: This is replaced by move_to_device
-def place_on_gpu(data):
-    """Utility to place data on GPU, where data could be a torch.Tensor, a tuple
-    or list of Tensors, or a tuple or list of tuple or lists of Tensors"""
-    data_type = type(data)
-    if data_type in (list, tuple):
-        data = [place_on_gpu(data[i]) for i in range(len(data))]
-        data = data_type(data)
-        return data
-    elif isinstance(data, torch.Tensor):
-        return data.cuda()
-    else:
-        return ValueError(f"Data type {type(data)} not recognized.")
-
-
-def move_to_device(obj, device=-1):
-    """
-    Given a structure (possibly) containing Tensors on the CPU,
-    move all the Tensors to the specified GPU (or do nothing, if they should be on the CPU).
-
-    device = -1 -> "cpu"
-    device =  0 -> "cuda:0"
-
-    """
-    if device < 0 or not torch.cuda.is_available():
-        return obj
-    elif isinstance(obj, torch.Tensor):
-        return obj.cuda(device)
-    elif isinstance(obj, dict):
-        return {key: move_to_device(value, device) for key, value in obj.items()}
-    elif isinstance(obj, list):
-        return [move_to_device(item, device) for item in obj]
-    elif isinstance(obj, tuple):
-        return tuple([move_to_device(item, device) for item in obj])
-    else:
-        return obj
 
 
 def set_seed(seed: int):

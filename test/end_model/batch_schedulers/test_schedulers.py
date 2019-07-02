07c2eac8dea96b19f8dbb271fd1515480ceeae82
Braden Hancock <hancock.braden@gmail.com>
2019-07-02 16:21:31 -0700

    Add tests for batch schedulers

---
+++ test/end_model/batch_schedulers/test_schedulers.py
@@ -0,0 +1,41 @@
+import unittest
+
+import torch
+
+from snorkel.analysis.utils import set_seed
+from snorkel.end_model.batch_schedulers import SequentialScheduler, ShuffledScheduler
+from snorkel.end_model.data import MultitaskDataLoader, MultitaskDataset
+
+dataset1 = MultitaskDataset(
+    "d1", "train", {"data": ["a", "b", "c"]}, {"labels": torch.LongTensor([1, 2, 3])}
+)
+dataset2 = MultitaskDataset(
+    "d2", "train", {"data": ["d", "e", "f"]}, {"labels": torch.LongTensor([4, 5, 6])}
+)
+
+dataloader1 = MultitaskDataLoader(dataset1, batch_size=2)
+dataloader2 = MultitaskDataLoader(dataset2, batch_size=2)
+dataloaders = [dataloader1, dataloader2]
+
+
+class SequentialTest(unittest.TestCase):
+    def test_sequential(self):
+        scheduler = SequentialScheduler()
+        data = []
+        for (batch, dl) in scheduler.get_batches(dataloaders):
+            X_dict, Y_dict = batch
+            data.extend(X_dict["data"])
+        self.assertEqual(data, ["a", "b", "c", "d", "e", "f"])
+
+    def test_shuffled(self):
+        set_seed(123)
+        scheduler = ShuffledScheduler()
+        data = []
+        for (batch, dl) in scheduler.get_batches(dataloaders):
+            X_dict, Y_dict = batch
+            data.extend(X_dict["data"])
+        self.assertNotEqual(data, ["a", "b", "c", "d", "e", "f"])
+
+
+if __name__ == "__main__":
+    unittest.main()

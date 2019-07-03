15d3d644d2e61ada8019e3604b20639b3879ca70
Paroma Varma <varma.paroma@gmail.com>
2019-07-02 17:04:36 -0700

    Small fix for cond. ind. model

--- test/labeling/model/test_label_model.py
+++ test/labeling/model/test_label_model.py
@@ -20,11 +20,7 @@ class LabelModelTest(unittest.TestCase):
     def _test_label_model(self, data, test_acc=True):
         label_model = LabelModel(k=data.k, verbose=False)
         label_model.train_model(
-            data.L,
-            deps=data.E,
-            class_balance=data.p,
-            n_epochs=1000,
-            log_train_every=200,
+            data.L, class_balance=data.p, n_epochs=1000, log_train_every=200
         )
 
         # Test parameter estimation error
@@ -49,24 +45,22 @@ class LabelModelTest(unittest.TestCase):
             self._test_label_model(data)
 
     def test_augmented_L_construction(self):
-        # 5 LFs: a triangle, a connected edge to it, and a singleton source
+        # 5 LFs
         n = 3
         m = 5
         k = 2
-        E = [(0, 1), (1, 2), (2, 0), (0, 3)]
         L = np.array([[1, 1, 1, 2, 1], [1, 2, 2, 1, 0], [1, 1, 1, 1, 0]])
         lm = LabelModel(k=k, verbose=False)
         lm._set_constants(L)
-        lm._set_dependencies(E)
+        lm._create_tree()
         L_aug = lm._get_augmented_label_matrix(L, higher_order=True)
 
-        # Should have 22 columns:
+        # Should have 10 columns:
         # - 5 * 2 = 10 for the sources
-        # - 8 + 4 for the 3- and 2-clique resp. --> = 22
-        self.assertEqual(L_aug.shape, (3, 22))
+        self.assertEqual(L_aug.shape, (3, 10))
 
-        # Same as above but minus 2 abstains = 19 total nonzero entries
-        self.assertEqual(L_aug.sum(), 19)
+        # 13 total nonzero entries
+        self.assertEqual(L_aug.sum(), 13)
 
         # Next, check the singleton entries
         for i in range(n):
@@ -75,24 +69,15 @@ class LabelModelTest(unittest.TestCase):
                     self.assertEqual(L_aug[i, j * k + L[i, j] - 1], 1)
 
         # Finally, check the clique entries
-        # Triangle clique
-        self.assertEqual(len(lm.c_tree.node[1]["members"]), 3)
+        # Singleton clique 1
+        self.assertEqual(len(lm.c_tree.node[1]["members"]), 1)
         j = lm.c_tree.node[1]["start_index"]
         self.assertEqual(L_aug[0, j], 1)
-        self.assertEqual(L_aug[1, j + 3], 1)
-        self.assertEqual(L_aug[2, j], 1)
-        # Binary clique
-        self.assertEqual(len(lm.c_tree.node[2]["members"]), 2)
-        j = lm.c_tree.node[2]["start_index"]
-        self.assertEqual(L_aug[0, j + 1], 1)
-        self.assertEqual(L_aug[1, j], 1)
-        self.assertEqual(L_aug[2, j], 1)
 
-    def test_with_deps(self):
-        for seed in range(self.n_iters):
-            np.random.seed(seed)
-            data = SingleTaskTreeDepsGenerator(self.n, self.m, k=self.k, edge_prob=1.0)
-            self._test_label_model(data, test_acc=False)
+        # Singleton clique 2
+        self.assertEqual(len(lm.c_tree.node[2]["members"]), 1)
+        j = lm.c_tree.node[2]["start_index"]
+        self.assertEqual(L_aug[0, j + 1], 0)
 
 
 if __name__ == "__main__":

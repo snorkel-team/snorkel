b7bb39d60514eb4a1fdbc7a16c715ce5f657ec00
Braden Hancock <hancock.braden@gmail.com>
2019-07-03 11:01:04 -0700

    Refactor task_schedulers as batch_schedulers (#1200)
    * Refactor task_schedulers as batch_schedulers
    
    * Simplify end model scheduler/dataset signatures (#1205)
    
    * Simplify MultitaskDataset class
    
    * Simplify batch_scheduler signatures
    
    * Make split and name an attribute of dataset i/o dataloader
    
    * Add tests for batch schedulers
    
    * Update tests to change of split definition
    
    * Add missing docstrings
    
    * Address comments

---
+++ snorkel/end_model/batch_schedulers/sequential_scheduler.py
@@ -0,0 +1,34 @@
+from typing import Any, Dict, Sequence, Tuple
+
+from snorkel.end_model.data import MultitaskDataLoader
+
+from .scheduler import Scheduler
+
+
+class SequentialScheduler(Scheduler):
+    """Return batches from all dataloaders in sequential order."""
+
+    def __init__(self):
+        super().__init__()
+
+    def get_batches(
+        self, dataloaders: Sequence[MultitaskDataLoader]
+    ) -> Tuple[Tuple[Dict[str, Any], Dict[str, Any]], MultitaskDataLoader]:
+        """Return batches from dataloaders sequentially in the order they were given.
+
+        Parameters
+        ----------
+        dataloaders
+            A sequence of dataloaders to get batches from
+
+        Yields
+        -------
+        (batch, dataloader)
+            batch is a tuple of (X_dict, Y_dict) and dataloader is the dataloader
+            that that batch came from. That dataloader will not be accessed by the
+            model; it is passed primarily so that the model can pull the necessary
+            metadata to know what to do with the batch it has been given.
+        """
+        for dataloader in dataloaders:
+            for batch in dataloader:
+                yield batch, dataloader

5821587f06305e507ec2c88d4b7819b212fcdc54
Braden Hancock <hancock.braden@gmail.com>
2019-07-04 16:17:18 -0700

    Add helper to build AdvancedClassifier from modules

--- snorkel/classification/models/advanced/advanced_classifier.py
+++ snorkel/classification/models/advanced/advanced_classifier.py
@@ -337,3 +337,48 @@ class AdvancedClassifier(nn.Module):
                 self.to(torch.device(f"cuda:{device}"))
             else:
                 logging.info("No cuda device available. Switch to cpu instead.")
+
+    @classmethod
+    def from_modules(
+        self,
+        modules: List[nn.Module],
+        dropout: float = 0.0,
+        loss_func: Callable[..., torch.FloatTensor] = ce_loss,
+        output_func: Callable[..., np.ndarray] = softmax,
+        metrics: List[str] = ["accuracy"],
+        **kwargs,
+    ):
+        """Define a simple AdvancedClassifier from `Module`s instead of `Tasks
+
+        TBD
+        """
+        module_pool: Dict[str, nn.Module] = {}
+        task_flow: List[Operation] = []
+        inputs: Sequence[Tuple[str, Union[str, int]]]
+        for i, module in enumerate(modules):
+
+            args = [module]
+            if i < len(modules) - 1:
+                args.append(nn.ReLU())
+                if dropout > 0:
+                    args.append(nn.Dropout(dropout))
+            module_block = nn.Sequential(*args)
+
+            module_pool[f"module{i}"] = module_block
+            if i == 0:
+                inputs = [("_input_", "data")]
+            else:
+                inputs = [(task_flow[-1].name, 0)]
+            op = Operation(name=f"op{i}", module_name=f"module{i}", inputs=inputs)
+            task_flow.append(op)
+
+        last_op = f"op{len(modules) - 1}"
+        task = Task(
+            name="task",
+            module_pool=nn.ModuleDict(module_pool),
+            task_flow=task_flow,
+            loss_func=partial(ce_loss, last_op),
+            output_func=partial(output_func, last_op),
+            scorer=Scorer(metrics=metrics),
+        )
+        return AdvancedClassifier([task], **kwargs)

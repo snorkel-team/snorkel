99a9a704cfdfa280b0e3398ee07c9a593f6f0543
Braden Hancock <hancock.braden@gmail.com>
2019-07-04 21:49:33 -0700

    Add more typing to classification/ (#1220)
    * Merge model save/load options
    
    * Add helper to build AdvancedClassifier from modules
    
    * Add unit tests
    
    * Fix log_manager LOG_DIR path
    
    * Add types
    
    * Fix formatting
    
    * Update types and add docstring to from_modules() helper
    
    * Remove extraneous cpu() conversion
    
    * Split PR in half
    
    * Replace torch.Tensor with Tensor

--- test/classification/models/test_model.py
+++ test/classification/models/test_model.py
@@ -9,35 +9,8 @@ from snorkel.classification.scorer import Scorer
 
 
 class TaskTest(unittest.TestCase):
-    def create_task(self, task_name, module_suffixes):
-        module1_name = f"linear1{module_suffixes[0]}"
-        module2_name = f"linear2{module_suffixes[1]}"
-
-        module_pool = nn.ModuleDict(
-            {
-                module1_name: nn.Sequential(nn.Linear(2, 10), nn.ReLU()),
-                module2_name: nn.Linear(10, 2),
-            }
-        )
-
-        op1 = Operation(module_name=module1_name, inputs=[("_input_", "coordinates")])
-        op2 = Operation(module_name=module2_name, inputs=[(op1.name, 0)])
-
-        task_flow = [op1, op2]
-
-        task = Task(
-            name=task_name,
-            module_pool=module_pool,
-            task_flow=task_flow,
-            loss_func=partial(ce_loss, op2.name),
-            output_func=partial(softmax, op2.name),
-            scorer=Scorer(metrics=["accuracy"]),
-        )
-
-        return task
-
     def test_onetask_model(self):
-        task1 = self.create_task("task1", module_suffixes=["A", "A"])
+        task1 = create_task("task1")
         model = AdvancedClassifier(tasks=[task1])
         self.assertEqual(len(model.task_names), 1)
         self.assertEqual(len(model.task_flows), 1)
@@ -45,8 +18,8 @@ class TaskTest(unittest.TestCase):
 
     def test_twotask_all_overlap_model(self):
         """Add two tasks with identical modules and flows"""
-        task1 = self.create_task("task1", module_suffixes=["A", "A"])
-        task2 = self.create_task("task2", module_suffixes=["A", "A"])
+        task1 = create_task("task1")
+        task2 = create_task("task2")
         model = AdvancedClassifier(tasks=[task1, task2])
         self.assertEqual(len(model.task_names), 2)
         self.assertEqual(len(model.task_flows), 2)
@@ -54,8 +27,8 @@ class TaskTest(unittest.TestCase):
 
     def test_twotask_none_overlap_model(self):
         """Add two tasks with totally separate modules and flows"""
-        task1 = self.create_task("task1", module_suffixes=["A", "A"])
-        task2 = self.create_task("task2", module_suffixes=["B", "B"])
+        task1 = create_task("task1", module_suffixes=["A", "A"])
+        task2 = create_task("task2", module_suffixes=["B", "B"])
         model = AdvancedClassifier(tasks=[task1, task2])
         self.assertEqual(len(model.task_names), 2)
         self.assertEqual(len(model.task_flows), 2)
@@ -63,9 +36,41 @@ class TaskTest(unittest.TestCase):
 
     def test_twotask_partial_overlap_model(self):
         """Add two tasks with overlapping modules and flows"""
-        task1 = self.create_task("task1", module_suffixes=["A", "A"])
-        task2 = self.create_task("task2", module_suffixes=["A", "B"])
+        task1 = create_task("task1", module_suffixes=["A", "A"])
+        task2 = create_task("task2", module_suffixes=["A", "B"])
         model = AdvancedClassifier(tasks=[task1, task2])
         self.assertEqual(len(model.task_names), 2)
         self.assertEqual(len(model.task_flows), 2)
         self.assertEqual(len(model.module_pool), 3)
+
+
+def create_task(task_name, module_suffixes=("", "")):
+    module1_name = f"linear1{module_suffixes[0]}"
+    module2_name = f"linear2{module_suffixes[1]}"
+
+    module_pool = nn.ModuleDict(
+        {
+            module1_name: nn.Sequential(nn.Linear(2, 4), nn.ReLU()),
+            module2_name: nn.Linear(4, 2),
+        }
+    )
+
+    op0 = Operation(module_name=module1_name, inputs=[("_input_", "data")], name="op0")
+    op1 = Operation(module_name=module2_name, inputs=[(op0.name, 0)], name="op1")
+
+    task_flow = [op0, op1]
+
+    task = Task(
+        name=task_name,
+        module_pool=module_pool,
+        task_flow=task_flow,
+        loss_func=partial(ce_loss, op1.name),
+        output_func=partial(softmax, op1.name),
+        scorer=Scorer(metrics=["accuracy"]),
+    )
+
+    return task
+
+
+if __name__ == "__main__":
+    unittest.main()

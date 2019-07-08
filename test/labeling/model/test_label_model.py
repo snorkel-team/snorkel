4cc2391383b4932efcb148391db031a0cfa1734f
Paroma Varma <varma.paroma@gmail.com>
2019-07-07 23:12:55 -0700

    Address comments

--- test/labeling/model/test_label_model.py
+++ test/labeling/model/test_label_model.py
@@ -47,9 +47,10 @@ class LabelModelTest(unittest.TestCase):
         L = np.array([[1, 2, 1], [1, 2, 1], [2, 1, 1], [1, 2, 2]])
         label_model = self._set_up_model(L)
 
+        # O = (1/n) * L^TL = L^TL/4
         true_O = np.array(
             [
-                [3 / 4, 0, 0, 3 / 4, 1.0 / 2, 1 / 4],
+                [3 / 4, 0, 0, 3 / 4, 1 / 2, 1 / 4],
                 [0, 1 / 4, 1 / 4, 0, 1 / 4, 0],
                 [0, 1 / 4, 1 / 4, 0, 1 / 4, 0],
                 [3 / 4, 0, 0, 3 / 4, 1 / 2, 1 / 4],
@@ -65,7 +66,7 @@ class LabelModelTest(unittest.TestCase):
         label_model._generate_O(L, higher_order=False)
         true_O = np.array(
             [
-                [3 / 4, 0, 0, 3 / 4, 1.0 / 2, 1 / 4],
+                [3 / 4, 0, 0, 3 / 4, 1 / 2, 1 / 4],
                 [0, 1 / 4, 1 / 4, 0, 1 / 4, 0],
                 [0, 1 / 4, 1 / 4, 0, 1 / 4, 0],
                 [3 / 4, 0, 0, 3 / 4, 1 / 2, 1 / 4],
@@ -142,8 +143,16 @@ class LabelModelTest(unittest.TestCase):
         label_model.k = 2
         label_model.P = torch.Tensor([[0.5, 0.0], [0.0, 0.5]])
         accs = label_model.get_accuracies(probs=probs)
+
+        # accs[i] = sum(diag(probs[i*(k+1):(i+1)*(k+1)][1:,:]) * P)
+        # accs[0] = 0.5*0.9 + 0.5*0.5 = 0.7
+        # accs[1] = 0.5*0.9 + 0.5*0.75 = 0.825
         np.testing.assert_array_almost_equal(accs, np.array([0.7, 0.825]))
 
+        # accs[i] = sum(diag(probs[i*(k+1):(i+1)*(k+1)][1:,:]) * P)
+        # accs[0] = 0.5*0.5 + 0.5*0.5 = 0.5 since (P(\lambda_0 = 1 | Y = y) = 0.5)
+        # accs[1] = 0.5*0.7 + 0.5*0.01 = 0.355 since (P(\lambda_1 = 1 | Y = 1) = 0.7)
+        # default prec_init = 0.7, clamp lowest prob to 0.01
         label_model.mu = nn.Parameter(label_model.mu_init.clone())
         accs = label_model.get_accuracies(probs=None)
         np.testing.assert_array_almost_equal(accs, np.array([0.5, 0.355]))
@@ -153,6 +162,8 @@ class LabelModelTest(unittest.TestCase):
         L = np.array([[1, 2, 1], [1, 2, 1]])
         label_model = self._set_up_model(L)
 
+        # block diagonal with 0s for dependent LFs
+        # without deps, k X k block of 0s down diagonal
         true_mask = np.array(
             [
                 [0, 0, 1, 1, 1, 1],
@@ -171,12 +182,18 @@ class LabelModelTest(unittest.TestCase):
         L = np.array([[1, 2, 1], [1, 0, 1]])
         label_model = self._set_up_model(L, class_balance=[0.6, 0.4])
 
+        # mu_init = P(\lf=y|Y=y) = clamp(P(\lf=y) * prec_i / P(Y=y), (0,1))
+        # mu_init[lf0, lf2 = 1 | Y = 1] = clamp(1.0 * 0.7 / 0.6) = 1.0 since P(lf = 1) = 1.0
+        # mu_init[lf1 | Y = 2] = clamp(0.5 * 0.7 / 0.4) = 0.875 since P(lf = 2) = 0.5
         mu_init = label_model.mu_init.numpy()
         true_mu_init = np.array(
-            [[1.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.8750], [1.0, 0.0], [0.0, 0.0]]
+            [[1.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.875], [1.0, 0.0], [0.0, 0.0]]
         )
         np.testing.assert_array_equal(mu_init, true_mu_init)
 
+        # mu_init = P(\lf=y|Y=y) = clamp(P(\lf=y) * prec_i / P(Y=y), (0,1))
+        # mu_init[lf0, lf2 = 1 | Y = 1] = clamp(1.0 * 0.7 / 0.6) = 1.0 since P(lf = 1) = 1.0
+        # mu_init[lf1 = 2 | Y = 2] = clamp(0.5 * 0.7 / 0.7) = 0.5 since P(lf = 2) = 0.5
         label_model._set_class_balance(class_balance=[0.3, 0.7], Y_dev=None)
         label_model._init_params()
 
@@ -193,6 +210,7 @@ class LabelModelTest(unittest.TestCase):
         label_model.mu = nn.Parameter(label_model.mu_init.clone())
         probs = label_model.predict_proba(L)
 
+        # with matching labels from 3 LFs, predicted probs clamped at (0.99,0.01)
         true_probs = np.array([[0.99, 0.01], [0.99, 0.01]])
         np.testing.assert_array_almost_equal(probs, true_probs)
 
@@ -202,24 +220,28 @@ class LabelModelTest(unittest.TestCase):
         label_model._get_augmented_label_matrix(L, higher_order=True)
 
         label_model.mu = nn.Parameter(label_model.mu_init.clone() + 0.05)
+
+        # l2_loss = l2*M*K*||mu - mu_init||_2 = 3*2*(0.05^2) = 0.03
         self.assertAlmostEqual(
             label_model.loss_l2(l2=1.0).detach().numpy().ravel()[0], 0.03
         )
         self.assertAlmostEqual(
             label_model.loss_l2(l2=np.ones(6)).detach().numpy().ravel()[0], 0.03
         )
+
+        # mu_loss = ||O - \mu^T P \mu||_2 + ||\mu^T P - diag(O)||_2
         self.assertAlmostEqual(
             label_model.loss_mu().detach().numpy().ravel()[0], 0.675, 3
         )
 
-    def test_loss_decrease(self):
+    def test_train_model(self):
         L = np.array([[1, 0, 1], [1, 2, 1]])
         label_model = self._set_up_model(L)
 
-        label_model.train_model(L, n_epochs=1)
+        label_model.train_model(L, n_epochs=1, lr=0.01, momentum=0.9)
         init_loss = label_model.loss_mu().detach().numpy().ravel()[0]
 
-        label_model.train_model(L, n_epochs=10)
+        label_model.train_model(L, n_epochs=10, lr=0.01, momentum=0.9)
         next_loss = label_model.loss_mu().detach().numpy().ravel()[0]
 
         self.assertLessEqual(next_loss, init_loss)

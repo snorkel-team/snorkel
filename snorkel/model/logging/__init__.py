6fbe089c872c37f79f0481b2d77484cf01ce4578
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-07-09 10:21:04 -0700

    Merge snorkel/model into snorkel/labeling/model (#1230)
    * Merge snorkel/model into snorkel/labeling/model
    
    * Remove log writer and checkpointer
    
    * Strip down logger
    
    * Address comments
    
    * Simplify config

--- snorkel/model/logging/__init__.py
+++
@@ -1,6 +0,0 @@
-from .checkpointer import Checkpointer
-from .logger import Logger, Timer
-from .tensorboard import TensorBoardWriter
-from .writer import LogWriter
-
-__all__ = ["Checkpointer", "Logger", "LogWriter", "TensorBoardWriter", "Timer"]

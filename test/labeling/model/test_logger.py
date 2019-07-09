815ba9d3edad109b10b34c20da20813df7556a26
Paroma Varma <varma.paroma@gmail.com>
2019-07-09 12:12:37 -0700

    Fix formatting

--- test/labeling/model/test_logger.py
+++ test/labeling/model/test_logger.py
@@ -5,12 +5,15 @@ from snorkel.labeling.model.logger import Logger
 
 class LoggerTest(unittest.TestCase):
     def test_bad_metrics_dict(self):
-        mtl_metrics_dict = {"task1/train/loss": 0.05}
         bad_metrics_dict = {"task1/slice1/train/loss": 0.05}
 
         logger = Logger(log_train_every=1)
-        logger.print_to_screen(mtl_metrics_dict)
         self.assertRaises(Exception, logger.print_to_screen, bad_metrics_dict)
+
+    def test_valid_metrics_dict(self):
+        mtl_metrics_dict = {"task1/valid/loss": 0.05}
+        logger = Logger(log_train_every=1)
+        logger.print_to_screen(mtl_metrics_dict)
 
 
 if __name__ == "__main__":

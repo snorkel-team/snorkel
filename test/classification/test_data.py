23a060fdc0fd4a4a1e6f5c81d41cca6cc807ce71
Braden Hancock <hancock.braden@gmail.com>
2019-07-09 10:21:40 -0700

    Empty classification/model and set default loss/output functions (#1234)
    * Merge model save/load options
    
    * Add helper to build AdvancedClassifier from modules
    
    * Add unit tests
    
    * Fix log_manager LOG_DIR path
    
    * Add types
    
    * Fix formatting
    
    * Update types and add docstring to from_modules() helper
    
    * Remove extraneous cpu() conversion
    
    * Split PR in half
    
    * Simplify/merge save and load methods
    
    * Move move_to_device down lower
    
    * Add from_modules() helper and tests
    
    * Remove unused SimpleClassifier
    
    * Remove from_modules()
    
    * Add default loss and output funcs to Task
    
    * Empty out classification/models/
    
    * Rename AdvancedClassifier to SnorkelClassifier
    
    * Refactor to DictDataset and DictDataLoader
    
    * Remove unwanted copies of advanced_classifier.py
    
    * Move tests to reflect directory structure
    
    * Fix import paths and add intelligent task defaults
    
    * Update test_task to use Operations
    
    * Fix type error and duplicate method
    
    * Run tox -e fix
    
    * Use tempfile in unit test
    
    * Shrink diff by restoring old class names
    
    * Run tox
    
    * Refactor Dataset, DataLoader, and Model names (#1235)
    
    * Refactor Dataset, DataLoader, and Model names
    
    * Run tox
    
    * Remove task_to_label_dict (use task names as keys in Y_dict) (#1236)
    
    * Remove task_to_label_dict (use task names as keys in Y_dict)
    
    * Add a docstring

--- test/classification/test_data.py
+++ test/classification/test_data.py
@@ -5,16 +5,12 @@ import numpy as np
 import scipy.sparse as sparse
 import torch
 
-from snorkel.classification.data import (
-    ClassifierDataLoader,
-    ClassifierDataset,
-    split_data,
-)
+from snorkel.classification.data import DictDataLoader, DictDataset, split_data
 
 
 class DatasetTest(unittest.TestCase):
     def test_classifier_dataset(self):
-        """Unit test of ClassifierDataset"""
+        """Unit test of DictDataset"""
 
         x1 = [
             torch.Tensor([1]),
@@ -26,16 +22,16 @@ class DatasetTest(unittest.TestCase):
 
         y1 = torch.Tensor([0, 0, 0, 0, 0])
 
-        dataset = ClassifierDataset(
-            X_dict={"data1": x1}, Y_dict={"label1": y1}, name="new_data", split="train"
+        dataset = DictDataset(
+            X_dict={"data1": x1}, Y_dict={"task1": y1}, name="new_data", split="train"
         )
 
         # Check if the dataset is correctly constructed
         self.assertTrue(torch.equal(dataset[0][0]["data1"], x1[0]))
-        self.assertTrue(torch.equal(dataset[0][1]["label1"], y1[0]))
+        self.assertTrue(torch.equal(dataset[0][1]["task1"], y1[0]))
 
     def test_classifier_dataloader(self):
-        """Unit test of ClassifierDataLoader"""
+        """Unit test of DictDataLoader"""
 
         x1 = [
             torch.Tensor([1]),
@@ -57,21 +53,18 @@ class DatasetTest(unittest.TestCase):
 
         y2 = torch.Tensor([1, 1, 1, 1, 1])
 
-        dataset = ClassifierDataset(
+        dataset = DictDataset(
             name="new_data",
             split="train",
             X_dict={"data1": x1, "data2": x2},
-            Y_dict={"label1": y1, "label2": y2},
+            Y_dict={"task1": y1, "task2": y2},
         )
 
-        dataloader1 = ClassifierDataLoader(
-            task_to_label_dict={"task1": "label1"}, dataset=dataset, batch_size=2
-        )
+        dataloader1 = DictDataLoader(dataset=dataset, batch_size=2)
 
         x_batch, y_batch = next(iter(dataloader1))
 
         # Check if the dataloader is correctly constructed
-        self.assertEqual(dataloader1.task_to_label_dict, {"task1": "label1"})
         self.assertEqual(dataloader1.dataset.split, "train")
         self.assertTrue(torch.equal(x_batch["data1"], torch.Tensor([[1, 0], [1, 2]])))
         self.assertTrue(
@@ -79,17 +72,14 @@ class DatasetTest(unittest.TestCase):
                 x_batch["data2"], torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0]])
             )
         )
-        self.assertTrue(torch.equal(y_batch["label1"], torch.Tensor([0, 0])))
-        self.assertTrue(torch.equal(y_batch["label2"], torch.Tensor([1, 1])))
+        self.assertTrue(torch.equal(y_batch["task1"], torch.Tensor([0, 0])))
+        self.assertTrue(torch.equal(y_batch["task2"], torch.Tensor([1, 1])))
 
-        dataloader2 = ClassifierDataLoader(
-            task_to_label_dict={"task2": "label2"}, dataset=dataset, batch_size=3
-        )
+        dataloader2 = DictDataLoader(dataset=dataset, batch_size=3)
 
         x_batch, y_batch = next(iter(dataloader2))
 
         # Check if the dataloader with differet batch size is correctly constructed
-        self.assertEqual(dataloader2.task_to_label_dict, {"task2": "label2"})
         self.assertEqual(dataloader2.dataset.split, "train")
         self.assertTrue(
             torch.equal(
@@ -102,8 +92,8 @@ class DatasetTest(unittest.TestCase):
                 torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0], [1, 2, 3, 0, 0]]),
             )
         )
-        self.assertTrue(torch.equal(y_batch["label1"], torch.Tensor([0, 0, 0])))
-        self.assertTrue(torch.equal(y_batch["label2"], torch.Tensor([1, 1, 1])))
+        self.assertTrue(torch.equal(y_batch["task1"], torch.Tensor([0, 0, 0])))
+        self.assertTrue(torch.equal(y_batch["task2"], torch.Tensor([1, 1, 1])))
 
         y3 = [
             torch.Tensor([2]),
@@ -113,7 +103,7 @@ class DatasetTest(unittest.TestCase):
             torch.Tensor([2]),
         ]
 
-        dataset.Y_dict["label2"] = y3
+        dataset.Y_dict["task2"] = y3
 
         x_batch, y_batch = next(iter(dataloader1))
         # Check dataloader is correctly updated with update dataset
@@ -122,7 +112,7 @@ class DatasetTest(unittest.TestCase):
                 x_batch["data2"], torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0]])
             )
         )
-        self.assertTrue(torch.equal(y_batch["label2"], torch.Tensor([[2], [2]])))
+        self.assertTrue(torch.equal(y_batch["task2"], torch.Tensor([[2], [2]])))
 
         x_batch, y_batch = next(iter(dataloader2))
         self.assertTrue(
@@ -131,7 +121,7 @@ class DatasetTest(unittest.TestCase):
                 torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0], [1, 2, 3, 0, 0]]),
             )
         )
-        self.assertTrue(torch.equal(y_batch["label2"], torch.Tensor([[2], [2], [2]])))
+        self.assertTrue(torch.equal(y_batch["task2"], torch.Tensor([[2], [2], [2]])))
 
     def test_split_data(self):
         N = 1000

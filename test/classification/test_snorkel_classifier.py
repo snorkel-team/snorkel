cc0d0dd9a661f8b4aab04da971b675f741f4903c
Braden Hancock <hancock.braden@gmail.com>
2019-07-10 13:25:16 -0700

    Move helpers below, test calculate_loss() directly

--- test/classification/test_snorkel_classifier.py
+++ test/classification/test_snorkel_classifier.py
@@ -14,59 +14,22 @@ NUM_EXAMPLES = 10
 BATCH_SIZE = 2
 
 
-def create_dataloader(task_name="task", split="train", **kwargs):
-    X = torch.FloatTensor([[i, i] for i in range(NUM_EXAMPLES)])
-    Y = torch.ones(NUM_EXAMPLES, 1).long()
-
-    dataset = DictDataset(
-        name="dataset", split=split, X_dict={"data": X}, Y_dict={task_name: Y}
-    )
-
-    dataloader = DictDataLoader(dataset, batch_size=BATCH_SIZE, **kwargs)
-    return dataloader
-
-
-def create_task(task_name, module_suffixes=("", "")):
-    module1_name = f"linear1{module_suffixes[0]}"
-    module2_name = f"linear2{module_suffixes[1]}"
-
-    module_pool = nn.ModuleDict(
-        {
-            module1_name: nn.Sequential(nn.Linear(2, 4), nn.ReLU()),
-            module2_name: nn.Linear(4, 2),
-        }
-    )
-
-    op0 = Operation(module_name=module1_name, inputs=[("_input_", "data")], name="op0")
-    op1 = Operation(module_name=module2_name, inputs=[(op0.name, 0)], name="op1")
-
-    task_flow = [op0, op1]
-
-    task = Task(
-        name=task_name,
-        module_pool=module_pool,
-        task_flow=task_flow,
-        scorer=Scorer(metrics=["accuracy"]),
-    )
-
-    return task
-
-
-task1 = create_task("task1", module_suffixes=["A", "A"])
-task2 = create_task("task2", module_suffixes=["B", "B"])
-dataloader = create_dataloader("task1")
-
-
 class ClassifierTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.task1 = create_task("task1", module_suffixes=["A", "A"])
+        cls.task2 = create_task("task2", module_suffixes=["B", "B"])
+        cls.dataloader = create_dataloader("task1")
+
     def test_onetask_model(self):
-        model = SnorkelClassifier(tasks=[task1])
+        model = SnorkelClassifier(tasks=[self.task1])
         self.assertEqual(len(model.task_names), 1)
         self.assertEqual(len(model.task_flows), 1)
         self.assertEqual(len(model.module_pool), 2)
 
     def test_twotask_none_overlap_model(self):
         """Add two tasks with totally separate modules and flows"""
-        model = SnorkelClassifier(tasks=[task1, task2])
+        model = SnorkelClassifier(tasks=[self.task1, self.task2])
         self.assertEqual(len(model.task_names), 2)
         self.assertEqual(len(model.task_flows), 2)
         self.assertEqual(len(model.module_pool), 4)
@@ -91,34 +54,35 @@ class ClassifierTest(unittest.TestCase):
 
     def test_bad_tasks(self):
         with self.assertRaisesRegex(ValueError, "Found duplicate task"):
-            SnorkelClassifier(tasks=[task1, task1])
+            SnorkelClassifier(tasks=[self.task1, self.task1])
         with self.assertRaisesRegex(ValueError, "Unrecognized task type"):
-            SnorkelClassifier(tasks=[task1, {"fake_task": 42}])
+            SnorkelClassifier(tasks=[self.task1, {"fake_task": 42}])
 
     def test_predict(self):
-        model = SnorkelClassifier([task1])
-        results = model.predict(dataloader)
+        model = SnorkelClassifier([self.task1])
+        results = model.predict(self.dataloader)
         self.assertEqual(sorted(list(results.keys())), ["golds", "probs"])
         np.testing.assert_array_equal(
-            results["golds"]["task1"], dataloader.dataset.Y_dict["task1"].numpy()
+            results["golds"]["task1"], self.dataloader.dataset.Y_dict["task1"].numpy()
         )
         self.assertEqual(results["probs"]["task1"].shape, (NUM_EXAMPLES, 2))
 
-        results = model.predict(dataloader, return_preds=True)
+        results = model.predict(self.dataloader, return_preds=True)
         self.assertEqual(sorted(list(results.keys())), ["golds", "preds", "probs"])
         self.assertEqual(results["preds"]["task1"].shape, (NUM_EXAMPLES,))
 
     def test_empty_batch(self):
         # Make the first BATCH_SIZE labels 0 so that one batch will have no labels
-        dataloader = create_dataloader("task1", shuffle=False)
+        dataset = create_dataloader("task1", shuffle=False).dataset
         for i in range(BATCH_SIZE):
-            dataloader.dataset.Y_dict["task1"][i] = 0
-        model = SnorkelClassifier([task1])
-        model.predict(dataloader)
+            dataset.Y_dict["task1"][i] = 0
+        model = SnorkelClassifier([self.task1])
+        loss_dict, count_dict = model.calculate_loss(dataset.X_dict, dataset.Y_dict)
+        self.assertEqual(count_dict["task1"], NUM_EXAMPLES - BATCH_SIZE)
 
     def test_score(self):
-        model = SnorkelClassifier([task1])
-        metrics = model.score([dataloader])
+        model = SnorkelClassifier([self.task1])
+        metrics = model.score([self.dataloader])
         self.assertIsInstance(metrics["task1/dataset/train/accuracy"], float)
 
     def test_save_load(self):
@@ -126,6 +90,8 @@ class ClassifierTest(unittest.TestCase):
 
         task1 = create_task("task1")
         task2 = create_task("task2")
+        # Make task2's second linear layer have different weights
+        task2.module_pool["linear2"] = nn.Linear(2, 2)
 
         model = SnorkelClassifier([task1])
         self.assertTrue(
@@ -151,6 +117,52 @@ class ClassifierTest(unittest.TestCase):
         )
 
         os.close(fd)
+
+
+def create_dataloader(task_name="task", split="train", **kwargs):
+    X = torch.FloatTensor([[i, i] for i in range(NUM_EXAMPLES)])
+    Y = torch.ones(NUM_EXAMPLES, 1).long()
+
+    dataset = DictDataset(
+        name="dataset", split=split, X_dict={"data": X}, Y_dict={task_name: Y}
+    )
+
+    dataloader = DictDataLoader(dataset, batch_size=BATCH_SIZE, **kwargs)
+    return dataloader
+
+
+def create_task(task_name, module_suffixes=("", "")):
+    module1_name = f"linear1{module_suffixes[0]}"
+    module2_name = f"linear2{module_suffixes[1]}"
+
+    linear1 = nn.Linear(2, 2)
+    linear1.weight.data.copy_(torch.eye(2))
+    linear1.bias.data.copy_(torch.zeros((2,)))
+
+    linear2 = nn.Linear(2, 2)
+    linear2.weight.data.copy_(torch.eye(2))
+    linear2.bias.data.copy_(torch.zeros((2,)))
+
+    module_pool = nn.ModuleDict(
+        {
+            module1_name: nn.Sequential(linear1, nn.ReLU()),
+            module2_name: linear2,
+        }
+    )
+
+    op0 = Operation(module_name=module1_name, inputs=[("_input_", "data")], name="op0")
+    op1 = Operation(module_name=module2_name, inputs=[(op0.name, 0)], name="op1")
+
+    task_flow = [op0, op1]
+
+    task = Task(
+        name=task_name,
+        module_pool=module_pool,
+        task_flow=task_flow,
+        scorer=Scorer(metrics=["accuracy"]),
+    )
+
+    return task
 
 
 if __name__ == "__main__":

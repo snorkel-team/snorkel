679613058d56e9342d24d5d969d1973cbf2d2233
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-07-11 18:27:52 +0000

    Fix docstrings for snorkel/labeling/analysis.py

--- snorkel/labeling/analysis.py
+++ snorkel/labeling/analysis.py
@@ -11,87 +11,123 @@ from snorkel.types import ArrayLike
 Matrix = Union[np.ndarray, sparse.csr_matrix]
 
 
-############################################################
-# Label Matrix Diagnostics
-############################################################
 def _covered_data_points(L: Matrix) -> np.ndarray:
-    """Returns an indicator vector where ith element = 1 if x_i is labeled by at
-    least one LF."""
+    """Get indicator vector z where z_i = 1 if x_i is labeled by at least one LF."""
     return np.ravel(np.where(L.sum(axis=1) != 0, 1, 0))
 
 
 def _overlapped_data_points(L: Matrix) -> np.ndarray:
-    """Returns an indicator vector where ith element = 1 if x_i is labeled by
-    more than one LF."""
+    """Get indicator vector z where z_i = 1 if x_i is labeled by more than one LF."""
     return np.where(np.ravel((L != 0).sum(axis=1)) > 1, 1, 0)
 
 
 def _conflicted_data_points(L: sparse.spmatrix) -> np.ndarray:
-    """Returns an indicator vector where ith element = 1 if x_i is labeled by
-    at least two LFs that give it disagreeing labels."""
+    """Get indicator vector z where z_i = 1 if x_i is labeled differently by two LFs."""
     m = sparse.diags(np.ravel(L.max(axis=1).todense()))
     return np.ravel(np.max(m @ (L != 0) != L, axis=1).astype(int).todense())
 
 
 def label_coverage(L: Matrix) -> float:
-    """Returns the **fraction of data points with > 0 (non-zero) labels**
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith item
+    """Compute the fraction of data points with at least one label.
+
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+
+    Returns
+    -------
+    float
+        Fraction of data points with labels
     """
     return _covered_data_points(L).sum() / L.shape[0]
 
 
 def label_overlap(L: Matrix) -> float:
-    """Returns the **fraction of data points with > 1 (non-zero) labels**
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith item
+    """Compute the fraction of data points with at least two (non-abstain) labels.
+
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+
+    Returns
+    -------
+    float
+        Fraction of data points with overlapping labels
     """
     return _overlapped_data_points(L).sum() / L.shape[0]
 
 
 def label_conflict(L: sparse.spmatrix) -> float:
-    """Returns the **fraction of data points with conflicting (disagreeing)
-    labels.**
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith item
+    """Compute the fraction of data points with conflicting (non-abstain) labels.
+
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+
+    Returns
+    -------
+    float
+        Fraction of data points with conflicting labels
     """
     return _conflicted_data_points(L).sum() / L.shape[0]
 
 
 def lf_polarities(L: Matrix) -> List[List[int]]:
-    """Return the polarities of each LF based on evidence in a label matrix.
+    """Infer the polarities of each LF based on evidence in a label matrix.
 
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith candidate
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+
+    Returns
+    -------
+    List[List[int]]
+        Unique output labels for each LF
     """
     return [sorted(list(set(L[:, i].data))) for i in range(L.shape[1])]
 
 
 def lf_coverages(L: Matrix) -> np.ravel:
-    """Return the **fraction of data points that each LF labels.**
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith candidate
+    """Compute frac. of examples each LF labels.
+
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+
+    Returns
+    -------
+    numpy.ndarray
+        Fraction of labeled examples for each LF
     """
     return np.ravel((L != 0).sum(axis=0)) / L.shape[0]
 
 
 def lf_overlaps(L: Matrix, normalize_by_coverage: bool = False) -> np.ndarray:
-    """Return the **fraction of items each LF labels that are also labeled by at
-     least one other LF.**
+    """Compute frac. of examples each LF labels that are labeled by another LF.
+
+    An overlapping example is one that at least one other LF returns a
+    (non-abstain) label for.
 
     Note that the maximum possible overlap fraction for an LF is the LF's
     coverage, unless `normalize_by_coverage=True`, in which case it is 1.
 
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith candidate
-        normalize_by_coverage: Normalize by coverage of the LF, so that it
-            returns the percent of LF labels that have overlaps.
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+    normalize_by_coverage
+        Normalize by coverage of the LF, so that it returns the percent of LF labels
+        that have overlaps.
+
+    Returns
+    -------
+    numpy.ndarray
+        Fraction of overlapping examples for each LF
     """
     overlaps = (L != 0).T @ _overlapped_data_points(L) / L.shape[0]
     if normalize_by_coverage:
@@ -100,18 +136,26 @@ def lf_overlaps(L: Matrix, normalize_by_coverage: bool = False) -> np.ndarray:
 
 
 def lf_conflicts(L: sparse.spmatrix, normalize_by_overlaps: bool = False) -> np.ndarray:
-    """Return the **fraction of items each LF labels that are also given a
-    different (non-abstain) label by at least one other LF.**
+    """Compute frac. of examples each LF labels and labeled differently by another LF.
+
+    A conflicting example is one that at least one other LF returns a
+    different (non-abstain) label for.
 
     Note that the maximum possible conflict fraction for an LF is the LF's
-        overlaps fraction, unless `normalize_by_overlaps=True`, in which case it
-        is 1.
+    overlaps fraction, unless `normalize_by_overlaps=True`, in which case it is 1.
 
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith candidate
-        normalize_by_overlaps: Normalize by overlaps of the LF, so that it
-            returns the percent of LF overlaps that have conflicts.
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+    normalize_by_overlaps
+        Normalize by overlaps of the LF, so that it returns the percent of LF
+        overlaps that have conflicts.
+
+    Returns
+    -------
+    numpy.ndarray
+        Fraction of conflicting examples for each LF
     """
     conflicts = (L != 0).T @ _conflicted_data_points(L) / L.shape[0]
     if normalize_by_overlaps:
@@ -120,12 +164,21 @@ def lf_conflicts(L: sparse.spmatrix, normalize_by_overlaps: bool = False) -> np.
 
 
 def lf_empirical_accuracies(L: Matrix, Y: ArrayLike) -> np.ndarray:
-    """Return the **empirical accuracy** against a set of labels Y (e.g. dev
-    set) for each LF.
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith candidate
-        Y: an [n] or [n, 1] np.ndarray of gold labels
+    """Compute empirical accuracy against a set of labels Y for each LF.
+
+    Usually, Y represents development set labels.
+
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+    Y
+        [n] or [n, 1] np.ndarray of gold labels
+
+    Returns
+    -------
+    numpy.ndarray
+        Empirical accuracies for each LF
     """
     # Assume labeled set is small, work with dense matrices
     Y = arraylike_to_numpy(Y)
@@ -140,13 +193,24 @@ def lf_summary(
     lf_names: Optional[Union[List[str], List[int]]] = None,
     est_accs: Optional[np.ndarray] = None,
 ) -> DataFrame:
-    """Returns a pandas DataFrame with the various per-LF statistics.
+    """Create a pandas DataFrame with the various per-LF statistics.
 
-    Args:
-        L: an n x m scipy.sparse matrix where L_{i,j} is the label given by the
-            jth LF to the ith candidate
-        Y: an [n] or [n, 1] np.ndarray of gold labels.
-            If provided, the empirical accuracy for each LF will be calculated
+    Parameters
+    ----------
+    L
+        Matrix where L_{i,j} is the label given by the jth LF to the ith candidate
+    Y
+        [n] or [n, 1] np.ndarray of gold labels. If provided, the empirical accuracy
+        for each LF will be calculated.
+    lf_names
+        Name of each LF. If None, indices are used.
+    est_accs
+        Learned accuracies for each LF
+
+    Returns
+    -------
+    pandas.DataFrame
+        Summary statistics for each LF
     """
     n, m = L.shape
     if lf_names is not None:
@@ -186,11 +250,19 @@ def lf_summary(
 
 
 def single_lf_summary(Y_p: ArrayLike, Y: Optional[ArrayLike] = None) -> DataFrame:
-    """Calculates coverage, overlap, conflicts, and accuracy for a single LF
+    """Calculate coverage, overlap, conflicts, and accuracy for a single LF.
 
-    Args:
-        Y_p: a np.array or torch.Tensor of predicted labels
-        Y: a np.array or torch.Tensor of true labels (if known)
+    Parameters
+    ----------
+    Y_p
+        Array of predicted labels
+    Y
+        Array of true labels (if known)
+
+    Returns
+    -------
+    pandas.DataFrame
+        Summary statistics for LF
     """
     L = sparse.csr_matrix(arraylike_to_numpy(Y_p).reshape(-1, 1))
     summary = lf_summary(L, Y)

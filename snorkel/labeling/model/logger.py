38173c73e7e0d7528768a4df307bf205c163b2d2
Paroma Varma <varma.paroma@gmail.com>
2019-07-11 14:16:45 -0700

    Add docstrings

--- snorkel/labeling/model/logger.py
+++ snorkel/labeling/model/logger.py
@@ -3,22 +3,58 @@ from typing import DefaultDict, Dict, List, Optional
 
 
 class Logger:
+    """Class for logging LabelModel.
+
+    Parameters
+    ----------
+    log_train_every
+        Number of units at which to log model
+    """
+
     def __init__(self, log_train_every: int) -> None:
         self.log_train_every = log_train_every
         self.unit_count = 0
 
     def check(self) -> bool:
-        """Returns True if the logging frequency has been met."""
+        """Check if the logging frequency has been met.
+
+        Returns
+        -------
+        bool
+            Whether to log or not based on logging frequency
+        """
         self.unit_count += 1
         return self.unit_count >= self.log_train_every
 
     def log(self, metrics_dict: Dict[str, float]) -> None:
-        """Print calculated metrics and optionally write to file (json/tb)"""
+        """Print calculated metrics and optionally write to file (json/tb).
+
+        Parameters
+        ----------
+        metrics_dict
+            Dictionary of metric names (keys) and values to log
+
+        Example
+        -------
+        metrics_dict = {"train/loss": 5.00}
+        logger.log(metrics_dict)
+        """
         self.print_to_screen(metrics_dict)
         self.unit_count = 0
 
     def print_to_screen(self, metrics_dict: Dict[str, float]) -> None:
-        """Print all metrics in metrics_dict to screen"""
+        """Print all metrics in metrics_dict to screen.
+
+        Parameters
+        ----------
+        metrics_dict
+            Dictionary of metric names (keys) and values to log
+
+        Raises
+        ------
+        Exception
+            If metric names formatted incorrectly
+        """
         score_strings: DefaultDict[str, List[str]] = defaultdict(list)
         for full_name, value in metrics_dict.items():
             task: Optional[str]

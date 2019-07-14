2ef499cf671496d7b3eea4afaccc213f1dcd1293
Braden Hancock <hancock.braden@gmail.com>
2019-07-14 02:29:20 -0700

    Add docstring to merge_config()

--- snorkel/classification/utils.py
+++ snorkel/classification/utils.py
@@ -78,7 +78,29 @@ def pad_batch(
 def merge_config(config: Config, config_updates: Dict[str, Any]) -> Config:
     """Merge a (potentially nested) dict of kwargs into a config (NamedTuple).
 
-    TBD
+    Parameters
+    ----------
+    config
+        An instantiated Config to update
+    config_updates
+        A potentially nested dict of settings to update in the Config
+
+    Returns
+    -------
+    Config
+        The updated Config
+
+    Example
+    -------
+    ```
+    config_updates = {
+        "n_epochs": 5,
+        "optimizer_config": {
+            "lr": 0.001,
+        }
+    }
+    trainer_config = merge_config(TrainerConfig(), config_updates)
+    ```
     """
     for key, value in config_updates.items():
         if key.endswith("_config") and isinstance(value, dict):

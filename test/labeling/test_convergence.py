0d7d054ba9eb5acab2f8c038ae7933e2f6cd9517
Manas Joglekar <brahmaneya@gmail.com>
2019-07-17 01:55:55 +0000

    Addressed comments

--- test/labeling/test_convergence.py
+++ test/labeling/test_convergence.py
@@ -12,61 +12,37 @@ from snorkel.labeling.preprocess import preprocessor
 from snorkel.types import DataPoint
 
 
-class LabelingConvergenceTest(unittest.TestCase):
-    @classmethod
-    def setup_class(cls):
-        # Ensure deterministic runs
-        set_seed(123)
-
-        # Create raw data
-        cls.N_TRAIN = 1500
-        cls.N_VALID = 300
-
-        cls.k = 2
-        cls.df_train = create_data(cls.N_TRAIN)
-        cls.df_valid = create_data(cls.N_VALID)
-
-    @pytest.mark.complex
-    def test_labeling_convergence(self) -> None:
-        """Test convergence of end to end labeling pipeline."""
-        # Apply LFs
-        labeling_functions = [f] + [
-            get_labeling_function(divisor) for divisor in range(2, 6)
-        ]
-        applier = PandasLFApplier(labeling_functions)
-        L_train = applier.apply(self.df_train)
-        L_valid = applier.apply(self.df_valid)
-
-        self.assertEqual(L_train.shape, (self.N_TRAIN, len(labeling_functions)))
-        self.assertEqual(L_valid.shape, (self.N_VALID, len(labeling_functions)))
-
-        # Train LabelModel
-        label_model = LabelModel(cardinality=self.k, verbose=False)
-        label_model.train_model(L_train, lr=0.01, l2=0.0, n_epochs=100)
-        Y_lm = label_model.predict_proba(L_train.todense()).argmax(axis=1) + 1
-        Y = self.df_train.y
-        err = np.where(Y != Y_lm, 1, 0).sum() / self.N_TRAIN
-        self.assertLess(err, 0.1)
-
-
 def create_data(n: int) -> pd.DataFrame:
+    """Create random pairs x1, x2 in [-1., 1.] with label x1 > x2 + 0.25."""
     X = np.random.random((n, 2)) * 2 - 1
     Y = (X[:, 0] > X[:, 1] + 0.25).astype(int) + 1
 
-    df = pd.DataFrame({"idx": np.arange(n), "x1": X[:, 0], "x2": X[:, 1], "y": Y})
+    df = pd.DataFrame(
+        {"x0": np.random.randint(0, 1000, n), "x1": X[:, 0], "x2": X[:, 1], "y": Y}
+    )
     return df
 
 
-def get_labeling_function(divisor: int) -> LabelingFunction:
-    """Get LabelingFunction that abstains unless idx is divisible by divisor."""
+def get_positive_labeling_function(divisor: int) -> LabelingFunction:
+    """Get LabelingFunction that abstains unless x0 is divisible by divisor."""
 
     def f(x: DataPoint) -> int:
-        if x.idx % divisor != 0:
-            return 0
-        if x.x1 > x.x2 + 0.25:
+        if x.x0 % divisor == 0 and x.x1 > x.x2 + 0.25:
             return 2
         else:
+            return 0
+
+    return LabelingFunction(f"lf_{divisor}", f)
+
+
+def get_negative_labeling_function(divisor: int) -> LabelingFunction:
+    """Get LabelingFunction that abstains unless x0 is divisible by divisor."""
+
+    def f(x: DataPoint) -> int:
+        if x.x0 % divisor == 0 and x.x1 <= x.x2 + 0.25:
             return 1
+        else:
+            return 0
 
     return LabelingFunction(f"lf_{divisor}", f)
 
@@ -80,13 +56,46 @@ def copy_features(x: DataPoint) -> DataPoint:
 
 @labeling_function(preprocessors=[copy_features], resources=dict(divisor=10))
 def f(x: DataPoint, divisor: int) -> int:
-    # Abstain unless idx is divisible by divisor.
-    if x.idx % divisor != 0:
-        return 0
-    if x.x1 > x.x3:
+    # Abstain unless x0 is divisible by divisor.
+    if x.x0 % divisor == 0 and x.x1 > x.x3:
         return 2
     else:
-        return 1
+        return 0
+
+
+class LabelingConvergenceTest(unittest.TestCase):
+    @classmethod
+    def setup_class(cls):
+        # Ensure deterministic runs
+        set_seed(123)
+
+        # Create raw data
+        cls.N_TRAIN = 1500
+
+        cls.k = 2
+        cls.df_train = create_data(cls.N_TRAIN)
+
+    @pytest.mark.complex
+    def test_labeling_convergence(self) -> None:
+        """Test convergence of end to end labeling pipeline."""
+        # Apply LFs
+        labeling_functions = (
+            [f]
+            + [get_positive_labeling_function(divisor) for divisor in range(2, 9)]
+            + [get_negative_labeling_function(divisor) for divisor in range(2, 9)]
+        )
+        applier = PandasLFApplier(labeling_functions)
+        L_train = applier.apply(self.df_train)
+
+        self.assertEqual(L_train.shape, (self.N_TRAIN, len(labeling_functions)))
+
+        # Train LabelModel
+        label_model = LabelModel(cardinality=self.k, verbose=False)
+        label_model.train_model(L_train, lr=0.01, l2=0.0, n_epochs=100)
+        Y_lm = label_model.predict_proba(L_train.todense()).argmax(axis=1) + 1
+        Y = self.df_train.y
+        err = np.where(Y != Y_lm, 1, 0).sum() / self.N_TRAIN
+        self.assertLess(err, 0.2)
 
 
 if __name__ == "__main__":

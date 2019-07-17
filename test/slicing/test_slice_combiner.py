84c3b50908701c1c2ec5149bba86e06df26233d1
Vincent Chen <vincentschen@users.noreply.github.com>
2019-07-16 20:01:57 -0700

    Slicing refactor + fixes (#1275)
    * Add longer training + tighter bounds for convergence tests
    
    * Fix slice combiner bug where predictor_preds not accounted for
    
    * Fix bug where add_slice_tasks misses slice_ind and slice_pred ops
    
    * Extend convergence test
    
    * Refactor slice combiner for readability
    
    * Remove commented code
    
    * Cleanup slice task naming (task_slice:pred_pred_head --> task_slice:pred_head)
    
    * Rename outputs -> flow_dict for clarity
    
    * Apparently, when run all together, the seed changes enough for it to just miss convergence
    
    * Fix deprecated pytorch warnings
    
    * Fix ignore types
    
    * Add back softmaxes over ind/pred heads
    
    * Fix formatting tests
    
    * Add more rigorous slice combiner unittests
    
    * Add unittests to check slicing module pool
    
    * Cleanup
    
    * Add temperature 'tau' to slice combiner attention
    
    * Add smoother tau value for tests with more slices/classes
    
    * Fix types
    
    * Cleanup
    
    * Add temperature test

--- test/slicing/test_slice_combiner.py
+++ test/slicing/test_slice_combiner.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 import torch
@@ -11,23 +12,247 @@ class SliceCombinerTest(unittest.TestCase):
     def setup_class(cls):
         set_seed(123)
 
-    def test_init_and_forward(self):
+    def test_forward_shape(self):
+        """Test that the reweight representation shape matches expected feature size."""
+
+        # common case
         batch_size = 4
         h_dim = 20
+        num_classes = 2
 
         outputs = {
-            "_input_": {"data": torch.FloatTensor(batch_size, 2).uniform_(0, 1)},
-            "linear": [torch.FloatTensor(batch_size, 2).uniform_(0, 1)],
             "task_slice:base_ind_head": [
+                # Always 2-dim binary outputs
                 torch.FloatTensor(batch_size, 2).uniform_(0, 1)
             ],
             "task_slice:base_pred_transform": [
-                torch.FloatTensor(batch_size, 20).uniform_(0, 1)
+                torch.FloatTensor(batch_size, h_dim).uniform_(0, 1)
             ],
             "task_slice:base_pred_head": [
-                torch.FloatTensor(batch_size, 2).uniform_(0, 1)
+                torch.FloatTensor(batch_size, num_classes).uniform_(0, 1)
             ],
         }
         combiner_module = SliceCombinerModule()
         combined_rep = combiner_module(outputs)
         self.assertEqual(tuple(combined_rep.shape), (batch_size, h_dim))
+
+        # edge case (all ones)
+        batch_size = 1
+        h_dim = 1
+        num_classes = 1
+
+        outputs = {
+            "task_slice:base_ind_head": [
+                # Always 2-dim binary outputs
+                torch.FloatTensor(batch_size, 2).uniform_(0, 1)
+            ],
+            "task_slice:base_pred_transform": [
+                torch.FloatTensor(batch_size, h_dim).uniform_(0, 1)
+            ],
+            "task_slice:base_pred_head": [
+                torch.FloatTensor(batch_size, num_classes).uniform_(0, 1)
+            ],
+        }
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        self.assertEqual(tuple(combined_rep.shape), (batch_size, h_dim))
+
+    def test_average_reweighting(self):
+        """Test average reweighting (equal weight across two slices)."""
+        batch_size = 4
+        h_dim = 20
+        num_classes = 2
+
+        outputs = {
+            "task_slice:a_ind_head": [torch.ones(batch_size, 2) * 10.0],
+            "task_slice:a_pred_transform": [torch.ones(batch_size, h_dim) * 4],
+            "task_slice:a_pred_head": [torch.ones(batch_size, num_classes) * 10.0],
+            "task_slice:b_ind_head": [torch.ones(batch_size, 2) * -10.0],
+            "task_slice:b_pred_transform": [torch.ones(batch_size, h_dim) * 2],
+            "task_slice:b_pred_head": [torch.ones(batch_size, num_classes) * -10.0],
+        }
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        self.assertTrue(torch.allclose(combined_rep, torch.ones(batch_size, h_dim) * 3))
+
+    def test_average_reweighting_by_ind(self):
+        """Test average reweighting by ind (zeros in pred head)."""
+        batch_size = 4
+        h_dim = 20
+        num_classes = 2
+
+        outputs = {
+            "task_slice:a_ind_head": [torch.ones(batch_size, 2) * 10.0],
+            "task_slice:a_pred_transform": [torch.ones(batch_size, h_dim) * 4],
+            "task_slice:a_pred_head": [torch.zeros(batch_size, num_classes)],
+            "task_slice:b_ind_head": [torch.ones(batch_size, 2) * -10.0],
+            "task_slice:b_pred_transform": [torch.ones(batch_size, h_dim) * 2],
+            "task_slice:b_pred_head": [torch.zeros(batch_size, num_classes)],
+        }
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        self.assertTrue(torch.allclose(combined_rep, torch.ones(batch_size, h_dim) * 3))
+
+    def test_average_reweighting_by_pred_confidence(self):
+        """Test average reweighting by pred confidence (zeros in ind head)."""
+        batch_size = 4
+        h_dim = 20
+        num_classes = 2
+
+        outputs = {
+            "task_slice:a_ind_head": [torch.zeros(batch_size, 2)],
+            "task_slice:a_pred_transform": [torch.ones(batch_size, h_dim) * 4],
+            "task_slice:a_pred_head": [torch.ones(batch_size, num_classes) * 5],
+            "task_slice:b_ind_head": [torch.zeros(batch_size, 2)],
+            "task_slice:b_pred_transform": [torch.ones(batch_size, h_dim) * 2],
+            "task_slice:b_pred_head": [torch.ones(batch_size, num_classes) * 5],
+        }
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        self.assertTrue(torch.all(combined_rep == torch.ones(batch_size, h_dim) * 3))
+
+        # Changing sign of prediction shouldn't change confidence
+        outputs = {
+            "task_slice:a_ind_head": [torch.zeros(batch_size, 2)],
+            "task_slice:a_pred_transform": [torch.ones(batch_size, h_dim) * 4],
+            "task_slice:a_pred_head": [torch.ones(batch_size, num_classes) * -5],
+            "task_slice:b_ind_head": [torch.zeros(batch_size, 2)],
+            "task_slice:b_pred_transform": [torch.ones(batch_size, h_dim) * 2],
+            "task_slice:b_pred_head": [torch.ones(batch_size, num_classes) * 5],
+        }
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        self.assertTrue(torch.allclose(combined_rep, torch.ones(batch_size, h_dim) * 3))
+
+    def test_many_slices(self):
+        """Test combiner on 100 synthetic generated slices."""
+
+        batch_size = 4
+        h_dim = 20
+        num_classes = 2
+
+        outputs = {}
+        # Generate 100 slices, half voting in one direction (index i) and
+        # half in another (index i%2), resulting in averaged weights
+        for i in range(100):
+            if i % 2 == 0:
+                outputs[f"task_slice:{i}_ind_head"] = [torch.ones(batch_size, 2) * 20.0]
+                outputs[f"task_slice:{i}_pred_transform"] = [
+                    torch.ones(batch_size, h_dim) * 4
+                ]
+                outputs[f"task_slice:{i}_pred_head"] = [
+                    torch.ones(batch_size, num_classes) * 20.0
+                ]
+            else:
+                outputs[f"task_slice:{i}_ind_head"] = [
+                    torch.ones(batch_size, 2) * -20.0
+                ]
+                outputs[f"task_slice:{i}_pred_transform"] = [
+                    torch.ones(batch_size, h_dim) * 2
+                ]
+                outputs[f"task_slice:{i}_pred_head"] = [
+                    torch.ones(batch_size, num_classes) * -20.0
+                ]
+
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        self.assertTrue(torch.allclose(combined_rep, torch.ones(batch_size, h_dim) * 3))
+
+    def test_combiner_multiclass(self):
+        """Test combiner in multiclass setting."""
+        batch_size = 4
+        h_dim = 20
+        num_classes = 10
+
+        # For each class, make one class (at random) the maximum score
+        # For everything else, set to "logit" between [-5, 5]
+        max_score_indexes_a = [
+            random.randint(0, num_classes) for _ in range(batch_size)
+        ]
+        pred_outputs_a = torch.FloatTensor(batch_size, num_classes).uniform_(-5, 5)
+        pred_outputs_a = pred_outputs_a.scatter_(
+            # scatter max score (10.0) at indexes
+            1,
+            torch.tensor(max_score_indexes_a).unsqueeze(1),
+            10.0,
+        )
+
+        max_score_indexes_b = [
+            random.randint(0, num_classes) for _ in range(batch_size)
+        ]
+        pred_outputs_b = torch.FloatTensor(batch_size, num_classes).uniform_(-5, 5)
+        pred_outputs_b = pred_outputs_b.scatter_(
+            # scatter max score (-10.0) at indexes
+            1,
+            torch.tensor(max_score_indexes_b).unsqueeze(1),
+            10.0,
+        )
+
+        outputs = {
+            "task_slice:a_ind_head": [torch.ones(batch_size, 2) * -10.0],
+            "task_slice:a_pred_transform": [torch.ones(batch_size, h_dim) * 4],
+            "task_slice:a_pred_head": [pred_outputs_a],
+            "task_slice:b_ind_head": [torch.ones(batch_size, 2) * 10.0],
+            "task_slice:b_pred_transform": [torch.ones(batch_size, h_dim) * 2],
+            "task_slice:b_pred_head": [pred_outputs_b],
+        }
+
+        # Ensure smoother temperature for multi-class
+        combiner_module = SliceCombinerModule()
+        combined_rep = combiner_module(outputs)
+        # higher tolerance because randomness in non-max class outputs -> for softmax
+        self.assertTrue(
+            torch.allclose(combined_rep, torch.ones(batch_size, h_dim) * 3, atol=1e-2)
+        )
+
+    def test_temperature(self):
+        """Test temperature parameter for attention weights."""
+        batch_size = 4
+        h_dim = 20
+        num_classes = 3
+
+        # Add noise to each set of inputs to attention weights
+        epsilon = 1e-5
+        outputs = {
+            "task_slice:a_ind_head": [
+                torch.ones(batch_size, 2) * 10.0
+                + torch.FloatTensor(batch_size, 2).normal_(0.0, epsilon)
+            ],
+            "task_slice:a_pred_transform": [
+                torch.ones(batch_size, h_dim) * 4
+                + torch.FloatTensor(batch_size, h_dim).normal_(0.0, epsilon)
+            ],
+            "task_slice:a_pred_head": [
+                torch.ones(batch_size, num_classes) * 10.0
+                + torch.FloatTensor(batch_size, num_classes).normal_(0.0, epsilon)
+            ],
+            "task_slice:b_ind_head": [
+                torch.ones(batch_size, 2) * -10.0
+                + torch.FloatTensor(batch_size, 2).normal_(0.0, epsilon)
+            ],
+            "task_slice:b_pred_transform": [
+                torch.ones(batch_size, h_dim) * 2
+                + torch.FloatTensor(batch_size, h_dim).normal_(0.0, epsilon)
+            ],
+            "task_slice:b_pred_head": [
+                torch.ones(batch_size, num_classes) * -10.0
+                + torch.FloatTensor(batch_size, num_classes).normal_(0.0, epsilon)
+            ],
+        }
+
+        # With larger temperature, attention is smoother and reweighted rep is closer
+        # closer to true average, despite noise
+        combiner_module = SliceCombinerModule(temperature=1e5)
+        combined_rep = combiner_module(outputs)
+        self.assertTrue(torch.allclose(combined_rep, torch.ones(batch_size, h_dim) * 3))
+
+        # With (impractically) small temperature, attention peaks/biases towards in
+        # some direction of noisy attention weights
+        combiner_module = SliceCombinerModule(temperature=1e-15)
+        combined_rep = combiner_module(outputs)
+
+        # Check number of elements that match either of the original weights
+        num_matching_original = torch.sum(
+            torch.isclose(combined_rep, torch.ones(batch_size, h_dim) * 2)
+        ) + torch.sum(torch.isclose(combined_rep, torch.ones(batch_size, h_dim) * 4))
+        self.assertEqual(num_matching_original, batch_size * h_dim)

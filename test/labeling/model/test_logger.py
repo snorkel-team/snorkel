b4feac9f0538e86cba4154a60f0b68ee25954862
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-07-19 21:44:35 +0000

    Add logger tests

--- test/labeling/model/test_logger.py
+++ test/labeling/model/test_logger.py
@@ -4,9 +4,17 @@ from snorkel.labeling.model.logger import Logger
 
 
 class LoggerTest(unittest.TestCase):
+    def test_basic(self):
+        metrics_dict = {"train/loss": 0.01}
+        logger = Logger(log_train_every=1)
+        logger.log(metrics_dict)
+
+        metrics_dict = {"train/message": "well done!"}
+        logger = Logger(log_train_every=1)
+        logger.log(metrics_dict)
+
     def test_bad_metrics_dict(self):
         bad_metrics_dict = {"task1/slice1/train/loss": 0.05}
-
         logger = Logger(log_train_every=1)
         self.assertRaises(Exception, logger.log, bad_metrics_dict)
 

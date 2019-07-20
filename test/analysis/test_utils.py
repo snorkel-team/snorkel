9ae8d44a580f49eeaf9617111241e7210b547b50
Paroma Varma <varma.paroma@gmail.com>
2019-07-20 07:24:03 -0700

    remove conflict markers

--- test/analysis/test_utils.py
+++ test/analysis/test_utils.py
@@ -3,10 +3,7 @@ import unittest
 import numpy as np
 
 from snorkel.analysis.utils import (
-<<<<<<< HEAD
     _hash,
-=======
->>>>>>> d012aefb... move tie_break, add tie_break to predict, add tests for tie break
     break_ties,
     convert_labels,
     filter_labels,

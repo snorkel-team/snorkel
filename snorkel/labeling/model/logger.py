270376bca76317d9dd5b016011b2d511d2d6817f
Braden Hancock <hancock.braden@gmail.com>
2019-07-22 15:34:07 -0700

    Offset by 1 to print multiples of log_train_every

--- snorkel/labeling/model/logger.py
+++ snorkel/labeling/model/logger.py
@@ -20,7 +20,7 @@ class Logger:
 
     def __init__(self, log_train_every: int) -> None:
         self.log_train_every = log_train_every
-        self.unit_count = 0
+        self.unit_count = -1
 
     def check(self) -> bool:
         """Check if the logging frequency has been met.
@@ -31,7 +31,7 @@ class Logger:
             Whether to log or not based on logging frequency
         """
         self.unit_count += 1
-        return (self.unit_count - 1) % self.log_train_every == 0
+        return self.unit_count % self.log_train_every == 0
 
     def log(self, metrics_dict: Dict[str, float]) -> None:
         """Print all metrics in metrics_dict to screen.

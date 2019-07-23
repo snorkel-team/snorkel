f0762ce485a7264b8deb733398d5f9a1e68ca379
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-07-22 17:46:10 -0700

    Remove synthetic tutorial (#1308)

--- snorkel/synthetic/synthetic_data.py
+++ snorkel/synthetic/synthetic_data.py
@@ -1,13 +1,7 @@
-from functools import partial
-from typing import List, Optional, Tuple, Union
+from typing import Tuple
 
 import numpy as np
-import pandas as pd
 from scipy.sparse import csr_matrix, lil_matrix
-
-from snorkel.augmentation.tf import LambdaTransformationFunction
-from snorkel.labeling.lf import LabelingFunction
-from snorkel.types import DataPoint
 
 
 def generate_simple_label_matrix(
@@ -54,87 +48,3 @@ def generate_simple_label_matrix(
             L[i, j] = np.random.choice(k + 1, p=P[j, :, Y[i] - 1])
     L = L.tocsr()
     return P, Y, L
-
-
-def generate_mog_dataset(
-    n: int, d: int, cov: Optional[np.ndarray] = None, n_noise_dim: int = 0
-) -> pd.DataFrame:
-    r"""Generate a simple mixture-of-gaussians (MOG) dataset.
-
-    Consists of d-dim vectors x \in \mathbb{R}^d, and binary labels y \in {1,2}.
-    Returns as a pandas DataFrame.
-    """
-    ones = np.ones(d)
-    nh = int(np.floor(n / 2))
-    if cov is None:
-        cov = np.diag(np.random.random(d))
-    X_pos = np.random.multivariate_normal(ones, cov, nh)
-    X_neg = np.random.multivariate_normal(-1 * ones, cov, n - nh)
-
-    # Combine and shuffle
-    X = np.vstack([X_pos, X_neg])
-    Y = np.concatenate([np.ones(nh), 2 * np.ones(n - nh)]).astype(int)
-    order = list(range(n))
-    np.random.shuffle(order)
-    X = X[order]
-    Y = Y[order]
-
-    # Add some noise
-    if n_noise_dim > 0:
-        X = np.hstack((X, np.random.rand(X.shape[0], n_noise_dim)))
-
-    # Convert to DataFrame
-    return pd.DataFrame(dict(x=list(X), y=Y))
-
-
-def lf_template(x: DataPoint, index: int = 0, abstain_rate: float = 0.0) -> int:
-    r"""LF templates that votes based on attribute x ``index``.
-
-    Takes as input a DataPoint x with attribute x \in \mathbb{R}^d, and outputs
-    based on the ``index``th entry, with probability ``abstain_rate`` of outputting 0 (abstain).
-    """
-    if np.random.random() < abstain_rate:
-        return 0
-    elif x.x[index] > 0:
-        return 1
-    else:
-        return 2
-
-
-def generate_single_feature_lfs(
-    dims: Union[int, List[int]], abstain_rate: float = 0.0
-) -> List[LabelingFunction]:
-    """Generate a list of m labeling functions (LFs) based on a single attribute 'x'.
-
-    Each LF abstains with probability ``abstain_rate``, else label based on the
-    ith entry of the input DataPoint x.x.
-    """
-    if isinstance(dims, int):
-        dims = list(range(dims))
-    return [
-        LabelingFunction(
-            f"LF_feature_{i}", partial(lf_template, index=i, abstain_rate=abstain_rate)
-        )
-        for i in dims
-    ]
-
-
-def tf_template(x: DataPoint, i: int) -> DataPoint:
-    r"""TF template that transforms data attribute x.x randomly.
-
-    Takes as input a DataPoint x with attribute x \in \mathbb{R}^d and randomly
-    transforms ``index``th value.
-    """
-    x.x[i] = np.random.rand()
-    return x
-
-
-def generate_resampling_tfs(
-    dims: Union[int, List[int]]
-) -> List[LambdaTransformationFunction]:
-    """Generate a list of transformation functions (TFs) that transform attribute 'x'."""
-    if isinstance(dims, int):
-        dims = list(range(dims))
-    return [
-        LambdaTransformationFunction(f"tf_{i}", partial(tf_template, i=i)) for i in dims
-    ]

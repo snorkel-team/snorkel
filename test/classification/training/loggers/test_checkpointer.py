d2b248bc36ca657a9e6b953ebdfa68af93d10ced
Vincent Chen <vincentschen@users.noreply.github.com>
2019-07-23 14:09:36 -0700

    Cleanup checkpointer + fix checkpoint_metric silent failure from malformed naming (#1310)
    * Fix checkpoint_metric silent failure from malformed naming
    
    * Decouple 'checkpoint_dir' and log_writer.log_dir'; Add checkpoint_runway validation
    
    * Address PR

--- test/classification/training/loggers/test_checkpointer.py
+++ test/classification/training/loggers/test_checkpointer.py
@@ -21,43 +21,47 @@ class TestLogManager(unittest.TestCase):
             **log_manager_config,
             checkpoint_dir=self.test_dir,
             checkpoint_runway=3,
-            checkpoint_metric="f1:max",
+            checkpoint_metric="task/dataset/valid/f1:max",
         )
         model = SnorkelClassifier([])
-        checkpointer.checkpoint(2, model, dict(f1=0.5))
+        checkpointer.checkpoint(2, model, {"task/dataset/valid/f1": 0.5})
         self.assertEqual(len(checkpointer.best_metric_dict), 0)
-        checkpointer.checkpoint(3, model, dict(f1=0.8, f2=0.5))
-        self.assertEqual(checkpointer.best_metric_dict["f1"], 0.8)
-        checkpointer.checkpoint(4, model, dict(f1=0.9))
-        self.assertEqual(checkpointer.best_metric_dict["f1"], 0.9)
+        checkpointer.checkpoint(
+            3, model, {"task/dataset/valid/f1": 0.8, "task/dataset/valid/f2": 0.5}
+        )
+        self.assertEqual(checkpointer.best_metric_dict["task/dataset/valid/f1"], 0.8)
+        checkpointer.checkpoint(4, model, {"task/dataset/valid/f1": 0.9})
+        self.assertEqual(checkpointer.best_metric_dict["task/dataset/valid/f1"], 0.9)
 
     def test_checkpointer_min(self) -> None:
         checkpointer = Checkpointer(
             **log_manager_config,
             checkpoint_dir=self.test_dir,
             checkpoint_runway=3,
-            checkpoint_metric="f1:min",
+            checkpoint_metric="task/dataset/valid/f1:min",
         )
         model = SnorkelClassifier([])
-        checkpointer.checkpoint(3, model, dict(f1=0.8, f2=0.5))
-        self.assertEqual(checkpointer.best_metric_dict["f1"], 0.8)
-        checkpointer.checkpoint(4, model, dict(f1=0.7))
-        self.assertEqual(checkpointer.best_metric_dict["f1"], 0.7)
+        checkpointer.checkpoint(
+            3, model, {"task/dataset/valid/f1": 0.8, "task/dataset/valid/f2": 0.5}
+        )
+        self.assertEqual(checkpointer.best_metric_dict["task/dataset/valid/f1"], 0.8)
+        checkpointer.checkpoint(4, model, {"task/dataset/valid/f1": 0.7})
+        self.assertEqual(checkpointer.best_metric_dict["task/dataset/valid/f1"], 0.7)
 
     def test_checkpointer_clear(self) -> None:
         checkpoint_dir = os.path.join(self.test_dir, "clear")
         checkpointer = Checkpointer(
             **log_manager_config,
             checkpoint_dir=checkpoint_dir,
-            checkpoint_metric="f1:max",
+            checkpoint_metric="task/dataset/valid/f1:max",
             checkpoint_clear=True,
         )
         model = SnorkelClassifier([])
-        checkpointer.checkpoint(1, model, dict(f1=0.8))
-        expected_files = ["checkpoint_1.pth", "best_model_f1.pth"]
+        checkpointer.checkpoint(1, model, {"task/dataset/valid/f1": 0.8})
+        expected_files = ["checkpoint_1.pth", "best_model_task_dataset_valid_f1.pth"]
         self.assertEqual(set(os.listdir(checkpoint_dir)), set(expected_files))
         checkpointer.clear()
-        expected_files = ["best_model_f1.pth"]
+        expected_files = ["best_model_task_dataset_valid_f1.pth"]
         self.assertEqual(os.listdir(checkpoint_dir), expected_files)
 
     def test_checkpointer_load_best(self) -> None:
@@ -65,16 +69,16 @@ class TestLogManager(unittest.TestCase):
         checkpointer = Checkpointer(
             **log_manager_config,
             checkpoint_dir=checkpoint_dir,
-            checkpoint_metric="f1:max",
+            checkpoint_metric="task/dataset/valid/f1:max",
         )
         model = SnorkelClassifier([])
-        checkpointer.checkpoint(1, model, dict(f1=0.8))
+        checkpointer.checkpoint(1, model, {"task/dataset/valid/f1": 0.8})
         load_model = checkpointer.load_best_model(model)
         self.assertEqual(model, load_model)
 
-    def test_no_checkpoint_dir(self) -> None:
-        with self.assertRaisesRegex(ValueError, "no checkpoint_dir"):
-            Checkpointer(**log_manager_config, checkpoint_dir=None)
+    def test_bad_checkpoint_runway(self) -> None:
+        with self.assertRaisesRegex(ValueError, "checkpoint_runway"):
+            Checkpointer(**log_manager_config, checkpoint_runway=-1)
 
     def test_no_zero_frequency(self) -> None:
         with self.assertRaisesRegex(ValueError, "checkpoint freq"):
@@ -87,12 +91,19 @@ class TestLogManager(unittest.TestCase):
             Checkpointer(
                 **log_manager_config,
                 checkpoint_dir=self.test_dir,
-                checkpoint_metric="f1-min",
+                checkpoint_metric="task/dataset/split/f1-min",
             )
 
         with self.assertRaisesRegex(ValueError, "metric mode"):
             Checkpointer(
                 **log_manager_config,
                 checkpoint_dir=self.test_dir,
-                checkpoint_metric="f1:mode",
+                checkpoint_metric="task/dataset/split/f1:mode",
+            )
+
+        with self.assertRaisesRegex(ValueError, "checkpoint_metric must be formatted"):
+            Checkpointer(
+                **log_manager_config,
+                checkpoint_dir=self.test_dir,
+                checkpoint_metric="accuracy:max",
             )

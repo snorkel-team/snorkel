55cbeb966a3f1ae7ed5b473b7159225baf42604a
Braden Hancock <hancock.braden@gmail.com>
2019-08-09 10:53:03 -0700

    Up test coverage

--- test/classification/test_loss.py
+++ test/classification/test_loss.py
@@ -4,7 +4,10 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-from snorkel.classification import cross_entropy_with_probs
+from snorkel.classification import (
+    cross_entropy_with_probs,
+    cross_entropy_with_probs_from_outputs,
+)
 from snorkel.utils import preds_to_probs
 
 
@@ -58,6 +61,31 @@ class SoftCrossEntropyTest(unittest.TestCase):
         ces_loss1 = cross_entropy_with_probs(Y_probs1, Y_golds_probs)
         ces_loss2 = cross_entropy_with_probs(Y_probs2, Y_golds_probs)
         self.assertEqual(ces_loss1, ces_loss2)
+
+    def test_invalid_reduction(self):
+        Y_golds = torch.LongTensor([0, 1, 2])
+        Y_golds_probs = torch.Tensor(preds_to_probs(Y_golds.numpy(), num_classes=4))
+
+        Y_probs = torch.rand_like(Y_golds_probs)
+        Y_probs = Y_probs / Y_probs.sum(dim=1).reshape(-1, 1)
+
+        with self.assertRaisesRegex(ValueError, "Keyword 'reduction' must be"):
+            cross_entropy_with_probs(Y_probs, Y_golds_probs, reduction="bad")
+
+    def test_from_outputs(self):
+        Y_golds = torch.LongTensor([0, 1, 2])
+        Y_golds_probs = torch.Tensor(preds_to_probs(Y_golds.numpy(), num_classes=4))
+
+        Y_probs = torch.rand_like(Y_golds_probs)
+
+        outputs = {"last_op": [Y_probs]}
+        active = torch.ones_like(Y_golds).to(torch.uint8)
+
+        loss_from_tensors = cross_entropy_with_probs(Y_probs, Y_golds_probs)
+        loss_from_outputs = cross_entropy_with_probs_from_outputs(
+            op_name="last_op", outputs=outputs, Y=Y_golds_probs, active=active
+        )
+        self.assertEqual(loss_from_tensors, loss_from_outputs)
 
     def test_loss_weights(self):
         FACTOR = 10

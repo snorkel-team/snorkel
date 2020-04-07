c50e82103e84910df4483dad2d789a9185d3f0f7
Arnault Chazareix <arnaultc@sicara.com>
2020-04-07 21:51:24 +0200

    add HashingFunction to snorkel.types

--- snorkel/types/__init__.py
+++ snorkel/types/__init__.py
@@ -1,2 +1,3 @@
 from .classifier import Config  # noqa: F401
 from .data import DataPoint, DataPoints, Field, FieldMap  # noqa: F401
+from .hashing import HashingFunction  # noqa: F401
---
+++ snorkel/types/hashing.py
@@ -0,0 +1,4 @@
+from collections import Hashable
+from typing import Any, Callable
+
+HashingFunction = Callable[[Any], Hashable]

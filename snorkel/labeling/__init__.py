745fedfc0421c037b87ca58fa82afdb2bb7d9127
Vikesh Singh Baghel <vikeshsingh37@gmail.com>
2020-04-16 15:21:37 +0530

    Update __init__.py
    Handle import error for LabelModel,MajorityClassVoter, MajorityLabelVoter, RandomVoter

--- snorkel/labeling/__init__.py
+++ snorkel/labeling/__init__.py
@@ -5,3 +5,5 @@ from .apply.core import LFApplier  # noqa: F401
 from .apply.pandas import PandasLFApplier  # noqa: F401
 from .lf.core import LabelingFunction, labeling_function  # noqa: F401
 from .utils import filter_unlabeled_dataframe  # noqa: F401
+from .model.baselines import MajorityClassVoter, MajorityLabelVoter, RandomVoter  # noqa: F401
+from .model.label_model import LabelModel  # noqa: F401

2f3837bb00eb7ba6ca61116ac42a7ca56ea8bbcc
anerirana <anerirana2000@gmail.com>
2021-05-23 13:51:30 +0530

    adding progress bar parameter to fit method

--- snorkel/labeling/model/label_model.py
+++ snorkel/labeling/model/label_model.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from munkres import Munkres  # type: ignore
-from tqdm import tqdm
+from tqdm import trange
 
 from snorkel.labeling.analysis import LFAnalysis
 from snorkel.labeling.model.base_labeler import BaseLabeler
@@ -810,6 +810,7 @@ class LabelModel(nn.Module, BaseLabeler):
         L_train: np.ndarray,
         Y_dev: Optional[np.ndarray] = None,
         class_balance: Optional[List[float]] = None,
+        progress_bar: bool = True,
         **kwargs: Any,
     ) -> None:
         """Train label model.
@@ -824,6 +825,8 @@ class LabelModel(nn.Module, BaseLabeler):
             Gold labels for dev set for estimating class_balance, by default None
         class_balance
             Each class's percentage of the population, by default None
+        progress_bar
+            To display a progress bar, by default True
         **kwargs
             Arguments for changing train config defaults.
 
@@ -920,35 +923,41 @@ class LabelModel(nn.Module, BaseLabeler):
         # Train the model
         metrics_hist = {}  # The most recently seen value for all metrics
 
-        with tqdm(
-            range(start_iteration, self.train_config.n_epochs), unit="epoch"
-        ) as tepoch:
-            for epoch in tepoch:
-                self.running_loss = 0.0
-                self.running_examples = 0
+        if progress_bar:
+            epochs = trange(start_iteration, self.train_config.n_epochs, unit="epoch")
+        else:
+            epochs = range(start_iteration, self.train_config.n_epochs)
 
-                # Zero the parameter gradients
-                self.optimizer.zero_grad()
+        for epoch in epochs:
+            self.running_loss = 0.0
+            self.running_examples = 0
 
-                # Forward pass to calculate the average loss per example
-                loss = self._loss_mu(l2=self.train_config.l2)
-                if torch.isnan(loss):
-                    msg = "Loss is NaN. Consider reducing learning rate."
-                    raise Exception(msg)
+            # Zero the parameter gradients
+            self.optimizer.zero_grad()
 
-                # Backward pass to calculate gradients
-                # Loss is an average loss per example
-                loss.backward()
+            # Forward pass to calculate the average loss per example
+            loss = self._loss_mu(l2=self.train_config.l2)
+            if torch.isnan(loss):
+                msg = "Loss is NaN. Consider reducing learning rate."
+                raise Exception(msg)
 
-                # Perform optimizer step
-                self.optimizer.step()
+            # Backward pass to calculate gradients
+            # Loss is an average loss per example
+            loss.backward()
 
-                # Calculate metrics, log, and checkpoint as necessary
-                metrics_dict = self._execute_logging(loss)
-                metrics_hist.update(metrics_dict)
+            # Perform optimizer step
+            self.optimizer.step()
 
-                # Update learning rate
-                self._update_lr_scheduler(epoch)
+            # Calculate metrics, log, and checkpoint as necessary
+            metrics_dict = self._execute_logging(loss)
+            metrics_hist.update(metrics_dict)
+
+            # Update learning rate
+            self._update_lr_scheduler(epoch)
+
+        # Cleanup progress bar if enabled
+        if progress_bar:
+            epochs.close()
 
         # Post-processing operations on mu
         self._clamp_params()
